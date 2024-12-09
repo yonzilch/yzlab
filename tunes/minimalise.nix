bc422bfe836601e26f6dc35216977aeab1f1777a
yonzilch <github@yonzilch.com>
2024-12-09 22:50:20 +0800

    fix minimalise.nix

--- tunes/minimalise.nix
+++ tunes/minimalise.nix
@@ -19,19 +19,21 @@
     services = {
       systemd-udev-settle.enable = false;
       systemd-journal-flush.enable = false;
-      journald = {
-      extraConfig = ''
-        Storage=volatile
-        Compress=yes
-        SystemMaxUse=50M
-        RuntimeMaxUse=10M
-        MaxFileSec=1day
-        MaxRetentionSec=1month
-        RateLimitInterval=30s
-        RateLimitBurst=1000
-      '';
     };
   };
+
+  # Minimize journal
+  services.journald = {
+    extraConfig = ''
+      Storage=volatile
+      Compress=yes
+      SystemMaxUse=50M
+      RuntimeMaxUse=10M
+      MaxFileSec=1day
+      MaxRetentionSec=1month
+      RateLimitInterval=30s
+      RateLimitBurst=1000
+    '';
   };
 
   # Disable unnecessary programs
