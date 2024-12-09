fa6ba61cb44ea6c7cb0333a22cbc232e3e0d64cf
yonzilch <github@yonzilch.com>
2024-12-09 19:03:35 +0800

    update config of host smallpine

--- hosts/smallpine/config.nix
+++ hosts/smallpine/config.nix
@@ -13,60 +13,47 @@
     ../../tunes/timezone.nix
   ];
 
-  boot.loader.grub.enable = true;
-
-  boot.loader.grub.device = "/dev/vda";
-  swapDevices = [{ device = "/swapfile"; size = 1075; }];
+  # boot
   boot.kernelParams = [ "console=tty0" ];
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/vda";
+
+  #swap
+  swapDevices = [{ device = "/swapfile"; size = 2048; }];
 
   # openssh
   services.openssh.enable = true;
-  services.openssh.settings.PermitRootLogin = "yes";
+  services.openssh.settings = {
+    PermitRootLogin = "yes";
+    PubkeyAuthentication = "yes";
+    PasswordAuthentication = "yes";
+  };
 
   # networking
-  networking = {
-    networkmanager.enable = true;
-    usePredictableInterfaceNames = false;
-    interfaces.eth0.ipv4.addresses = [
-      {
-        address = "74.48.189.174";
-        prefixLength = 26;
-      }
+  systemd.network.networks.eth0 = {
+    address = [
+      "74.48.189.174/26"
+      "2607:f130:0:17d::e1aa:3/128"
     ];
-    defaultGateway = {
-      address = "74.48.189.129";
-      interface = "eth0";
-    };
-    interfaces.eth0.ipv6.addresses = [
+    gateway = [ "74.48.189.129" ];
+    routes = [
       {
-        address = "2607:f130:0:17d::e1aa:3";
-        prefixLength = 128;
-      }
-      {
-        address = "2607:f130:0:17d::3881";
-        prefixLength = 64;
-      }
-      {
-        address = "2607:f130:0:17d::fc7a:71c5";
-        prefixLength = 64;
-      }
-      {
-        address = "2607:f130:0:17d::4f60:7a23";
-        prefixLength = 64;
-      }
-    ];
-    defaultGateway6 = {
-      address = "2607:f130:0:17d::1";
-      interface = "eth0";
-    };
+        routeConfig = {
+          Gateway = "2607:f130:0:17d::1";
+          GatewayOnLink = true;
+        };
     hostName = host;
   };
 
-  users.users.root = {
-    hashedPassword = "$y$j9T$dCZKGGtp932RhwMuaua54.$qKlsBjVBe54nWMmVGcshCK1fOwZ9Y0I3bZldkNZ5bCD";
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDX+DDGhCFLw2DHAOCo0mq62UvrghcfiofdMoOGa/eAK"
-    ];
+  # users
+  users = {
+    mutableUsers = false;
+    users.root = {
+      initialHashedPassword = "$y$j9T$dCZKGGtp932RhwMuaua54.$qKlsBjVBe54nWMmVGcshCK1fOwZ9Y0I3bZldkNZ5bCD";
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDX+DDGhCFLw2DHAOCo0mq62UvrghcfiofdMoOGa/eAK"
+      ];
+    };
   };
 
   system.stateVersion = "24.11";
