8cb5ea6d88f5d2dbfeabb2301d08fa7283d3f5b6
yonzilch <github@yonzilch.com>
2024-12-15 15:05:08 +0800

    update hardware.nix in host crunchbits-1,crunchbits-2

--- hosts/crunchbits-1/hardware.nix
+++ hosts/crunchbits-1/hardware.nix
@@ -1,13 +1,13 @@
 _: {
 
-  boot.initrd.availableKernelModules = [ "virtio_blk" "virtio_mmio" "virtio_net" "virtio_pci" "virtio_scsi" ];
+  boot.initrd.availableKernelModules = [ "virtio_scsi" "xhci_pci" "sd_mod" "sr_mod" "ahci" "ata_piix" "virtio_pci" "xen_blkfront" "vmw_pvscsi" ];
   boot.initrd.includeDefaultModules = false;
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/e89721fc-fd5f-41a8-a6bd-a8d82118ad52";
+    { device = "/dev/disk/by-uuid/d147d7e1-d7dc-4154-9a8a-ff549f8b9c55";
       fsType = "ext4";
     };
   networking.useDHCP = false;
