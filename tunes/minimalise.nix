2f42bb42f406d0e68fc44acd6295b79c95d911f1
yonzilch <github@yonzilch.com>
2024-12-18 21:34:00 +0800

    update minimalise.nix

--- tunes/minimalise.nix
+++ tunes/minimalise.nix
@@ -8,7 +8,6 @@
   documentation.enable = false;
   documentation.doc.enable = false;
   documentation.info.enable = false;
-  documentation.html.enable = false;
   documentation.man.enable = false;
   documentation.nixos.enable = false;
 
