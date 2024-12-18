400ebda7c34cd1d118681c3c273bf9d55cd8c4b7
yonzilch <github@yonzilch.com>
2024-12-18 20:50:01 +0800

    update minimalise.nix

--- tunes/network.nix
+++ tunes/network.nix
@@ -1,7 +1,6 @@
 { config, host, lib, pkgs, ... }:
 {
   networking = {
-    enableIPv6 = false;
     firewall.enable = false;
     hostName = host;
     nameservers = [ "1.1.1.1" "2606:4700:4700::1111" ];
