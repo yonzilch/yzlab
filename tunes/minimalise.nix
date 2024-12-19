3092ee0e3cf1609852afa81953e4c5bc7db6f291
yonzilch <github@yonzilch.com>
2024-12-19 21:19:25 +0800

    update minimalise.nix

--- tunes/minimalise.nix
+++ tunes/minimalise.nix
@@ -14,7 +14,7 @@
   # Minimize environment
   environment = {
     defaultPackages = lib.mkForce [ ];
-    systemPackages = lib.filter (pkg: pkg.name != "which") (environment.systemPackages);
+    systemPackages = lib.remove pkgs.which environment.systemPackages;
   };
 
   # Disable unnecessary fonts
