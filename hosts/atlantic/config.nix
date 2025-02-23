dfacdf772b63ba8fe96fbd8962cc2b7f490f7d89
yonzilch <github@yonzilch.com>
2025-02-23 21:06:14 +0800

    test: update config in host atlantic

--- hosts/atlantic/config.nix
+++ hosts/atlantic/config.nix
@@ -13,9 +13,9 @@
     enable = true;
     networks.eth0 = {
       address = [
-        "89.107.60.18/24"
+        "103.47.224.225/22"
       ];
-      gateway = [ "89.107.60.1" ];
+      gateway = [ "103.47.224.1" ];
       matchConfig.Name = "eth0";
       networkConfig = {
         IPv6AcceptRA = false;
