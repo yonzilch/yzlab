42a2876cbcadcfdf5dede5fbd1cdb5946987a4d2
yonzilch <github@yonzilch.com>
2025-08-10 22:50:46 +0800

    feat: add machine acerola

---
+++ sops/eval/acerola/network.nix
@@ -0,0 +1,15 @@
+{
+	"data": "ENC[AES256_GCM,data:J35sQ09UiQrLvXZru3ap3NqJCEcjzg0CakaUiqOTw0MeKtK8L5/PFNwAO/5dve8blBe9muFVDmRoPjE4e52JaFw+uZtBm4Jj4rmLiV+wRDnYrSN6FTOUM2RvxyqKHsms/yTEyAYLcChEqWmDUMKO/F6r32qn+ju0pv/BVgoJYoUowWr2u231ulFhgSUQ+LnMIotBIQ+zRDxOkqafzZRoFlsXS8QDUywekwkdThryooh2ZsCLtGzU00pyZ150frFoGvCgmzXA1Nh1jqg9KBzY3gvDvdhTeRmzsPSANudj54CcD63Jrhw37aX5rQMKMb8pNEnP3f7feBRLdFLz3Ehx/I5vCj7nnpm9oN/STjV+bZ4mDumgW9R0DVFSb+SRm8kSgdc4Q9Z0soNyPUyAHxt2s+QD7u0Zz2DjY94onoxL1MAavSfeCHpu7pPSIKmKpBTsb5EPuou5InDIbCWtRvTKNSBhzJF29dON738kxzUtFDkjNtu2af5oAYxYamsMJAD8c6Lx4vn9vHn0rL354usMHh3RVp6d9b1O04Fv0ly5KYo0b2gk61Y64OAgiKTSkU6NNN04IVMRZP9mxTavGd0pEiArjhFKhzUOINx53wna+QZAkFcm,iv:40FbiXNXfeA2bToRGjeL/totetfwN5L0llYXHnFBKNg=,tag:6vrbCHgerdlC2vmqpjbqMA==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBzYmZFTzJRYTJqRlA3MXZC\nV2tLZENrWk14OHEzTGlPS2hNWGROWmxGbHhnCkl4dEZxTk53aFRkWlIzZGZ1YmNY\nZFBKRXNIYmxWZEFYQy9NOTVFVGl4QkEKLS0tIGl4K0JOUUhiL05LVkZWaHFubkQr\ndTZmVUNaRVgrMGtjTUlvUDZNRmJic3MKPwcTUXxGb3AE1GNM3AVJIj2gMMkKOusP\nAU/zdaW2iahxYAifhkRXa6ltY8VrEfJaWVHDsjlIKFM/2/CscVv6cQ==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-08-10T14:49:01Z",
+		"mac": "ENC[AES256_GCM,data:1nbzhVYTc6fpwtU9aD/PsKRbUq7yTFUDEk1eHR70N+OGTvnndq5tsT0GUkZceZ7flh9ey+ENvELNMWjUicxF7UOhqtbv77Qib0KqjEcjwq7yxWmv8R2YWpRIP8tvW4Xj25ILmKkJ4xK+UYakiQvtUy/UcCsnzYjl7m6OqgvjjKk=,iv:HVRxO7jSi68/4pjJ4LKzZw0ihsyEq7DwnzzVAt+kxKo=,tag:8ACbFRIX7RXoUGwOWKk9/Q==,type:str]",
+		"unencrypted_suffix": "_unencrypted",
+		"version": "3.10.2"
+	}
+}
