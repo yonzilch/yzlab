0e48d002c98e416a144c3380fb7b3e82cab4afba
yonzilch <github@yonzilch.com>
2025-08-11 17:33:18 +0800

    chore: update machine silicon

--- sops/eval/silicon/openlist.nix
+++ sops/eval/silicon/openlist.nix
@@ -1,14 +1,14 @@
 {
-	"data": "ENC[AES256_GCM,data:g8wJRiwFo0+JRIFqKwN2v81Ml/RWo6rzqfkga21MlKRphw1YD4t77tOULZAKIGeNHg5PYvtF0ORrLJIIsHrPh87jM+HBNNPDzp86ovrs5PhcBvv1wyh4sqltOgcwzpOjeXTABD/7JAYYEEUBYK6CHjdjySCF4ORVRDRFZnNyE1+sXhyBLUgGQVK6XY9s9Ksqust8YFZOo3LpxDjmdx3pR53qTmAKswKkueFJGQ==,iv:Gp4oiJFHNrIrCZ9pOVAUN9hrfH6dwlvlMBxuxEwy2KM=,tag:zCTFLYI9ZpGdMrFNtfhJLA==,type:str]",
+	"data": "ENC[AES256_GCM,data:IKCD4obvypKfF5vwlTaEEbFkO6n3/I7Cu28XREIWL6VX3VSfG67Nlu30FC8bQwQjtP5MOcm+VAEAERcTfcD7onSGrhuJKCdOKj4X3vHNqD0oZUR8KyLgeWfzWzf0qnN3xgaoOaWRwc9bQsXiF1wFZQzFfHxCIT+xpXlMmv9K4C2RLdj1a4qnYRt37aYuXNDVWt7daMAehX0koxCLjencHffNyuSLw02YPZxOZQ==,iv:h9b1/wCedGBaI2aEED5VTA7zl8W8FKCWg3jvth5FUlg=,tag:eAYWfhE8bRzVigYDj02nIA==,type:str]",
 	"sops": {
 		"age": [
 			{
 				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
-				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBYSk5LOFZIVTRMaEZ2RGJj\ncnAxK2RGTDhlUVdHL3A3SlM0YmRkWk5va0Y0CnFwc2lEZVdQNUtSSHFqTVRDSUcr\ndDJFVklDV0hCZk5DOGtpcUYzcStkbE0KLS0tIFlkMWN4QmdDM0thYm5qZG5icXl4\ncjZqRDNiL0NkS1dSdTFKWEpKbUNHRm8KvufIABMZj0qiykBUy2ivsEV5alpLU/Rf\n2ePMmzQakqI8Ix0itOaZQW/4lvjLPYB9EFkPgVdPgq7UpccK5TSGBA==\n-----END AGE ENCRYPTED FILE-----\n"
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSArL1orVzh5NGRUL2VnelAr\nZEtDYW0zNWVFVmFPcEt1bnFKQ0lpZDdxNVZjCmszQXFsVHBnVVJ2V0ZtdU16cEcz\nb3JYc09rY2prTTB5SUx0UkJYZzRFZ2MKLS0tIFlYcHN2QXpsTkI3cFZhd0NULzBZ\nZFVmaEQ3WC91RDNBcEtHRit4OXhzL1UK9FaQ3+II7qlItPfa0nnkqV+z27s+FSg7\ngRqZuXdMuiDbknrjI1zOJzwHvTV4H+pI21ThW5KiZGZU7WPHJU03Xw==\n-----END AGE ENCRYPTED FILE-----\n"
 			}
 		],
-		"lastmodified": "2025-08-09T07:58:20Z",
-		"mac": "ENC[AES256_GCM,data:47ISAOLKkVhzAd6fhDhvqsyRZdyKKQ71Bkdy6mhcStw62K0+ptZuIvEiNIg9y3GfdPvQ53YCaT87LDviHWAuFDqr0kU/Wh+SP42SuLP3+15iVY45sKqmFcYQc3wWUtEyiEsNKdam9Kviyt7dXotGHtunkWvkoq/Dv4gHPtggrSI=,iv:mnhG2Mt7m6GhNzkhXy61RDTdj3ZPRWr5TsNDuxtScj0=,tag:ajucQl5mDpZaeHMmUZ6Bvg==,type:str]",
+		"lastmodified": "2025-08-11T09:27:06Z",
+		"mac": "ENC[AES256_GCM,data:ApUhZ8R9SvGpcna6xW/WJgfLznXo6cH1Rr2iM+bRkoq++XP5m0LPgG8pultDytfKj7WdwUWuuwno7t8HYEZFPTyVL18cjmAKAPRqe7h+WuqBQpCkCdV1+5XUXqj4NXZekty4NNpRRWpomHPGG6iEoh3Sl3S4YCXuSFtZTnQefp4=,iv:bPG+/WhnvlNGaHXIgmyQCPRv4cR5v5qd09bhEq1eXyI=,tag:c4guA27gFt4eU4CH/JGI3g==,type:str]",
 		"unencrypted_suffix": "_unencrypted",
 		"version": "3.10.2"
 	}
--- sops/eval/silicon/st.nix
+++ sops/eval/silicon/st.nix
@@ -1,14 +1,14 @@
 {
-	"data": "ENC[AES256_GCM,data:G12WBVdA9zN2LHsLcN7CtehDblO+ebWgq16t2g4fOQDEY1zxHrtjzK6Bap/DgOrkliH5mnK0JkV/YETXoTKvOkXBLXE=,iv:R9uz4ZnRrO6BibG9nFOO/OzWw9PiOa5VS7E2+b2GEZI=,tag:UbpiBdSygJ0eGmswlilivA==,type:str]",
+	"data": "ENC[AES256_GCM,data:3zstRVe/byP1dG2KWOnuXUua2MPgqGeZBL/UjgZmRy7N29cyuf9BIRzv4XwJd4gzhROu56CKVYksNiTsiWjcItPPHSM=,iv:Wqsi3Pwf1d3j5anpHmgotPHkFKX8d6eWrQEe1rvvo5A=,tag:UfQJuZw9SoQJHHpxn/VhtA==,type:str]",
 	"sops": {
 		"age": [
 			{
 				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
-				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSAyL1lVdWt1a29haDU1R1J1\nTEw3dEYwZE9MVm14TGZibndubWJSNnVGOEI0CllOY00veEc1aS9YWjUrUUdveEVQ\nVWZHU2VPTFYyNHAralc3V0MvZ3F3UFEKLS0tIEtEQ1kxOXVvWkNMWHRialpGNFl3\nbUd3bXVEZ01PTUpvMU1Ba3hySWYvVVkK0VWkXyodzRleH6maf6InlqdM9Lk/VURy\n0r8rkgtiNBIbPL0Z1u8Jpb2jjHkWfhptzMy/fcc6UEQ0l8UXQTOgGw==\n-----END AGE ENCRYPTED FILE-----\n"
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBIYnN5bGtmV2JJaG9GdGRq\nMTk0OFZRRzlEenJ4R3NNOEdLaVdBWUYvU1hJCjJZSjJpSE1rQm9rd29JL3o3SUN0\nOG5MU05FV2hqWUxFSlhtYmhoV09oNncKLS0tIGN6a3VMRzNRMGpBK1dkOUdLYnQ3\nOVROeW9nbDBSNjFQNUxuOGNQb3BjVWsKujSCyFBLXbJtjpwxONO57W/hjn63jQ94\nID4+hWgYtlGkjYaJq/nZDX+6jyfOqkbUKCjbkUvEI+9jEeCBQtMCIg==\n-----END AGE ENCRYPTED FILE-----\n"
 			}
 		],
-		"lastmodified": "2025-08-09T07:58:20Z",
-		"mac": "ENC[AES256_GCM,data:bLGwmB0imqaUYIhoTNYdekst3inOMGFQVX2/UfPeWD5WxsUikT/BSkpav0SVdUPGPi/jvqI/rI1MNROzr3vVFA/hmbK32GWrz0A2bwe2+ajQRBFJyVIqsU3/TvV3TMRvvoE4HZo6SMS8fqAxi/EbfPsAh7+L4D6phXWqp1Ps5m4=,iv:gMMZ5ZtwWc3JBJJLtxWqtjt9zurVPvpCV0vs5VPQfXY=,tag:N61k1ncAd4Fw4sj9C/6t+A==,type:str]",
+		"lastmodified": "2025-08-11T09:27:06Z",
+		"mac": "ENC[AES256_GCM,data:dg0MxnFcv0Lt+dNhbiv0QKu23ghoW25CXHBmKkYvaiaK1fz6cqcZ867CSLXAS1RqN34a70C3Pl8cUxRGu3vRLS1fnUD4KJsT33lHm3jND5gQoTYXL4jVqVaH++TqchTB+Fri5RRV8lyTQo0gHX0/EH4Z3L9J3GI4kDiGmeUAEgs=,iv:q4V2Dula8JNiuhyWwpow6+PRELHQlMo+J/svJidzXV0=,tag:xm6VdoVE8JXB80VEaoVOvQ==,type:str]",
 		"unencrypted_suffix": "_unencrypted",
 		"version": "3.10.2"
 	}
