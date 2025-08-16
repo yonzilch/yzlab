f09a26cf6a643bf5825d74045c958c0924f8fa1e
yonzilch <github@yonzilch.com>
2025-08-16 10:17:39 +0800

    feat: reinstall machine kyouma

--- machines/kyouma/configuration.nix
+++ machines/kyouma/configuration.nix
@@ -1,5 +1,5 @@
 {lib, ...}: let
-  hostname = "layer7";
+  hostname = "kyouma";
   ls = lib.filesystem.listFilesRecursive;
   primary-device = "/dev/disk/by-path/virtio-pci-0000:00:0a.0";
 in {
@@ -9,7 +9,6 @@ in {
       ../../modules/optional/qbee.nix
       ../../modules/optional/st.nix
       ../../modules/optional/terminal-implement.nix
-      ../../modules/optional/zfs.nix
     ]
     ++ ls ./modules
     ++ ls ../../modules/shared
@@ -25,9 +24,16 @@ in {
 
   disko.devices.disk.main.device = primary-device;
 
+  swapDevices = [
+    {
+      device = "/swapfile";
+      size = 4096;
+    }
+  ];
+
   users.users.root.openssh.authorizedKeys.keys = [
     ''
-      ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBYA9+GwVMqoxxuAK5DImKASIozfItnpOoZ0KqkG1dRI root@layer7
+      ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFZS2imrJz5KTU+/4kHeMXo0QQNJt4OM9AlWjR99jgFT root@kyouma
     ''
   ];
 }
---
+++ sops/eval/kyouma/network.nix
@@ -0,0 +1,15 @@
+{
+	"data": "ENC[AES256_GCM,data:Fow3xRCMtk7nQxj7vRkgUf8mlvBSsvJH727XJGe15Zv7HN6ZTMtixefhUEtqiS6ZM6QjJbXSJKrnU3oiMuNPEVTVNmrHoEgpPNapWShRmIuYCRNQ9rQgujCX3o+g7x6NmeiJE8xyxt/o8xtRtvKizKNAIh3diAu/wExKq4MdG28SJV45ViLqJ4h7MsmvfGh3jbLUclOot30NpWwtuxxo61SgqMuzRIyAj8HxGtopeqRpRmCUuw69fTmm9PX3dO29ONBYaVG8TUY3gveuDvnNnjF4PLXiFBRa9OEf8h4BvpewCmmkC82245nw24qZr29drwecR0Lrw61QkC6T63H1NC30GN9OGo6Hj33ONvwJkOaGlqwQRZVv6yqqThz52v5dAdfCWX/Au6Utp04RHrCll7upd78o5jpSgMiJShCYU4vNnxN1mmuOsr5TWBMMQuWATOB8qcMFMzXvkpz6y70k9qzS1inK1oTMksiZwzPnvWK1wcP2ZD7csw27y1xOrv5y1TVAHi5CIZMkdRJdLBr2rlGGPT1sMvvEhstbW07XkqNWU2GrXgECMQHCcW78lXqfTa2dlnx+WEIYXit7l3pQrm713cq2OQBuIPy0jTIXJeuR+nyGj3893qHHBnCSaopbYGbqeQ==,iv:7tRu754yYJE4FBiJODWtAn5trauvlMa3WKesWYP8QxM=,tag:3m8aY0nLZ+hNW1Ohj0hmwg==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBPZUQrRDUvcG5SOUdqYlBp\nQUluWGIwWlhUSjk2U3hLaDJqcVc0a0ZCUlJZClgwNmxxdjBFNE1kSUVZd21JWTdE\nTVAremJYTSs1cWlyRDFOTE5JaHNBdmsKLS0tIGlaeG0wN1ZwYml1b0l3UUhOVWU4\nMTAvMFA5TVQzOUFuc1NSbkJXTCtjdDgKSquMrroj9XwJDON/2FPIfu7NQGUWFbqA\nTkRdKrz3oEvTqkNBPCHds80C3FsE8sQvo4c8oDAm2022uMrL//Aj/w==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-08-16T02:15:17Z",
+		"mac": "ENC[AES256_GCM,data:ae5jVSdMMxd2HXVWDMTgh2iPpKhxi+nTYZPgugntzjkVKKZP+XepO7zFBjxWo39I/dPkbB3fO63dA+9tXx2eHfmq1I5Ra8lF1wI8upD3Zh8eHTTbdERNwX5DUZIaJi5+fssutg0I9wfWspoFUCq7wnuuGgf+Kmab0z/fQVxXyts=,iv:4bJqE8TLv+0ydHDkG1rH/byntzLR8A9smvFUc33GJaI=,tag:/9/PZ3K+31E3zmm7DpyPjw==,type:str]",
+		"unencrypted_suffix": "_unencrypted",
+		"version": "3.10.2"
+	}
+}
