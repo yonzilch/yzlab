44ebe3f67ac7e3190c18cf06188d237223f5b84a
yonzilch <github@yonzilch.com>
2025-08-16 13:19:39 +0800

    chore: update rclone config in machine silicon

--- sops/eval/silicon/st.nix
+++ sops/eval/silicon/st.nix
@@ -1,14 +1,14 @@
 {
-	"data": "ENC[AES256_GCM,data:3zstRVe/byP1dG2KWOnuXUua2MPgqGeZBL/UjgZmRy7N29cyuf9BIRzv4XwJd4gzhROu56CKVYksNiTsiWjcItPPHSM=,iv:Wqsi3Pwf1d3j5anpHmgotPHkFKX8d6eWrQEe1rvvo5A=,tag:UfQJuZw9SoQJHHpxn/VhtA==,type:str]",
+	"data": "ENC[AES256_GCM,data:rMjXtzaOx3XARULse4gwZzgftcej4gMsZ70pCs1ajtIwKkXlhN8Z0sqLglM07CSIgivgcLRD6/1B3zKlAD3sUwT7JLw=,iv:/fYSNc/U4rNjGR4GdRgHawbppZV5oPlh0NA1FzxhLuA=,tag:0rsHNVcT6RVwTixmKX/akg==,type:str]",
 	"sops": {
 		"age": [
 			{
 				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
-				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBIYnN5bGtmV2JJaG9GdGRq\nMTk0OFZRRzlEenJ4R3NNOEdLaVdBWUYvU1hJCjJZSjJpSE1rQm9rd29JL3o3SUN0\nOG5MU05FV2hqWUxFSlhtYmhoV09oNncKLS0tIGN6a3VMRzNRMGpBK1dkOUdLYnQ3\nOVROeW9nbDBSNjFQNUxuOGNQb3BjVWsKujSCyFBLXbJtjpwxONO57W/hjn63jQ94\nID4+hWgYtlGkjYaJq/nZDX+6jyfOqkbUKCjbkUvEI+9jEeCBQtMCIg==\n-----END AGE ENCRYPTED FILE-----\n"
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBvSlJLZjl2ak9zT3liK2Vp\nZUg5RWVnZ21JNkk2OWpQVStuTjZwYS9RazFvClFncU1jcjhXNnl3QXRUUTZVSU15\nMVdjVDJwSGVvUEEyeTIxY294bzU1MncKLS0tIHhFOHA5eElKUkFyYWE3OXg0ZTZv\naEY4ZnE4Mk5HTXF3YTlwejNNZ0Z2Y0EK9mdWsaTlQt5KcpryFYUIxBr0NrzsETP6\nxupxsVbszIWCUmGQ3ZxMDZuFz2sUGKpWgFMVzOVq3LqfrDANKpPswg==\n-----END AGE ENCRYPTED FILE-----\n"
 			}
 		],
-		"lastmodified": "2025-08-11T09:27:06Z",
-		"mac": "ENC[AES256_GCM,data:dg0MxnFcv0Lt+dNhbiv0QKu23ghoW25CXHBmKkYvaiaK1fz6cqcZ867CSLXAS1RqN34a70C3Pl8cUxRGu3vRLS1fnUD4KJsT33lHm3jND5gQoTYXL4jVqVaH++TqchTB+Fri5RRV8lyTQo0gHX0/EH4Z3L9J3GI4kDiGmeUAEgs=,iv:q4V2Dula8JNiuhyWwpow6+PRELHQlMo+J/svJidzXV0=,tag:xm6VdoVE8JXB80VEaoVOvQ==,type:str]",
+		"lastmodified": "2025-08-16T05:16:51Z",
+		"mac": "ENC[AES256_GCM,data:Q9R9ZsW6sJAVQWTxNh/93oa7DyLIGxVEq8CcHtzNiiEUaUV/CfwONhfCsjXOABA2qlbGLuWLCeoP3gHtLWv9CIRFI+58OtDJ7TJtEw8yVANvDU97l4YgUELoSFBU4q2TWnzuTWQmPI9BP5AHfm6ElgozsqO6d4qo57JKK6ZhsnM=,iv:oFX/D6UQloQkyOZKDb9tEdw1OL0Tk+AqF/pApqPx0hk=,tag:sFlCkZGq686GfV1FZnb39Q==,type:str]",
 		"unencrypted_suffix": "_unencrypted",
 		"version": "3.10.2"
 	}
