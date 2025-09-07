77b404c5fbe5d80abdfcce2345590df2b27168f0
yonzilch <github@yonzilch.com>
2025-09-07 18:34:11 +0800

    feat: add machine kagari

---
+++ machines/kagari/configuration.nix
@@ -0,0 +1,32 @@
+{ lib, ... }:
+let
+  hostname = "kagari";
+  ls = lib.filesystem.listFilesRecursive;
+  primary-device = "/dev/disk/by-path/pci-0000:00:09.0-scsi-0:0:0:0";
+in
+{
+  imports = [
+    ../../modules/optional/terminal-implement.nix
+    ../../modules/optional/zfs.nix
+  ]
+  ++ ls ../../modules/shared
+  ++ ls ../../sops/eval/${hostname};
+
+  boot.loader.limine = {
+    biosDevice = lib.mkForce primary-device;
+  };
+
+  clan.core.networking = {
+    targetHost = "root@${hostname}";
+  };
+
+  disko.devices.disk.main.device = primary-device;
+
+  users.users.root.openssh.authorizedKeys.keys = [
+    ''
+      ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEZ7KdjaExcJA6cWtB7Z/l53N4QJMHvnQJodYEF+pGEI root@kagari
+    ''
+  ];
+
+  zramSwap.enable = true;
+}
---
+++ machines/kagari/disko.nix
@@ -0,0 +1,67 @@
+_: {
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              size = "1M";
+              type = "EF02";
+              priority = 1;
+            };
+            esp = {
+              size = "128M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "zroot";
+              };
+            };
+          };
+        };
+      };
+    };
+    zpool = {
+      zroot = {
+        type = "zpool";
+        datasets = {
+          "root" = {
+            mountpoint = "/";
+            options = {
+              mountpoint = "legacy";
+              "com.sun:auto-snapshot" = "false";
+            };
+            type = "zfs_fs";
+          };
+          "root/nix" = {
+            mountpoint = "/nix";
+            options."com.sun:auto-snapshot" = "false";
+            type = "zfs_fs";
+          };
+        };
+        options = {
+          ashift = "12";
+          compatibility = "grub2";
+        };
+        rootFsOptions = {
+          acltype = "posixacl";
+          atime = "off";
+          compression = "lz4";
+          mountpoint = "none";
+          xattr = "sa";
+        };
+      };
+    };
+  };
+}
