11a6e7767eeb765eadc86ed1c272684649d82c01
yonzilch <github@yonzilch.com>
2025-09-22 21:36:13 +0800

    feat: add qbee and st in host kyouma

---
+++ modules/private/kyouma/st.nix
@@ -0,0 +1,15 @@
+{
+	"data": "ENC[AES256_GCM,data:8uBLZTNWJ2t8HwlkMYXt8FlzCdBzhZAJKcweEMs/kzgNOpGIqNsPa1WrOexOBGJ1wTrdqr382UP4nZFnf6wp979PHRg=,iv:wHybsLHqZDHwNumjS0dc2I4KWINNfSGIvW10YhkTjjE=,tag:Ta4JpZa+RwJRzfFhwkgwug==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBQSW5EWjEvRHNYTUd4TzBP\nKzFXbXpBTk9yV0kva084cDFuejI5WTlVVlU4Cng0NFhVYUpwWGdNbzByZE1hOEdJ\naTBGWTJTazZ1L2NUUlB4YXdSdjUzU1EKLS0tIHRBbG9tdWRmVG1BS3YwN0VzODZX\nK210ZFNNTFdBcFh3RGZ4TFJlUnNoQU0KLKLJeqDrIK2v5uxmDFDHk8JPxOWEIiLw\nwSQPkcmbVsS2o4W8fma4L7US8VV4X4XvXYZRBIGeEf4f/VdoeYK2Fw==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-09-22T13:02:16Z",
+		"mac": "ENC[AES256_GCM,data:HAzdcjdjAaFrW8DLoAIrNwhhymfe2/b5QcTJa9XrOohIJWeHWNmwP4g/I1ftzZTZ/MgfPmoU7B01k1gOWFqL7Lb00iIaLQX+pmZcHp83aaKE4rU5f/8PnrzbqBwwQq1i+dWUNVQuy1W+ig4rDfWwE8igzlN0TS8MRz3TRucPanw=,iv:EPyXICpVwwpJz69HabUDLh3zmD7Ma/wAYp5224ZAi0s=,tag:sZpXPf0ep9j0k9rB2AbuEg==,type:str]",
+		"unencrypted_suffix": "_unencrypted",
+		"version": "3.10.2"
+	}
+}
