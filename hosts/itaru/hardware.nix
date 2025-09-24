e4844a407b4024bc55d4cfcd0fd9716aac3ea3bd
yonzilch <github@yonzilch.com>
2025-09-24 15:14:00 +0800

    feat: add host itaru

---
+++ hosts/itaru/hardware.nix
@@ -0,0 +1,23 @@
+{
+  lib,
+  modulesPath,
+  ...
+}: {
+  imports = [
+    (modulesPath + "/profiles/qemu-guest.nix")
+  ];
+
+  boot.initrd.availableKernelModules = [
+    "ata_piix"
+    "xhci_pci"
+    "ahci"
+    "virtio_pci"
+    "sr_mod"
+    "virtio_blk"
+  ];
+  boot.initrd.kernelModules = [];
+  boot.kernelModules = ["kvm-intel"];
+  boot.extraModulePackages = [];
+  networking.useDHCP = lib.mkDefault true;
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+}
