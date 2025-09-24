544b6cf87ef6b8f12e6734f6caa79c19344c5c65
yonzilch <github@yonzilch.com>
2025-09-24 21:26:50 +0800

    feat: reinstall host vivalavida as new host mayuri

---
+++ modules/private/mayuri/users.nix
@@ -0,0 +1,15 @@
+{
+	"data": "ENC[AES256_GCM,data:u8++tRhePhUuteZhY/FPHVPGps8D/hgCXyi9TSzs8lt17+kyY8J1nJWlr799W1l+//wYnz/8cjAU5hCQo7wNzRRNAOq/W4K6gacbNHdi381NjJoRxO0QRZt8k1Ywr8VmpCOFsZXrE2zgo8ChGkrlTvERklJD53HceQyOBQ8wJKzFEztGffozmnIkyV8nIO32WbVMFzFKC4cKIsqoWNRpjjuPWmOTk2sku2d1nhS6zLIbYqcO3J781ZsrzZ6NuyDRqOkx7jC4yKEeuTqt/Tx/eErGSD0to6Lj2ZBlU/qw+Uo8mWQtSkFoOnGW7heVwlrWgwK8/3w4RWcfpRpW6YeSQjmN5aksoqdDIo1xoDf9wh4mz9OdeafzI/g4XYx6xYwliYBaDdSSJQ==,iv:USN2xtBMO4TeUJmlneG6RMPEE0EW+V46FAq2DQBWxVY=,tag:5moR1rO0L0ddzqf4mJM3fg==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBHdDFueEtzczVocHo0Uk9Q\nOVk1ZU1Uc1I4VXgxSDRReWY2WFcxUjgvY1RnCm9uTUkwMmtDTWUxdnphNW5YUkhF\nVUlocGJHU3BZeUlSZnFPVUpjdEpJZDgKLS0tIDhsYkkrZ3Z5Zy8xdGJLUUx3WDQy\nWVJydUhvWTRrZFJoY1IvRk9lTFBNbk0KhLCZ0toPxaC5NBXljv1MfBF1qkPO5MWW\nLvqTob6OJp4yCjkSN/i+IT5nirZlrsES+qxlu1o/GzfqaIyBQAoUNg==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-09-24T13:08:17Z",
+		"mac": "ENC[AES256_GCM,data:wXkbdx/1s2n3uEyVblDwd7X5BdbdaHSK1p9+eUy7/DWUMAaDr4TstJ1sufgD35/0JGeuPlruOl6xz1+xI5GBtGHP4KrVlrJ2TcP2be1TUdXKuozTjU0i2t+V40mjytt3YKEeHt+0jZv/1WL3Oonmh2j4cTKcgziaMZhK9E+4obM=,iv:Z0lQp8sze7yo1seDGV1K39Wk/axQnzX1aCJv3TxJIXs=,tag:YTNv4ZWtwSWRgdFgaauw5A==,type:str]",
+		"unencrypted_suffix": "_unencrypted",
+		"version": "3.10.2"
+	}
+}
