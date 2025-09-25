68c5bb5d63472d4ef7359c9c0479e72751faefc0
yonzilch <github@yonzilch.com>
2025-09-25 22:10:56 +0800

    feat: reinstall host silicon as new host suzuha

---
+++ modules/private/suzuha/komari-agent.nix
@@ -0,0 +1,15 @@
+{
+	"data": "ENC[AES256_GCM,data:gddIdHdSbZZw7sWRl9WKRep1e+IOHQvplnj+QXse3LZOiZzna0aLk5oVVtSyvohsfpUPqCfqcosXiCGRvZfksaHdaL5xhYh6540JrkZzzd8K72l6Jnf/OrCZTpEBjzf76t1wMLpC4nMGILRL3JR+cWI3OAfFI5JK9thR6GoICxkCdTAHOXlaXQ1uuszr4NfyVJ7UOTAziHhjdupIXvPXgT4C4Kauk38ogj3K2F3gBE3Sxhd6iwYkxHQ7XhrwCTtS9efSvDMT8BUaHQgNJTaSGf8r5r8sopc/wP8i9Qsd6rjDElPuG0suHS62VmonbUY944Po3vKWGmyviaHvYb8Hx/S9//1Q9PUXQuFadBds,iv:sV53fTicTl/1PffM0/de9ISBRUoqFf3k+GK+e3ZwSsw=,tag:cyctD/z+1Ol319pXmarAtw==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBKYmcwaGxJbllOUkZvS0s4\naFgrajVaUm9uSTNMQXpjTEpwOERlM2xUTno0CkVzWndFRTFBbFdGREcwQVllaEFH\na0JQWWNlSTFVNUFJeS91aWNnTzVPQ2cKLS0tIHliRWw1ZW1FZlVLWi8wU0JVemRW\nWTJDaDBSYWRkOVVka1V4bFQxSGVOSDAKqpFFz5WnWaKv/0x3RRnWAPLGBso4hHMa\nczGOtx+6rDBIwiHbfM0AbC49nK2Dgro9pnxVf7l9m549QzJDbEBaBw==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-09-25T14:06:25Z",
+		"mac": "ENC[AES256_GCM,data:kahUh2pJkTeMEmmujbfRiO4XCf2G51UMowLhIRXs6I67qWBM5lftJhVMLZu1ZzwrwmhegCk9U/nW0QCtRNGlMhx8Lz8FYyDFSIQ/6wji6+KWomCVVTBSgj0JO42TtaSJPSx64FO4vCiaWXrIHcCNs1v7eVA36+cAd2Ie+raNDF0=,iv:VEEze0MOoRNlrVuFswy3es+0n2NSYheFbXVmhM2M9og=,tag:aQMS7++BBWinphyt5Hm7LQ==,type:str]",
+		"unencrypted_suffix": "_unencrypted",
+		"version": "3.10.2"
+	}
+}
