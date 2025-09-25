a89d08de00514eecef3afa257648af70d33e41d0
yonzilch <github@yonzilch.com>
2025-09-25 10:22:33 +0800

    feat: reinstall host isla as host amane

---
+++ modules/private/amane/komari-agent.nix
@@ -0,0 +1,15 @@
+{
+	"data": "ENC[AES256_GCM,data:0ne0rIXLe1vh045PJHk1LDvxoidhtL3AE7l7E6NCY3Hl6aPIJcMeYcXgkelj0Ol/7uxL+h27yUODUjMewT0MN4FAi5GPGVIugqRKH/QJ+0ZAmlSyalJ4vFjp09vhFWjpBGXPpMEAPlCIfJeHoFb9kDySBR3OZHiPAMNEf4bXsDFuFmwoNrogFuu6e++hXLeY7pXxMH5tSmH5Rj39IP6M9RbzSvnW8jbvDpSbm2Ofy6heFibrMBOeDAHBZOJ4r7wBZUlD2ES8K9pXf7MuAqxNte4933ezyXx7haIfvHWajjJwVbJRsZpO4Mcm6YpKkzzs4d66nShQgecV3j1OdD71zLV1V24MhjnUwe/26tQDelgcbz4rgVPtPNILUtXArwPMKyMeZx0=,iv:H9d0NJ7ZCaUmPHz4/F3mMM3nQmHG+qMDL7DODcfQicU=,tag:3gGvMBGRbrwJw3KeyulmAg==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSByNDFmOE9uTWtPSlBUczd1\nd0tLZnpBNlNEYlc2d2tUMW1iMTNNS09XMUQwClVJQzdFM3lSSHpEc2VhVWZxcXl0\nUFExbFFVcmVjaFdKbVM3d05KUGhDeE0KLS0tIE1kN3ZSTnIrZXJ3Yk9uYkRndVpK\nYzNDalJkRlVreTRMSjdBdzNjTGlpeEUKsjSr6qaAEKW+JDWw9K/pUpTH7knfazJh\nIhmkKwcATkuvrvaUzZe/7wyOHjGcw+0bMjWKgsSf6oEpUjiy0lJngw==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-09-25T02:21:20Z",
+		"mac": "ENC[AES256_GCM,data:9IXnEEIhxlEkRxW2Gjo2fpjI/G4o4EKjmioyJjb845TCy14jKx4ba+yzy0m+pPUaBr6JkKFBavkzQKBYfG4oxbjP0yZPVFE2g9MUExcnetpSjQyGfxjCXUmQ/0RNCDX1Pex23mnqz51sPMPnrGrkF9/07yZ+b6inQgj6AAYAjfs=,iv:l4SZ2DtWsUdPB0iIDU4DhcP0skKM3NkvJC5NFITRp/c=,tag:hRJXAvcSXGzy/BhK6ItssA==,type:str]",
+		"unencrypted_suffix": "_unencrypted",
+		"version": "3.10.2"
+	}
+}
---
+++ modules/private/amane/users.nix
@@ -0,0 +1,15 @@
+{
+	"data": "ENC[AES256_GCM,data:45rWgBNQf4ld95MYB95msDKqopj2PE9ZXOHmiyIFT0rTS+scisj8HLayUYJO7TpB1Lm66sxKEVvN3ZT8GlbFVrE90Zcp5NZ4RRESba50rnC7E719dyBCHw/FqT09NUD3/msrqLn/5SE7E/3hUQ+xW0UzT6uI1qW0J/3msC/oQ4uAR5k/hI86O1aIqbmRzryLgGWXuelxUFbCm4F3QqFCjSPHmlU9JaWx9ezkyxNKsuvUw4KPrmTOe+FtPTY5Te/za2/y4teTuZCN7/WkD3BS55XeMQo+3XJsI3Gf8dvWiebKBPXtLXnvcBfQHpy9xI+zvmpcSmlHF0W9PAc2M0rluObdIxb9fxCjjlRVROi8Dr48JHeDl5eajQQ6q+6p5RAqoT6cU34c,iv:V9DIHOy3P0bm+bjtWXCC3U30iD0estF9bNzaj3B96/0=,tag:kzJEOSzr/4k61mPTTNoF2w==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSB0UWdVaVdIZjZtQ2kvU3FU\nTUNNMUtHMEtoQUdEVW50Yk5zQk5tZFB1c21vCmF6eHBBZjdTOGM5c05tVmgwRzR0\nRmFxYUI5TlFtQnV3dlV4VCtvNGdSM0kKLS0tIFpkcHR5dCszWllKdkdONjJ6OXM5\nUmlwVlkrR21FdTY2ZEJBOHRrUmRJNUUKMYbGzyZzIJumoIebSGlaqisMRG/RDrM1\nZqNNDDzN5tlucNXyPv6wGa/Mpd/BHEBjxrYuepSz2f7PMbaENUUaMw==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-09-25T02:21:20Z",
+		"mac": "ENC[AES256_GCM,data:MNpWI1/orlvr2aKe/LsGwF28/SXT94vGhoCOAn7O0T4hIHruS665KykBrAyHu/8417iOrc9giVFgJczWA4g6aMhNBszzj1ys3KdTT60LtF0Jb0HMk2wQcM+QU12ef2eX4yJ0lJKw6UsTR2D4J7FV9MJj6bWUZw0+cnDCsQY44yY=,iv:sShWB6fRpDfrtNtruXf4iDALhHplsJYZFlf7Poi9oaQ=,tag:JokqSvA4YeEyFr+0UqC8xg==,type:str]",
+		"unencrypted_suffix": "_unencrypted",
+		"version": "3.10.2"
+	}
+}
