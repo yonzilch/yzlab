587734346c485cecfdcf56597a3201c454339483
yonzilch <github@yonzilch.com>
2025-09-25 19:42:03 +0800

    feat: add st in host mayuri

---
+++ modules/private/mayuri/st.nix
@@ -0,0 +1,15 @@
+{
+	"data": "ENC[AES256_GCM,data:s4OTL5vuNZoJ3ZwlHqOt58GY3oTNXriGMVFoA+ZsJK+NdBotiZZkwdLQDVKFDmwbrPN/C68oMetJOa2h0zQt9puIrfU=,iv:KeaEtVyfKj1vuxve4CLa/aL6ntQUHrIBRvEEl+WQMKE=,tag:Oqh6O7L9iqMRZGrnVD7Iuw==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSByS0RBUklreVFSZ0NDSitK\nWEFJVEp3TTJoZzlHeWUxOW1GdTh2R1RLUjE0CnBNMHFZNkNoUjliR3JkL2tzRHl5\nMnVsNkU1YlNabTQ5bUx4akpISm4ycEkKLS0tIHJIU0V0V1BVaEtETUg2SmtIdkQ3\nVktqTUxzV1p5WmVjS2x3WnovekMrWDAKOCgW6LtLm/AwDRJiKaWhU8VLnbn0t+L8\nA7DZ2SEsy48Wvle3uDXfIn2qhWBf+T0gsB8GTvuim0Z2jnzaDGd3wQ==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-09-25T09:11:49Z",
+		"mac": "ENC[AES256_GCM,data:a+j+ld8qnGVTerJXfmKAj165Dadbt6TGZFQ65HGEHHC4VEx81WPrfXmliaRFFZjIE4hwo2Y2nDZyAMLlu76FM6TH9kGEc1XTEJOgPrOeJc15NtSXc4dAAd41VAqmUSppnOdBLASOAHxlogXLs5B3J2EpzFJJWoyZ5bYZFte+lfU=,iv:7O0e5PUP81vlnE/1Owp/Lu5+O5eNoGnMy9R996221nU=,tag:LAgEZbMYwWr504Fo3cF3Rg==,type:str]",
+		"unencrypted_suffix": "_unencrypted",
+		"version": "3.10.2"
+	}
+}
