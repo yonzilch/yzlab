5c2c8f35b1e8bf67b94cca19f91eb6cb68c05710
yonzilch <github@yonzilch.com>
2025-09-25 08:48:12 +0800

    feat: reinstall host acerola as new host feirisu

---
+++ modules/private/feirisu/network.nix
@@ -0,0 +1,15 @@
+{
+	"data": "ENC[AES256_GCM,data:NxtN7ZJpJw8GhzOZKkGYxD3uXEzEnuO57zsKJUqESEWd14UbCd9QOQmXhGRLz2rFSJUuCNUmRenb9VPTRYUfraOdkC/4FM6GeojOLmajzqcZfWxq7UFJ6YLuvR2p6OEAgj4avkH4ITlv33HrM4DZqu6lpqNdNK5FiD3jksMTWcFA0gzTSb4UgPy/rqbeJAxMiT7MC5rvirSqEOF7pe3fqznurMgzV5J5qjN1nmf7D7ameEcWAwhqhRXYvLd5xtp48PSHYXBAxMHQYSx2mGfBcpalOCmZ5fkxuYiu9c3LCdslqm/RYFblrs6v13WRoxcL5Qoy3/1Ip+rWM1w5PSZxHZ/1FFz9qV8EiwQrnTJbvBtTs5FG1fAlpO1zlb4wi/Zz9Xqpqk0QjsRllmULdsvtZKbhkjn3ueguN5EvLWQ4BL6tx2KmKYdPu9/8TFueGtcOh+4iK74qE9fV3qBdMjQhFnzhS8UM56yZvqKPDrYB65/pVphjmw7VHr1gd27n6dQqcvwqdeJVlhMBC1FA2QftPa1q8DoDuiHGzGcKR1DIWGfTYeqQLQUl3sl3GLf61rZmfWS+qIj5Tm316QPjK2joQsWdlB5bVrzHGiuXmFBbskgteUNy9ROuY6biCR2p5jnB3JtNgHfcK2mrQk3RFpN6RK6Vx5+w3mfb8Os1vCDOtUjX1MNVz4+XjIFmN8FaqaIQRw==,iv:1k78HukqAQrG/2oxjJDqrK7CcM0RLi1+n9Z2kEsC7sI=,tag:T+O5ULFYpaN87koR/SvVdQ==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBlR1lGZFhLYyswZ2paZnZZ\nd09ubWFVNC8xRW12S3c0ZG9INnVVS01tc0VrCjJTV1ZOK0RkemNMRi84bXR6VmRZ\nYTJlTUpsVk4wK1U5UVgzUHBuZ3JHYmsKLS0tIC9QREFwdEloR3RrWXE2TVFrNXB1\nZ1lvb3c0R3BjK09NVjd3Yk9tWGlKQ3cKvekM2CgRC/NbEhxHejwRWhyZOen8fUuh\naUwmC0SBjJTXAUnkyl3Ai4aD4j2O5/ZUKa0fSwvL4U6X5GwRVktipw==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-09-25T00:46:28Z",
+		"mac": "ENC[AES256_GCM,data:/jIuyg66sOWdaJmWWzae8eyMCHwg1Bz0thfvm+9gGGQeTppE7huLWd1lS7NqRvSLKzwtDDS+8U8o4zjICtpcKU4F28N57bQlL9WiQWez3stGH3pdDNM5d/Jw/SRGwXwSpeAa9m4P6HnkeSfhxIAibht9R6+b/e54UGR7uh0gL40=,iv:tK3jxn5HVPDurpIict89MjJERT4GLeTkc7CRQoKpZV0=,tag:l+Si93siN2Gh37N5YzoKog==,type:str]",
+		"unencrypted_suffix": "_unencrypted",
+		"version": "3.10.2"
+	}
+}
