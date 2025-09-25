558295e2a1aa78ee4dd7b243d9b04f23f82ce464
yonzilch <github@yonzilch.com>
2025-09-25 19:54:54 +0800

    feat: reinstall host bocchi

---
+++ hosts/bocchi/default.nix
@@ -0,0 +1,26 @@
+{
+  hostname,
+  lib,
+  ...
+}: let
+  ls = lib.filesystem.listFilesRecursive;
+  primary-disk = "/dev/disk/by-path/virtio-pci-0000:00:0b.0";
+in {
+  imports =
+    [
+      ./disko.nix
+      ./hardware.nix
+      ../../modules/optional/podman.nix
+      ../../modules/optional/terminal-implement.nix
+      ../../modules/optional/zfs.nix
+    ]
+    ++ ls ./modules
+    ++ ls ../../modules/private/${hostname}
+    ++ ls ../../modules/shared;
+
+  boot.loader.limine.biosDevice = primary-disk;
+  disko.devices.disk.main.device = primary-disk;
+  networking.hostId = "d9563534";
+  system.stateVersion = "25.11";
+  zramSwap.enable = true;
+}
---
+++ modules/private/bocchi/users.nix
@@ -0,0 +1,15 @@
+{
+	"data": "ENC[AES256_GCM,data:ZqAv+c8Rr+IP5ACJzXJa0ZFxfWkqapqOuK8fZlVZRv3oyFCHSemP8L2JYU85akkICpNOWIuCNxpAqhtGpYDEFXnBPlvAWCwD25xriaQNaxQPi87w1zacDSgcT47l/lHtQ1GqKPdc/6NaYK399gBYLOuSVxhjQL4FIn9GC0iW7kFu8dDdgJ81ji8JSbUqDosFnJ7DoKKjH2VZ6wHCW1a347kH1ZnUNkTefDJ+SAOeDG6YvmefmytNN4vRtM4m2LD6xVUBTIMNT/A8Sm7HVHrgC62Z2D41aHqz79Yg7oMCAC0oHmYePD08nNmJPJ/G75vTpvXn1/ZH6/NYT3BK+JMJUiYat92Kn8EC4Tp1J+L5AiLruv2xDZ39s/u5BdADjaKsrKTr3Zl9ow==,iv:mG0xI/8oN1zJ5i+UN/7AMPrztG0532/MTKsL7AUX698=,tag:fSHeQPp3idxF2jUjvbnE+g==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSA2bjFIS1pIQzdaQXkzMFN3\neUpKeWdnTDdRZm90SjQzRlJsaEw3WnlDVVE4ClkyaGlWNmlXdUdGYnFuWmEwWlJ5\nYTFtVXg4TW8vQXVvOGdCVlp2WEFmalkKLS0tIE92RkNKQkhXR3BzUUgrbU13dmVw\naXArei90bFMzbDlpRk1Yc2FWVTlueU0KgDpF92TqJRIdfT4GvGVpdrfvS1RNEFkN\nfeOrCRRXa6bn1rxSccL/bJ0y/KKs2tqsSdfnWnZlziDjRSMu5jM/7w==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-09-25T11:52:57Z",
+		"mac": "ENC[AES256_GCM,data:o060wg3/3OulKQaQiDJzuWQfkaui9KtV2oGnLpbh7DBY2tRxx6bvz8ujwTNIl5AMm6K+oDxK+sBjtgMGu+LJAUvsMwRJbi7vGa8ZwUsV/zrDGeroGp9p0hvMj6c7QugMQZl5iFtMXtPRcFct52jNfBgyISrT0RDqQ0DV+f/LrFI=,iv:Nq9K7IpHiAfLtW/MA3t86IFpWFnP/H1LaquAm4KapIg=,tag:p5jnLfpQGnUU+G1Qff4IGQ==,type:str]",
+		"unencrypted_suffix": "_unencrypted",
+		"version": "3.10.2"
+	}
+}
