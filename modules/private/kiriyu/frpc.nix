4b8dd534c60e0dd3b6ecd886489e7f7f0bd674bb
yonzilch <github@yonzilch.com>
2025-09-25 21:03:00 +0800

    feat: redeploy host umaru as new host kiriyu

---
+++ modules/private/kiriyu/frpc.nix
@@ -0,0 +1,15 @@
+{
+	"data": "ENC[AES256_GCM,data:eu8819tQv//XqczRbyjlro5XLx9GI04FzMq3Gb+g5Ui1Yjqz/MysEyoyFwI9nKOsAQVs/Jfu5TZen58LbicMMpvmdC8FwzywYOq7WIvMut3NHCd2bB+haf0155rMpw3r9bawN4TYyU/8QCVS48XQdDim9+NmPkKMEqcvBCr5mDtcA5Fsh9KXyrnxaVVdm8fxp3UohwQPexQBF1c14PVyrJHauwy31y6+tyAvbuTdsQ/oHvlVmONOjwbfQl8vXqjZkjoH2HJ/KdS14aCaH/VZWeV0xbhDD1ULB31vXae5Qje4gkPhpODeGdl/EPUvfYAno56aLExOPfWhPzvd8o4X0Ya0kMwapOXyc3khz0heTsQQE4vO70rP+VH7Oke+jTzeM6bNANQRardUArEYXEzEkZwp9KVnNjT7frB4EISme1MUdcicHT3hWnku/pG94HXNah37FFRoawXtyB91CKzbwQX5XFdtEmMxcnQDBSoiwtI8tA51eaw=,iv:o/ZEUPL1na0kFwJrlCDUOfeqAOYDy04zeqOvcr0q1oE=,tag:da90CydRyUjNkTx0xtVYaA==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBMK3ZUYyt3NytUbVpIVVhk\nbzZ5dmNmUlNiVmg1U2psSlVSSnZhalFYTHpRCjEzUHU2UlpnTklXZkJ1bTRxOHM3\nS3k1c1F2NFVXMVlna1R6OXkxYnlDeVkKLS0tIDRYbVIrUVhtcmhZY3JNMFhnRjJk\nUXR4b2kySW55bWlDUVlkVElIMnBnWWMKyMYsLU5A8oVcy/IUbJ1/Kpr90QoLC1nQ\nAJf5Qsp+j/2BFUmJS97044gmlhdWcoJohGOJpTxaAqoCFPsUiVLIBw==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-09-25T13:02:09Z",
+		"mac": "ENC[AES256_GCM,data:vxKZQlZpTZazt+RVx4Brulu05stViH+rmxxfo/18rmoXoz1HdkJRAm4hC+poZcG6FmUaBU/i1wp+0+g5d/nE3JrYuYYDWL2mXTi0RHquTLKZf4kbLTxyK2HiH74ovb5pdHm6o0pAy3sNdzphRkdQsJXFzbKQD+jmGZsE36fBoxk=,iv:QC39dpogucAyW1jHiB0x2Q2qLcFsaWJ2BOIOiDJbkRU=,tag:A7tIxM9YTPFBfHx+JNr+hQ==,type:str]",
+		"unencrypted_suffix": "_unencrypted",
+		"version": "3.10.2"
+	}
+}
