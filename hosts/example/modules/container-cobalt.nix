770afdda89e9b9eb1496ea740cd8cd3d862e226d
yonzilch <github@yonzilch.com>
2025-10-06 22:45:50 +0800

    feat: add beatbump cobalt dump pogocache in host feirisu

---
+++ hosts/example/modules/container-cobalt.nix
@@ -0,0 +1,15 @@
+_: {
+  virtualisation.oci-containers.containers."cobalt" = {
+    image = "ghcr.io/imputnet/cobalt:latest";
+    environment = {
+      "API_INSTANCE_COUNT" = "1";
+      "API_LISTEN_ADDRESS" = "0.0.0.0";
+      "API_PORT" = "9000";
+      "API_URL" = "https://cobalt.yzlab.eu.org/";
+      # "API_REDIS_URL" = "redis://localhost:6379";
+    };
+    ports = [
+      "127.0.0.1:19000:9000"
+    ];
+  };
+}
