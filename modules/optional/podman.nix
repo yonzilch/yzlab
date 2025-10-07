9c1705a3063fa62d0d054cf8d0220d39aba52e62
yonzilch <github@yonzilch.com>
2025-10-07 16:50:18 +0800

    feat: add ipv6 support in podman

--- modules/optional/podman.nix
+++ modules/optional/podman.nix
@@ -1,4 +1,4 @@
-_: {
+{
   virtualisation = {
     containers = {
       containersConf.settings = {
@@ -7,6 +7,8 @@ _: {
           "45.11.45.11"
           "9.9.9.9"
           "149.112.112.112"
+          "2a0d:2a00:1::2"
+          "2a0d:2a00:2::2"
         ];
       };
       enable = true;
@@ -25,13 +27,18 @@ _: {
       };
       enable = true;
       defaultNetwork.settings = {
-        dns = [
-          "185.222.222.222"
-          "45.11.45.11"
-          "9.9.9.9"
-          "149.112.112.112"
-        ];
         dns_enabled = true;
+        ipv6_enabled = true;
+        subnets = [
+          {
+            subnet = "10.88.0.0/16";
+            gateway = "10.88.0.1";
+          }
+          {
+            subnet = "fd00:db8::/64";
+            gateway = "fd00:db8::1";
+          }
+        ];
       };
       dockerCompat = true;
     };
