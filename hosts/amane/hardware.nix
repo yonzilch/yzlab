636b9aa43f0f378005536d23efe4228d329dfcc0
yonzilch <github@yonzilch.com>
2025-10-11 17:51:59 +0800

    feat: replace a new host as amane

--- hosts/amane/hardware.nix
+++ hosts/amane/hardware.nix
@@ -8,12 +8,10 @@
   ];
 
   boot.initrd.availableKernelModules = [
-    "ahci"
+    "ata_piix"
+    "uhci_hcd"
     "virtio_pci"
-    "virtio_scsi"
-    "xhci_pci"
-    "sd_mod"
-    "sr_mod"
+    "virtio_blk"
   ];
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-intel"];
