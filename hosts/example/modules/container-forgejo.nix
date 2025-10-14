241bd3abce2b95e2b1474839f23983f5ce595b15
yonzilch <github@yonzilch.com>
2025-10-14 22:07:38 +0800

    style: format code

--- hosts/example/modules/container-forgejo.nix
+++ hosts/example/modules/container-forgejo.nix
@@ -2,11 +2,11 @@ _: {
   virtualisation.oci-containers.containers."forgejo" = {
     image = "codeberg.org/forgejo/forgejo:12";
     # environment = { # see https://forgejo.org/docs/latest/admin/config-cheat-sheet
-      # "FORGEJO__database__DB_TYPE" = "postgres";
-      # "FORGEJO__database__HOST" = "postgres:5432";
-      # "FORGEJO__database__NAME" = "forgejo";
-      # "FORGEJO__database__USER" = "forgejo";
-      # "FORGEJO__database__PASSWD" = "xxxxxx";
+    # "FORGEJO__database__DB_TYPE" = "postgres";
+    # "FORGEJO__database__HOST" = "postgres:5432";
+    # "FORGEJO__database__NAME" = "forgejo";
+    # "FORGEJO__database__USER" = "forgejo";
+    # "FORGEJO__database__PASSWD" = "xxxxxx";
     # };
     ports = [
       "127.0.0.1:43000:3000"
