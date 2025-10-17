6d7e17a76b79bd8ecc8b64111cb31206d799f47a
yonzilch <github@yonzilch.com>
2025-10-17 14:42:23 +0800

    feat: update komari-agent version to v1.1.12

--- pkgs/komari-agent/default.nix
+++ pkgs/komari-agent/default.nix
@@ -6,10 +6,10 @@
 }:
 stdenvNoCC.mkDerivation {
   pname = "komari-agent";
-  version = "1.0.83";
+  version = "1.1.12";
   src = fetchurl {
-    url = "https://github.com/komari-monitor/komari-agent/releases/download/1.0.83/komari-agent-linux-amd64";
-    sha256 = "sha256-v+5wewXrHYiooeGllkWfiau0tv7xZOOMDa2vlwXWvSs=";
+    url = "https://github.com/komari-monitor/komari-agent/releases/download/1.1.12/komari-agent-linux-amd64";
+    sha256 = "sha256-ykIJPFf5duRtDYX9UfS3iatUH+xRMSyo1zT2TGe1sbc=";
   };
   dontUnpack = true;
   installPhase = ''
@@ -24,6 +24,6 @@ stdenvNoCC.mkDerivation {
     homepage = "https://github.com/komari-monitor/komari-agent";
     license = licenses.free;
     platforms = ["x86_64-linux"];
-    maintainers = [yonzilch];
+    maintainers = [maintainer.yonzilch];
   };
 }
