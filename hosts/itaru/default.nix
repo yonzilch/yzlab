42ec7d2a6c1a76aa4425439ff6ddbae1bf3703d7
yonzilch <github@yonzilch.com>
2025-10-20 22:56:13 +0800

    feat: add mcsm in host itaru

--- hosts/itaru/default.nix
+++ hosts/itaru/default.nix
@@ -11,6 +11,7 @@ in {
       ./disko.nix
       ./hardware.nix
       ../../modules/optional/komari-agent.nix
+      ../../modules/optional/podman.nix
       ../../modules/optional/terminal-implement.nix
       ../../modules/optional/zfs.nix
     ]
