43f5273ba3bc39abfe422632f05f7c5646250e99
yonzilch <github@yonzilch.com>
2025-10-21 22:31:12 +0800

    chore: mv containers from host mayuri to amane

---
+++ modules/private/amane/container-rustpad.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:tWW2e4SsJP6MA2OMOw297vbCtYl+UBIm8diuovUFx0TBtAu1VSGjFFzo038na4Oz4gLqRViECMvOBBl5szvVgfE6Lrn3ReNTitPmDxuKKvMMHiPBOvyo1esS2Etw9zo3ZlCemS3B0TlYH+QCuuMHKlo7IJ/hufWS6+H1IZIVMZfYsalybOYk5f2hwjIogVA+WxDylxzsjDMaZo8ppm2uNcoTp4tLBLY3iwYLMoY4pEfB+l/776iCyPFafE/SAPt2vokg0Qdqevy6SPmx/uwMVynELFQtbLSC6ibMVBj60ifi4hVjVuPBd4L2ofPTkwrMXbF/5NVWvllekdEttF31Yper/u7E9ZXlEakbobd8en0+bd3Uwp2Vke30c9/MSdSLrr2X1NX97GgkF73fip9qwO7xbyHTxPHy,iv:HmnwVkv0PdT2TMG7qUXyNy89Zg5b4sEZ4jL2kZ7EU6Y=,tag:IDcDnPsN1MdinozOnl6TdQ==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBuSG5XMjUwVkU5cFl0YU1B\nUldGMXBzR1hvdFZ2M1RJclMvbUUwemRieVdZCmU0bldYdDBnUUxlZlJiaUNtMnhQ\nRVdKbTRQZlFBMUtMOHhpaXdZZFR3N2cKLS0tIFc0cjRBUGQ4OG1FU2JPMTlubVR4\nRmtuak8wTkorbXA2UnREZmY3T0k5MVkKUVnl5syKNOznat8r5Ztj1BsQxyhwmD4P\n3M2muo6/PQ2xRsosRoOUseArVzktiTj52P07MDEt64ZDjK/K/oedEQ==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-10-21T14:00:58Z",
+		"mac": "ENC[AES256_GCM,data:ZrPXzH53RrfKpIcE3WFXvhAqkEi+2U/GGw4U9B3PQSwBKQuVNaZiuEDK0teDvxosVi28fYKlV+a+3BBACg3iMYx0WC6aYp3c9aPtJU2+AyIZMk4eliTdOrZXopgDtSWgguENT7hNrkhk2NTAF+gHUdZYfC+P46yarSTnBz09m0k=,iv:6WddUmUvbEEzeA9PXBg3BuNlqHeGKCCvplNjv8FonLk=,tag:SLk7a7v5jaV0YAGW4WrXNQ==,type:str]",
+		"version": "3.11.0"
+	}
+}
