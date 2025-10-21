7d773d663f58cc306fa207f0ed42d4f77e98bb1c
yonzilch <github@yonzilch.com>
2025-10-21 22:31:12 +0800

    chore: mv containers from host mayuri to amane

---
+++ modules/private/amane/container-dumb.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:DGhhoD1gkvWRRlvYXb7TZbcjajIi4FkFBjFevcsjbltD9XespcjhJ6cjd9lCPPsoP/4IXWMqBHYwb3EW7AtzBC0+wrWiI/rtstk2t/eRI9HfIqnqJP0yh7DloL+Nn0/1fy04fI9XQczAk+UrxborRsOd9hS6T9S1/w3fdr+B/0/D0ii5MVDEUJEE3PNIVw4UDt4od45wUKtkHvL7ztZSgk8y,iv:PPS0Kn2eI2kZO3bvdOhCjjWQ/IvF5qetSXUtFSrqmDI=,tag:kqPTM+xM8PQy1+co5c8g6A==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBvZzhHbFY1K0RqcExnaEdB\ncHBTRnY4R3grMjRxWjB4cmdFQlJ3a3VZQ3dnCllDSlFienoxMVBraGdTREtaSkF4\nemdWK2g5MUhOb3hzL0g4SjlueU9IeVUKLS0tIFkvTWJwTFd2SFlLbFcrSERPcGZG\nRExQMkFFblNXOFhOZzVMb0xKMFJaaW8K5XHrBLTH5F+jNeuGzjUuhmC6UUOv9Cv3\nolYMiuaqz1TmIwYjWGl05MLL3o+lE/v8/eBiB8iCE30vYP1n41dZrA==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-10-21T14:00:58Z",
+		"mac": "ENC[AES256_GCM,data:iKL8UI1thh2zABrNllpbODThsc7WzV3o1pUZ1V+bUNRhTO5F9qNkxX5XNhYXDzkY1fPBBC9HogfVJknP3lnimsk/T6yMqRDO6/zlibEhh2Fvhmjg65BREsBY6iGvrxkhOxkg9BiuE+6yVCHTaNCkVmjIg6qg6bNINbWV7J3nxSU=,iv:9Yg61R0E5siMaObLuJGH5hJpY0iGKsbEJ2rU99AUDJc=,tag:b8TrBmwZ14Na9aasG1rYaA==,type:str]",
+		"version": "3.11.0"
+	}
+}
---
+++ modules/private/amane/container-searxng.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:RP5xgO+cRuBOC7TbkvmGFLX5edrt2YuUSxKjUE6Q/N6qYI8pjEu99DaqsaJrqG0HezIVBg7EHnZrdgV0c7WTfcM4+dHawj6/2fyOWSwB6GQHxghjn50k87WrHLei0MpMvTfPXldmdVT4qlMXlb5iRmQtK7s93nRBaOcXYpScFNW0dRO9bNK/YJzqDOIjRhjh1GydgaXdyUPE1gkZ+HZiJ2LJZjb6y4lrJlCbtL9k+a8NQUonGtuR/NtX/4DfLPQ2js1gPtxgXuxL5IT84YtVrdOR6UYinHk2EqYEeoSI0mfNNIPsWLXbDI0X13vJH67V8YYyh6qn/AZFah4Uqbs9d07+ES2tzKmc9d+v4wNoqB3iWY1RzhrilgWRAMIhH3Jh8z4=,iv:aGgnOduVsEE3Vnc0yl9y/a1R/94S3j+NBRbEnzAw5BM=,tag:iwmfsEQbqlz+YlBAB2q4AQ==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSArcEdIVU5vMEgzQVlBRWJp\nQnZTTW5kejlCVDE4cy8xdVFlbVVrMkx0djNJCk50SVMyTE5zNDhBdEZENi9JYVlK\nK3g5NTRsazUwQUR4SVRBWFU1cXo1ZjQKLS0tIFppSUlNcGk2YzhNNVVlODF6blVv\nVFdxdmtjZnVRM1FzM0dhR0h0RW9xcU0KlIznQvRMOGzCodUKc1oOdX7WMLQh2X2S\ntnfjjIBC+7xzertUkiPwwIrdjJivKeX7ELKRVdEKX00Ytqh45iBo2A==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-10-21T14:00:58Z",
+		"mac": "ENC[AES256_GCM,data:k8e79BSdk0qI3LYIMHdJRHNc0I1a4Uu4ywL1tx7X5AVLJW5m2X8BbMvAfLjRpy7EoZMoUPKzsmsHQzAexsyZ8FKefV8xLcAGlsBJeSuiKmSlWbD6EkuwO7kuPKbv7W+00Iv9KAPL4YjvlTPVISzq+oZoRAxoY8UEx4R8x9bt1NA=,iv:nd6I7WNR1KqS1Yk+fixez+DksFyaf9Cc/2Wcte/JFkk=,tag:fusHKyEqUkcL/wt7nDZfrA==,type:str]",
+		"version": "3.11.0"
+	}
+}
