523edcb468d8530ed73e2de484c21ed41b2e71ff
yonzilch <github@yonzilch.com>
2025-10-26 13:58:03 +0800

    chore: mv fusion homebox readeck from host amane to christina

---
+++ modules/private/christina/container-fusion.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:fnhnPk6DBZV0zlWHAyecI7Ic3/+fYFUAWksOjEjyf+MHZosopPddHXcyymmfozFpA6pofORCtuOCeQ7Sev2BjCHXe9BC+hotFp7n0BCBzpoVFKhnBzsmv9rFZ6UhvX3dOeTKLzOqptjLO/SsJdDANMaGrjhO0xnQAmnsGz4U34+0ZdIHX4v75i5ZJPrSvQOn/O7pDBPnYj2M9hOOuzTliEVT6iHurv353Hr1SZf32TD4jfjA6LaSVqfZQauVR/ntcuk/Dzo6jstShKopdj01Jc8mzOWR4ucgS7jVpoYJrjl4ut1WxoHGzsF4JaHtkop8RlY74GPms/BkoY+9xhAglbyyVpGaRLA=,iv:fjbIYMn26S0k7gQRc+4WtxoG7Y6O96DkPqB542MmZQM=,tag:Cftq4yJe+dXWF6gdcYcx3A==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBvWVdNK1VBWUV2ZE1DelFh\nVG1yclhTYXBTUEswSzdDU2RmVlJOZFhhOFR3CkxqeTRaVTVwWjRYK2sxKytIY0dK\ncmY4Y0YwSFVaZHF2VW5ESWV4dnFKRGsKLS0tIEdTSk5MMVA5UGJFMEI4NjFXSFdz\nb2pMc3hqQXdoSlRmeTVQc3VOZkdIMWsKGb4gKTdMWhS+ZTJo7UljWAKg2o5HzE1m\nl/ofd4qFzmsGhO9MYdSJLjmRdreFMwDhtW5OxFrSrzymJv7Ney6Zvw==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-10-26T05:57:19Z",
+		"mac": "ENC[AES256_GCM,data:lFxA7mX3Ncxl4CazOgb57dKB8zKQ7YaNq1nT33QNd02zHlCzdFPT9fEPk/jAhknxEzdCYWghqv2aH+5nR0PJI7ayUz0npzlJg2F7dvQL3v7u4v4k2qxPrbaoewjj7yRhL1FL9YkAEV3DcMaeBX0yQ3mme+3sAH1gZ08haI0K2Zk=,iv:+AeLmIcGB2tJcxzICBuU4FLiiO/WYsW+EeVZgFxoNzw=,tag:6hR7MMaG6WbwBRZCMB21Dw==,type:str]",
+		"version": "3.11.0"
+	}
+}
