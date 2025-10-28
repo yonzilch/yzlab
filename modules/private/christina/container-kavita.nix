35273b34c4662a36fae3bee1032d49fcbc745e51
yonzilch <github@yonzilch.com>
2025-10-28 16:02:49 +0800

    feat: add container-kavita in host christina

---
+++ modules/private/christina/container-kavita.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:X0RL5pFA6zLIgf4VqjGs+6PvM7wBvAC3+9zukvRqGNJThksdH3nstJBVfMOYt4VoCcIWVN3GncVgYNEQzJQpIWfBSGoaZwL6txeGONSW/ny2NntNuOWB5O2G0l/qc7Kv9p0eG/tej0rkqgzLQT8FmsoW6TTuy/1X7YdRvZCFf8tEXgArqQGM/RMrCETM5cJGy9CNFf3FdtVlXOQILPpEvmVLM3WepGhTLcRSsu07hPIo8c94dGwgmUikxFgR/LXXkQ+gqEzHGs/Z1Izh6ShkvZsGG3KEeDqJMqqOxVx3RY26kTgCFgmCWdpVg2XqKEtYIdJnJq++LazLfNxvjUZQKo+3slMown0NDuadtEbYZdAsJ/s4i1s=,iv:LC8vGokLj+SkVqegXQl+4dPDPXlhEALZKtmhMZX62yQ=,tag:R1K2bMJBcARnDD3D/RbscA==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBTQ0FISDFUVkdGR1FlUnJ6\namR4T1JWQUlNbEZmYy83VHFQbDRHMkZJODB3Ci9jSHNWVHVaMi9XWXl4b29kVGFE\na0l0TXo4dDdsZFJESktiaG5Mek9Sa2MKLS0tIEF6QVR0bDgrdlpOMEtCR3Z5MmJL\nWVZQdktaT1RKUi9jSG9WbGllampOUVUKb6fAW8FNNwhJYr9HHD0mv4y5wKw5Xw9p\n3jDSogTuJqBfRF0yizba56ekfQEt3WuPGmy86hnjslNXUDRqvLfr0w==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-10-28T08:02:20Z",
+		"mac": "ENC[AES256_GCM,data:eCZqcDCB/kowKjlQoGfon/h1rNHHpfeFqlXCpm1zpe3QBiFLhMlNni5RbDaGxV5499p6Iud3dmeeBod065YmnCUZzVBlONrz8yg7L7nEbdHRTKyZupclTFIFPTyzMIknCO5Zc3QYBXfqSAk/o0Sc8WPD6fOtrIuxClxaingCFS4=,iv:NSDPVhNDAYWxsEj+z3bUky6nU0sXMOYCshV9YxojsZ8=,tag:aHP1RxaXFWu4wPNnbQRFPQ==,type:str]",
+		"version": "3.11.0"
+	}
+}
