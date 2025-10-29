81c1e34dfc7e58ef4a35bec041f935164a75440d
yonzilch <github@yonzilch.com>
2025-10-29 22:38:12 +0800

    feat: add polaris in host amane

---
+++ modules/private/amane/polaris.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:6DwMFpLj9/GOiHm88FILBkRGjWc0lF6iVRCSBLsIvVbwJqYcYPKAOjgxQh0OyHIibMyFF5y2AZZvhbVs1PZSmDDafjSiKxpWYiCyoh2sH8/zIjhP3XO98X1Dz2P2wOYfhvI5pVAV4DmIoKkAoS2vleORWmd21KBsH1BFhCXbQ0bpKqCCMotGPNN7hDTvnzp8BhT5XAIs7Mq1mNZa7rea/TA68lwd1rMHmZZYU4sQNjMfTDhhGYgs5sbaJHR3Sx278YbhwwXj79Pne6WG5+VEbdGaO+BUYvyQPN8hbz9xE/Ykpy7BMNRMdXcYSc4wYaHBRHqlydmGl0URPtd+ntVi4jmC9c7FlDnLXdkZh26GqxCZw6Erhof2DVLuysMSVg2dqxsJwpKZ8bCsJGoPHrA22r0erekB5dWjRSS3xA2qe2fucYh7nLHuVWNz9mS0FMoXLhMqAA9nhGf7Z1uwaN355/IEufelJyueKylViTzdbmcQJPcAuKEvJSP3c++OAj+8mdFDErTOWg3mzfs7aIU=,iv:b+Skr9EV2qsHs9FLGIHfxI2o8seGk64sQnuc05mH8bI=,tag:PiGoU1p7nUkUiesOE/0zhA==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBXZjRDTTdIeFlYS1p6TUU2\nbnNXY3ZMZmgxZ0F4cUVYMEVQTzdkd3hhbEFFCmx0U09kdklPSzJxV3ZpaHZaTncx\neGttajlLRER1QzVEeW5DNDJ5cnNRczQKLS0tIGVUZmlJcWRsMTRGZnp4Q0NCU2Jl\nUitmQWo3U2xjdjdoRTZVQkdBTzR3MFkKZ91YLdhtQ65lhiJPIDKDIgvgNK/Vec0V\n3HJqBwnU+Xhy+YKJmgdUtxX/iOdulJmb7CJp52DgEln1FSbSpTfPCg==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-10-29T14:36:43Z",
+		"mac": "ENC[AES256_GCM,data:c0KFspy0cjRuKKDpSRLNuo9P+sPcif/u+Wx+g7EBBvd2vfDti/CQNQdCzfuTTBe9/ng6TdzJrH6Sd5tN4hm4woVPgBhIpCNA27f7/ydUCIvZabXvoWeFZ07SG23IDZzfzBKRryiAMSXN253D58jdh2UCV6LKgiA3uj1weFUNPOM=,iv:lJhM/H8b92do05rgimulVzwjwwgcgkJbJ6n5c6HKfIA=,tag:0w08tV9RFsPG3LGvxCMjCw==,type:str]",
+		"version": "3.11.0"
+	}
+}
