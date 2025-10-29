19f15467a9c76812510a6f1f3459e247425a1eea
yonzilch <github@yonzilch.com>
2025-10-29 10:29:26 +0800

    feat: add restic-server in host rintarou

---
+++ modules/private/rintarou/restic-server.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:Wq6eYEc61p0bQlg4PpzI309rzvnD8bmn75juuU4IuPoPDVIZll1V8ShiyD3xpRAYu3ruz55R2zJ1mAyCB5zBoJKELf/1vl1xJo7Cn86MWT9Nucx5GwqvFN0Cfic2JOSPXkGrtsJH7wHRFPdGZdm2G1UR4tHJxhFwLQt3hmnnNZJgj+RlPYfA7tOqe/djWSeZnoyjPY3gZQq8cOszJezRhFB8M5/dNi/HGVQMtj2fHleXNJGBhpstdK69d0Vdj6UqQMczs5AjdaV21Y3JoC2Xq35FYslsdoannjpC1782uzlBpxoR8UOOnPXz4aHJmbeusQBXHI5SaQ==,iv:bgGDWMXUvenHf5n+NLTg1RcuJXcmyzczHihwe9Sxioc=,tag:/sCMKLTOqo160NV5l0DTHA==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSAvVVkyVUVSMVZkS08vakxr\nK0lSdEtwcDB4VG02cnVsaXd0OE5taXh3MGdRCldoa2Jlby9qMW9LM0xZNHNWaHhQ\nWVBvOENoQXpVREo1cUdOMDRYMjM4dkEKLS0tIG1CVklsRWJKVGlUSmdqY1ZrTFdB\nc1R5enA4QytURHI3dmdwR3NjU3c3cDAKSYIlKiuhTO3bUFXiQ79FR7Els45GnGfx\nfJgmq5hvqJDGEotyKsVizJRYET+3jP7bXNDMxOGg2BWVwvco+Yanfg==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-10-29T02:29:07Z",
+		"mac": "ENC[AES256_GCM,data:gqcBNh/XHh3XGNeVWGisF+mQDQ9QKJp7Pt/Egchk6T4zWAdT4QsMsPXuIvWhQ/qWWss2VwjDklm7T+ZIV0Sw4PPzWNi1BSE4KFwAPF/QjrFGieEgD10Qpbre/ezmzbqbf95CaxAcbDPhJERmQ2S0/Dsv5CVglCB3+iiUNrmBNbM=,iv:sBjmtIT9KNbvCjBDwKlLjIhrzMUorNcUkV+rhsb5NGw=,tag:H27uiGH9DAjxqU4NRl5WdA==,type:str]",
+		"version": "3.11.0"
+	}
+}
