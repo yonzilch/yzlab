f6053dd763fbb55e4e2037e42017000bc577d630
yonzilch <github@yonzilch.com>
2025-10-30 19:27:55 +0800

    feat: add container-omni-tools in host amane

---
+++ modules/private/amane/container-omni-tools.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:igq1E1BGqgI62S9uxJh0041WaWYb8eNxryHCNNWG7QH9Rwd5Zsw907UUni1b0G5u2dY687ObMPeWrJd9H565hg2zZyhumhL1QTAWc2KGqRW+aXPGXAZ2zStATeZcwSPSiiWmYqiDlnpa+l9GXBqQQVvRUU4QbnObpKVXgqWsKSjcB8brT+GIABqLqPNP1hyK3OiYhaGVdZzi2UOZnDbTLaE=,iv:cZJFa2/nbEA1CuDij5rNsrCz8GakteaYElK8xW6e2NY=,tag:nE6aKcPfkNJE5cRN2dE8HQ==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSA3djhkbmZqQm1IeHlvdmcv\nV0YvZFd2c0JmUE45dys4c3czYjJJVy9VN2ljCmR1NDlBSDQ4dHU2cDEybk1ieEFP\nVXRJTVFOc3oxRXp3YklHcnc1VnNwRWsKLS0tIHNnZXljTUt6VldCd2NqNGR0Nit3\nTEdSck5CWUplTmx3a3NWR0IzNVJ3dkUKBgUMkilXruPtSNbGpLV4k8WXZTm0Q7fV\nyVNCky52O/tktONJQ/r74ySbs+T1oVJoegjZDBW7lNTi8+KTZrc0Ig==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-10-30T11:27:35Z",
+		"mac": "ENC[AES256_GCM,data:jJS6JpFeSbj/Sg21QxDMoblG1xnzuarmOZseOG8QtX3CINKvfOd8LRncl5svenCPssqp5WmlkcMoPPOUfvdPKfUTxXVe5sY6Xj17BITH3FqGcTSszvDaGUHL97/Mycb2xQ+fGhKGvZHk/oQEZBI6q3gIiW3Lge6aZJtOEyQQvww=,iv:CzDpAekYBv4Wb6x5PuKuuUOgc1l28IfyUl1PnhTSjCA=,tag:uOWg6+d1lF9DpB0f6jnoAQ==,type:str]",
+		"version": "3.11.0"
+	}
+}
