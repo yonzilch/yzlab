740c1697a320daa267d8e863055cac50e6bc34bf
yonzilch <github@yonzilch.com>
2025-10-30 19:47:35 +0800

    feat: add container-convertx in host amane

---
+++ modules/private/amane/container-convertx.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:bQTe4DBdwDJNPczZLjwmgtuG5g+5FxQEKlEg7Tod7OiZeREnzO5yA7fS4sU5AjMGZemzM5/QOoW0fAMbT3f2Aa00PctlxM1+iZ8PvzVHrJF6JXCxlAbngUl7sfYRe6D9DntkSIENix4mD0q6GCUh7Q0nQuekCGUedTf/ZVscz7W7Sm5QmBan3hSQ5ZZMilxTBjIB61rteBZyvUwwZSQ01LDpvDNqkfhwfTPULLGxhk9dHV+LucwFr6wWY1wDxMVJ8bJwc/bRgIScYZheevq2KDCAZe+WAqB/6spcVg==,iv:QdzO3itYKJXcXjgc4sY4HkiqQPucA7hs/PRlLn7El28=,tag:vrE/pE96BZar180mOEYX+g==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBzTmdFT3lITDBNWTh0ZHNY\nNDhLV2l3dmJvWlJXVXZ2WDhpSktkWDM5THlnClE0djhLSVhwZzQ3SDdHd3ZMeWFk\nU05iOWJpV0ZuVU5QMHdVSXJjYkdCTGcKLS0tIEtFdTdmQ0EvUWhodWlaNW9aRGxx\nbW8yUFkzQjdTRTR6czE5bHNFUUlmMW8KGH8tNEW/J6Rqp0qUyf0rAdFIPiCd/uxr\nIEZrDy9d6S/2mZsrDcu/EL+Oc1/+4P4NGz5sSzsk65Ft2W08ZG8gOw==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-10-30T11:45:59Z",
+		"mac": "ENC[AES256_GCM,data:0Ki448ajK7y6NrpVnjgPSG8bqdCSAapxOINCCXkkGrmEidlySqkeRI1veod4/ty6pYAb36Cv147eGVVMzsqfh9CIjEU1oeyAbkaYhDPlpnu00SLD3kHZoAgihf9KUKXglbfxfqd5KGHo7QIML+gBRU/DAPVeWds54Q7rlTVOGnA=,iv:hqKu8uR7Dmr/hv/TK2UhVnpMIVbZFLlmpb9I00m9+s4=,tag:Jsw9u95E4FGO+gLEd0R6HQ==,type:str]",
+		"version": "3.11.0"
+	}
+}
