24d296ed13941cd7474a816cc1690803eb7d844e
yonzilch <github@yonzilch.com>
2025-11-01 22:51:24 +0800

    feat: add container-audiobookshelf in host christina

---
+++ modules/private/christina/container-audiobookshelf.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:2MolhPTrI4MtGyhiAMQVtXmdldG3VQQe0KHabpZGnoDj1myHyX37zXKsjiz+flAzSmxfCI3vSK2e0IKPdqnQvjEC+VbafqoVkW588dOUZjwHpYszjxhSTsVfXY3aHDqxmztk3ouDwiZdurxM1x81qoY58Wu2dWQZXNYvcG8CGfHaI7DCkgk4m5/ITuaPDp95NABIJcEwHy/ljEFiCE3GsIHwEQI1SgFZhtLM3wv7emDNVw9Sv0JSlhNGCJKVl+eb+kl1o1zLnjsM1/odLTMyjr2/hkM5Ph+KmaU2YH6HOvegRIVfo2uyXGTAmm6Io/4l8fPYQvZ3a0ufFF7HV0uUY6hfleDGeP0MTRRe6G+So5damwat0QccMnpUUsPD2FSPps4pVVGTFHO0zmGkcxFzEaJQcFsKO2r4sYBOfV6Ndrmhe/WPyhVBwyCNknJ9bc+MSH+9HMwKEhJR/xfMtM1ZHLDbr/B76HNC2rY4gaO4ubuNNRur1UjunkPbxX4=,iv:8uieN8nd46zxjpTuYQ7N9UPe499gYTrxn9CSOyJw628=,tag:0nWvdufjcTl5cFj+F4uhtQ==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBHZm1Jb2laTHRTdEhubUhN\nNE5BMmpxQWNVRWhCUUI4VGx6M0VPN1ZkaEdFCklvbjVISFd4YnJia29mYkRyaTFE\nSWdTR3J5RnlxS3J3Y3dlWmZBaHg5bDAKLS0tIGtUd1hUU01OWW5JZ2FuTGhndFJx\naGJGbmRKUERVaFY0Yzh6eklmNWNZcWcK7Tx7PvGKLp7bK1w31QiJPbUvh7NK6JGk\ne4HWWMccd6xKziW2bvG4V7oNXz/9eFnFVxVkhCnfhLwyr+ZGpat0ZQ==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-11-01T14:51:01Z",
+		"mac": "ENC[AES256_GCM,data:tLeg8Meb7ftt4MAk1vqmE7vbB/RSV3+++0xfe7QAeBu6ISrZU3UsOCP21z7KKHCa18X9IfwdFPn2lVAsC0js/+W6xcnYN3BPsn4b7QehnOlJK5DsAsMyqld8Se672X/vg7/JyOkND+SgUUgiKeJvDs4I8vvqO6XVj96pt1qhwg8=,iv:p1DxzQwkKAnNAxZpkfKd8N24/8dzXxzRET/+q8gC9eM=,tag:Z95rzWnclSN2JGW0p54s2g==,type:str]",
+		"version": "3.11.0"
+	}
+}
