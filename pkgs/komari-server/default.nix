518ca6fc2742f14068b69a38c7068b0ed1c9954b
yonzilch <github@yonzilch.com>
2025-11-01 18:16:01 +0800

    feat: update komari-agent to v1.1.31 and komari-server to v1.1.3

--- pkgs/komari-server/default.nix
+++ pkgs/komari-server/default.nix
@@ -6,10 +6,10 @@
 }:
 stdenvNoCC.mkDerivation {
   pname = "komari-server";
-  version = "1.1.2a";
+  version = "1.1.3";
   src = fetchurl {
-    url = "https://github.com/komari-monitor/komari/releases/download/1.1.2a/komari-linux-amd64";
-    sha256 = "sha256-C5XQQP8mziHqPCIu7A4SMWJkxRhp+6z+F38dxZLVjmw=";
+    url = "https://github.com/komari-monitor/komari/releases/download/1.1.3/komari-linux-amd64";
+    sha256 = "sha256-ZISq9sjJ2zXzSR5GjEC8iMR26DaN9IyAN+1sJzvT2oA=";
   };
   dontUnpack = true;
   installPhase = ''
@@ -23,7 +23,7 @@ stdenvNoCC.mkDerivation {
     description = "A simple server monitor tool.";
     homepage = "https://github.com/komari-monitor/komari";
     license = licenses.free;
-    platforms = ["x86_64-linux"];
-    maintainers = [maintainer.yonzilch];
+    platforms = [ "x86_64-linux" ];
+    maintainers = [ maintainer.yonzilch ];
   };
 }
