770af9fb08f5003b83482c285084eb3c3bc0fe09
yonzilch <github@yonzilch.com>
2025-11-05 22:37:18 +0800

    feat: add container-komga in host christina

---
+++ modules/private/christina/container-komga.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:11jV1mw94qc0v7en7g7aimHC4NReeqsUENdfoPIG4+Mv/GfFl1KGvjubzR6dBBeCk3PDKoH4Es9PNmuv+oKn6gMay6ambNszwsgunl/ewUesD7X8MPkphDku64r+DwJp5VaGxaxoLLeLqeNLx3FnpLVqI8GdD0swmBZYWVq7Et9wbWrxESTLkE8ScI5T3Xlk3k+xkL7gpGJ8RJvq4+7HGDyW3hRiyV1AHe4EXJsnU5xJ3XBeEyAfJcSc2sDqD8JMMxHDfTph3brUlYw9FxJh9CdbPG5sxJ5K4gt9mgfJH0969SSN8i96cb+X60KXm0L9atpcLy8L1XlX1BVHrtw8HBONRUzdty7QmQM3ZWKQUPrMDqnad6Qz64G7x1htIQ4dDZtnWEkFd0RHfgE=,iv:tUxEuWrG6Q6sZ3ZduOi4KXir4S8SomiY6bUZxikhWv8=,tag:2vMSdkT9r7s51bIy6lrj6g==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBId0JwM0lQNjFsa2RJRnhO\nVVdhaEkzWVp2aWIweWtuc1pnRldjekU2ckFRClgyNFZoaDBGelB2alpFeCtIaFJs\nU1lDS1ZGTm50R0Q5ZDV3WlQ0Ymtqd0EKLS0tIC9DajVRVjZQNVA5bDFoUHJxeXh1\nSyt2WVlFYUxuMW5LVHVuVHg0QUJxVDAKou6FqNGDujoCe9+8azeMHsfpGA1AlHKG\nDNR/JdyB2oAt+V4jkxR+H7U9tZPPv3/RkIOnbuhy3jJE1KvX9DGaCw==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-11-05T14:36:56Z",
+		"mac": "ENC[AES256_GCM,data:fuep2gS3xkU1v9B3YGftouwjhGWL6HU2dnvCqzWK5QPhob0dbr88JonwOy2VMUNRyCkwEQpB3UMjlUNActpGzpGU3FhicDol58GJdZ02HzGLTG6NVkt0thrRWgmXa3zQBLzfmzgjZm4ntJ4u1MfqkSitXkfmgT7fBv3Px+LfNGw=,iv:JYXPKi8NYg0rDc0X7nIZANzLX6q9J9ZT7PTQl7y7GbM=,tag:O6ygdM8YOy0Q7T01y2neBQ==,type:str]",
+		"version": "3.11.0"
+	}
+}
