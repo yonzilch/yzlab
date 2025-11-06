f30c33e4e6e8bd408540b0656a788ca19d7cc008
yonzilch <github@yonzilch.com>
2025-11-06 22:53:05 +0800

    feat: add container-trillium and container-convertx in host christina

---
+++ modules/private/christina/container-trillium.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:ZyCE0vkXFm+YMpyDr6SX3DdRJRtckAhxT/k6ZNZNLjUwTTeuCbetTr7eSqBVeXn9KwPqw7iinmobXYMjXp/f7CVYnrAARWSqOTt7Fx3y1UaVvLy4eP06yqlPUbhdvFMDWuQQVkyjwWAnD5A5Rc0FGo1xF3OednPuFs3Y5FhKRJanfkGukweunB+uFE9FfQQO6Hil0gO0JToBi4I/31zz3pmGdc3McdoGRwwMeigtkTcyqhm1P0/gIC4XDhbHC6pls07gDQBbEZxv+bQgPRHv/483HidFqqARme+wgb7aLa2YrXo=,iv:rSlDmwQ9O5vSxGcF8FF2dEqmA8cFMDjqLYxvx4BD9O4=,tag:HG6qKm2amtIuRVqW8rDTWw==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBqajRBL3hmaXBvWHFMRWJK\nektZNlVrcHdlak53dE9HVnEyek1QaHFMcmlJCndrZks4MFRSNGZqWXJwRFJIV1ZQ\nU0o4aGFRNjhzMC9xckJzeDllZG5yREEKLS0tIC9GMkc0UHM4RHVJSjd1cW5XaE9p\nWDhhcDhYazFTNlQ4TjdzQnZyakJEMTgKqE+O63E/ff47bCbd203Z9kNtd9Xp1HGx\nk7vINNXxjGDQCLakgfNEheTmbSq+h1NOLo/Cl8EWdvLMeieCraJFEA==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-11-06T14:52:08Z",
+		"mac": "ENC[AES256_GCM,data:1dFAra7IUh6YSgiNAOlrLkYmAQ72vXOQXLb5r0fWud8sOFiUlOc/iuH51QeQsCtTXWthfKVSubseM3WXTfuUijY9BwpkiLgiHGTxvNpjMTZjb96G8OfreLVXrLIDnOXDqx4VaB/usZ8yzFl85bdCVlKYywp6DeTqMEfIDBKbIHo=,iv:J3uC5/eILD/Jv79mZVav7g09XYSs2yV1s7IjIeQRDVs=,tag:oXQOzDnFWgjGSzPQvWohMw==,type:str]",
+		"version": "3.11.0"
+	}
+}
