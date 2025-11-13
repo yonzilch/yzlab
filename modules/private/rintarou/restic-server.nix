af482b3f7aaaa4efe642996b4c390b59a03c7107
yonzilch <github@yonzilch.com>
2025-11-13 16:54:05 +0800

    chore: update restic-server dataDir in host rintarou

--- modules/private/rintarou/restic-server.nix
+++ modules/private/rintarou/restic-server.nix
@@ -1,14 +1,14 @@
 {
-	"data": "ENC[AES256_GCM,data:Wq6eYEc61p0bQlg4PpzI309rzvnD8bmn75juuU4IuPoPDVIZll1V8ShiyD3xpRAYu3ruz55R2zJ1mAyCB5zBoJKELf/1vl1xJo7Cn86MWT9Nucx5GwqvFN0Cfic2JOSPXkGrtsJH7wHRFPdGZdm2G1UR4tHJxhFwLQt3hmnnNZJgj+RlPYfA7tOqe/djWSeZnoyjPY3gZQq8cOszJezRhFB8M5/dNi/HGVQMtj2fHleXNJGBhpstdK69d0Vdj6UqQMczs5AjdaV21Y3JoC2Xq35FYslsdoannjpC1782uzlBpxoR8UOOnPXz4aHJmbeusQBXHI5SaQ==,iv:bgGDWMXUvenHf5n+NLTg1RcuJXcmyzczHihwe9Sxioc=,tag:/sCMKLTOqo160NV5l0DTHA==,type:str]",
+	"data": "ENC[AES256_GCM,data:6JrocRJTYt/TM2kUqfgbVoPjkfWMUnQKp1i9U8RPsS8V1gSZJR3Jbm0RJtxVWRHeCT6o5GIullkbl5EQNeqkkiFMOIBWZse1zoQ7fHtYus9wBDvyQTapFiH/KeK/3CuqpnWWW3Jkz9IGGLIXjy/EZzVncXzXRb+t5i/jYwqoWK4hbmaQZDRIlNGSRdtytSLjXBpeFtsGuXqSjgA2gs2sas11F5UDQ2EOJZ7wFF9Zmwx7hf8Mqd4hspULgJEQRsbV30I5MYWnGudTnaPDU4PRiaMC5cbj2lQks9Azg4etrxX8vEVaxCMqpjqyHkL5r3g8BPUkx3fD5oe6/1450k/sMS4v5AGoHOHrjPg0rodyVkYj9/LDmaNEysVM8E0owN5LxyW1M/jbu0z3s+VRrQRiLNeZh5t+ioISaHllSS2bcH/HKXJzmk3xKFR0tv04F27wFN3+lx69PrNrn5dBrxTpdtKxW/1/6/ppBpx0/f85icf+Ag==,iv:4stFlGBhV1xvazeRgJ/2OljznnT4wtmbi/H9tJ/1z/k=,tag:nofKwT0qXvdKiILPdugeSQ==,type:str]",
 	"sops": {
 		"age": [
 			{
 				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
-				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSAvVVkyVUVSMVZkS08vakxr\nK0lSdEtwcDB4VG02cnVsaXd0OE5taXh3MGdRCldoa2Jlby9qMW9LM0xZNHNWaHhQ\nWVBvOENoQXpVREo1cUdOMDRYMjM4dkEKLS0tIG1CVklsRWJKVGlUSmdqY1ZrTFdB\nc1R5enA4QytURHI3dmdwR3NjU3c3cDAKSYIlKiuhTO3bUFXiQ79FR7Els45GnGfx\nfJgmq5hvqJDGEotyKsVizJRYET+3jP7bXNDMxOGg2BWVwvco+Yanfg==\n-----END AGE ENCRYPTED FILE-----\n"
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBlMnBPT3g5SDVwRmFVODNs\nUFg4aVQwMm1BUEluYlQvNFV6cFpMY09rN2hnCmZNejZ3L3ZUbkt5elRVQVFGMTlM\nRWpmWDViUGl0M002bC9JZitkUjExaTgKLS0tIDJvSk8vV1JjWVNzVDJpamppNGpI\nZ1F1S203TnZsUmVRdGI1S0RmUjlUNFEKXwqCAGoUGhTalxMBZJhwaay6lSDdk6+d\neAVOwIXkJf0MSLp5uWMZonoyVQjVjhyyjtuGgAaFm8plAFqrNwT1CQ==\n-----END AGE ENCRYPTED FILE-----\n"
 			}
 		],
-		"lastmodified": "2025-10-29T02:29:07Z",
-		"mac": "ENC[AES256_GCM,data:gqcBNh/XHh3XGNeVWGisF+mQDQ9QKJp7Pt/Egchk6T4zWAdT4QsMsPXuIvWhQ/qWWss2VwjDklm7T+ZIV0Sw4PPzWNi1BSE4KFwAPF/QjrFGieEgD10Qpbre/ezmzbqbf95CaxAcbDPhJERmQ2S0/Dsv5CVglCB3+iiUNrmBNbM=,iv:sBjmtIT9KNbvCjBDwKlLjIhrzMUorNcUkV+rhsb5NGw=,tag:H27uiGH9DAjxqU4NRl5WdA==,type:str]",
+		"lastmodified": "2025-11-13T08:53:36Z",
+		"mac": "ENC[AES256_GCM,data:Z45WcqhuvzfdgsecxWPvdhRcCkXBAgxjvdA4wm4YN9/xc1qn6woBIuU/7AYw6ZBQsHm4eWYYUl206OX+6Fa3Zi+S69LYZAnUkqOJ//DdibW0SkC562VrK+WC9n30hogiljQ2Ux9RIilRIksiQN9Dg8tKaFiSmF+M+KyPkosxx0s=,iv:AhTJkUmXUD+kbZS9+8x3Pq03eWJQHrwsvRyUvHvG3eg=,tag:d5HtgElD0NMIaG8a1h6hrg==,type:str]",
 		"version": "3.11.0"
 	}
 }
