561a6a2d1644c4d0295c9ed51ed7ef9ec5a8539c
yonzilch <github@yonzilch.com>
2025-11-16 23:35:54 +0800

    feat: add host suzuha

---
+++ hosts/suzuha/default.nix
@@ -0,0 +1,23 @@
+{
+  hostname,
+  lib,
+  ...
+}: let
+  ls = lib.filesystem.listFilesRecursive;
+  primary-disk = "/dev/disk/by-path/virtio-pci-0000:00:07.0";
+in {
+  imports =
+    [
+      ./disko.nix
+      ./hardware.nix
+      ../../modules/options/komari-agent.nix
+      ../../modules/optional/terminal-implement.nix
+    ]
+    ++ ls ../../modules/private/${hostname}
+    ++ ls ../../modules/shared;
+
+  boot.loader.limine.biosDevice = primary-disk;
+  disko.devices.disk.main.device = primary-disk;
+  system.stateVersion = "25.11";
+  zramSwap.enable = true;
+}
