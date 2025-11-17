3341f15a5877779a1202c18a852b68b5b5c28a86
yonzilch <github@yonzilch.com>
2025-11-17 15:14:39 +0800

    feat: add jellyfin and openlist in host hiyajo

---
+++ modules/private/hiyajo/jellyfin.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:s1Sa0052kNhWo53tb9O28l5ymZV8ze3MUlsMNg+6SOsjsNkq1ztwPna76q2lbEm1240L5f+F5R/LfxY516hRp+Dj2aZ7N3gqltw=,iv:uTkhKkNYaU6dVMi8HMzixRHCWoXlKVnTEuzpSlI3AV8=,tag:8+ot/QKvRSLIDY52/GWWag==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSA4bG1LZnBhUVFJbUJYR3lq\nbHRUem84Wk1RaWNNeDI5M0JQZjA1eXcybGswCjVmc2d5eUZ3emx2ZzZUSlNINzlZ\nTzJBbDdBZmNXNDdJb3ZMcmtCaU1vZ1EKLS0tIFo2Zm5KR2FQVEQySlNIMHdhMldz\nUnFzaldtaGYzdEx4dWI3WGhyU0pzbWMKzQFWCRa90IPIHjsMdcCoagACIsTbeCVs\nAhM5IEnxtTuF+hxhA4w5XCmv2F4yyzkFj6AyDTwei1LDCxfVfvWpMA==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-11-17T07:14:25Z",
+		"mac": "ENC[AES256_GCM,data:ljYDYbBq3dEw9nGemLTRjnbr5//LTeCHwXGRS4PElpqjzqmnPCZDDyGgfw8FdMnkyq4GMxfs3wt1N7khxpWJRQ0nSDUGIkEoEiUKhmvAnZRCdApF73Ogz5Z46Q0VSQW0jZYVxYtKj9u1f5hXJB4xm9/TQ182iTxFr8mn400ReuE=,iv:eWmhqmzo/pE//PyPzFCXjeBnwt7mNXU8rsAWdOjMYDw=,tag:4UsOaM4N5M5uAubn3HWfPw==,type:str]",
+		"version": "3.11.0"
+	}
+}
