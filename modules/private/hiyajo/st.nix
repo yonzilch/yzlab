31b7b3f06b84d49aa640d847da01358735fd5b21
yonzilch <github@yonzilch.com>
2025-11-17 16:31:09 +0800

    feat: add st in host hiyajo

---
+++ modules/private/hiyajo/st.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:mDGgCZGDO1xuTqYIoBM2BCBlkE0mlA87ugJQa962Nh8zpoiuRbLGEyjT11ULVN79j62JoIKxGUcm23geC+oM0cHGbzA=,iv:njFlt6LK7/7PmvrwrSRISbkD3/EN7hXNKiB6UPsaZ0Y=,tag:mtRugcL/SUOjxl8WWxdy2g==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBiNVpmdEtpVmhDKzB3bUQ0\najA2NGZvcU5Da09CeSsvL29tbjlNeXRnN0RrCndWTU81OEFDd2FVZ1ZwTkxMaGtD\nWTF2dFZEanNka3NBRzYrZnVRK1RWVTAKLS0tIHNkdzJyb2hCTGpPeVVoemwveFBN\nQ0tSSHo3aWpJQkZlOUZVZmFxelJidjgKvwcTkbkq0oNXd6H7PPuXTsjLb8QUWmim\npwLV6HlTzixosS1Bx9f9I1wZflhcZR7ZRf5KYlKUl1B5x6qS7nNNfA==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-11-17T08:30:53Z",
+		"mac": "ENC[AES256_GCM,data:ISjoMEXbgdjrDc4P1QQGLx2OyfxqCI0h9KB5xuZM3eoGwYufoc+JXyg9J1FZcIpJydzuSo+AcVJ4h9OJv6RA5EwcJSJcOmoZCJe+xV4U6CMEuFzOmhjMK3/3FDj4dWwFJAod5ClZ0Qmxw8nFGfpBQ/iuSPd9ugD64msNf8YPpNU=,iv:sGTUj6UP1hKaMvOijsh9iP6b7U2ZxpTPqYHuQotX7yc=,tag:UYOktcF7A1r4lD2p+cwGdQ==,type:str]",
+		"version": "3.11.0"
+	}
+}
