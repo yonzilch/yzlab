aaeaa174ddf5feff8b51d35a3b05f74a7ef3facb
yonzilch <github@yonzilch.com>
2025-11-25 23:36:31 +0800

    feat: reinstall host hiyajo

--- hosts/hiyajo/hardware.nix
+++ hosts/hiyajo/hardware.nix
@@ -16,6 +16,6 @@
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-intel"];
   boot.extraModulePackages = [];
-  networking.useDHCP = lib.mkDefault true;
+
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
