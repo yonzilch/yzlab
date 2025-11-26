fee2ba7e1b2a4bdf2e561b618ba8bab6dae329b3
yonzilch <github@yonzilch.com>
2025-11-26 22:59:15 +0800

    feat: add host takehost

---
+++ modules/private/takehost/komari-agent.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:pT8JnH2jhFRHBP7eHOOWIZKxMg4e28K3nukyrMFiTdaNyFKC4CFPkaKfjejWE9YhdWBazHG/ibvSypkwZREwzdKJ+kjarv0IrDtCqhoROO3+2CMNi0ewdbQxZ0kE+zlHz+W6r+66CQDD899k9tbc45kQs5MIfw7bmyEi3/ATgGom1taIIwg66HFG9eHm0JM5X9XCTA46StPYju8cWfn2BzXuSO8jlYZ/+R6MVpXa9oARRrBeCiIv1kFn/anJybp4mNP9Lb/ax8j9SPhglxYEyZCp1l7w/L8BWSG570v2bcJmi3yc2b60/nBAmiB0atvGi8Wym1OvhzISsjaXFad3mL/2Ze5JAj6infQDLt0fppWw/MB6,iv:KX5aEso+WNg5crL4Z6lKa5mfrtiOYRg5XKHD0Ki5h6E=,tag:m/66R9263V8W33OUf1WzJw==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBKeVJRMi9sWm1mZFFCYWpl\nallLNmpKY2ptSG0yeE9YOVFSWllHTXA1STJRCjRwZFhsUXR5cTdsaUQ4Y1NFOFkr\nUndGM3VNbGhSdmJ4eFo2YXRXUlkvTDgKLS0tIG55RzNpVVhKTURzNUpHbVVrS1pN\nbmtpZzZBRS91Ty9rNG1XWlQ1SDNOVHcKNoDOLV+jQYHSSO/EbvJ/CEEqhtDwQSdO\nQmYsxDbodcohPnDkI5dfajy89smM0zuMSsH76pqreI/QprVGt1NtLg==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-11-26T14:58:58Z",
+		"mac": "ENC[AES256_GCM,data:vs8DukuLmIMoaoZzYNLgH+Ob5+y72lJNWik2JcmXrGSiXR7DYxFkeBYNVN/rtq3OxMj7Xi1kQJofCPJToY1R8AIZntFkKB4AtOFH3tOHmlRGESkpTeMMqU0DXc+hgicO9wtVMNQtULLf5a5etXpOpmHUCYh0TRczIIqc6nAEF0I=,iv:pqWBOW7RcO2KsbduoRwIGDacqPUCLhwO/42RHrOKz6s=,tag:A3JtHQ5+dTv4aWUA+YIFbw==,type:str]",
+		"version": "3.11.0"
+	}
+}
