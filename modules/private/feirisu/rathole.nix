c09a8a21d5e495d4f879e215107b80677e1039f8
yonzilch <github@yonzilch.com>
2025-11-26 11:29:28 +0800

    feat: add rathole in host feirisu and kiriyu

---
+++ modules/private/feirisu/rathole.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:CU40cIonER1ogBxP3Zj+sVCcIqeMq9ztN6XTHdn5t8LS4ptl0qw2s6ly3fmGI1a2AQftLJxnl+Vk+5LLRB047eEvM3JTfGSZOvKv8qbLbWQcra6DtelTr9C7pHI6Fp1eU8trE3AffzxG8XLTZ2F1GgW21MxUjTkimT42MSgpnQP/mdi1eaogs3Frqc0rQXO9k8KkaA0pOBHBnzyb2gjruFsafz/L/zTa+Iu4dYN9y2eIWUFwZOJzK1ko5a03eawbrVGhDRXg42bhtXOI+dI/KUMEG/+mLE1GHByUREjEQx5bAaofUojI7LG2cvlCdBbwvywmLiSDGGxnNaiyoKHShfujHwdywapg5Niw/k7xTxemP9R/Y7hCFD9u+7sjtX0x0WmiLE6ZpLg7bk//UvUg0L+h6WdkJWt0UGOXsv9O1BCT0VqoKYzk9u2QO60rU7WNw9OrYlBSO3lpbxS8tn2nFC5e58S8tVla8yFMDeGphuq5a3XbeSiC734XAw==,iv:lCO6X2nm1cxnwcUARiwF2oVHy0kAuoRXbB4CGnWvAdM=,tag:IxS66kJb7m0PF7VTYwvclg==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBGd20rb1E1MXVCV1VLSlhF\nN2hHaXdkV1ZqTHNkMlVBMTZCRmkwL3hLMmhRCi9zeW94cUp1RWE4VW5Sb095Q1Ey\nTGN5QlFoL1BtSnY0UXlJcm5jRkt1ZVEKLS0tIDZlbXZEQXhDNVREZ2Zza1V6OVNW\nWHN0a1pnSGo5Y0tQK2poNm1iS2FQN28KSLIW+ttm4UGesj1EEMnVBswdApO+qXuc\nabHwaVlgu1ZwfG6/K+LLdUyTOgWJCUPCmU8pxJivqJdkn/XUO20rMQ==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-11-26T03:29:03Z",
+		"mac": "ENC[AES256_GCM,data:RnpHmcbXOk/I8goxbYHIsWKr5Da77DWjqCRn2bAigT1LTgCovimqVU5FE5g39gQ9XjnMsYosbnkNTqxqlJmvWLoDZMkHHKh302adO3K3FyTM5Vv6SrSF2s3NpT2zy2zDJDeLTFhavWuhmpJjvocrFuF7KoxPizVXYFnvEIcWzjM=,iv:DxCXWBx6fRG24hfBjjRrgYeZ5eK5x0oUdmzfE+BatI4=,tag:xQapP8fzGjxe9kt929n+dQ==,type:str]",
+		"version": "3.11.0"
+	}
+}
