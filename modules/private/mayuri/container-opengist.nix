9a509034c93f1368b2cd1c4c924d7c12d7b43708
yonzilch <github@yonzilch.com>
2025-11-27 15:11:42 +0800

    feat: add some containers in host mayuri

---
+++ modules/private/mayuri/container-opengist.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:0YfrtZ6/FBjbo2tMwxh/SBFwLMSjLLUo+ntUuPG6lDWhg9kToNn72NH2u9ieN+JBPfP/JtiBWHF8viJefkWXxshxQQaMEpSVOcQ3lCu6VBEdgJeoAL/z6RuQ6usOkWGgjY2zkM6wzDYMCjbkRS3kqYIsRJEy44MugRt1FZm71pYoXw7w8kGEXk9id6cK3iPadDGtzsYWdKgIKxyIeRBQMOCXsoBijge+GvmEK1ikGPpd6kkwywgeMp1GaExQDeuAmrJFd2RT84cZqgvEQp8tjCDIPLN2joqKZT3J8Nynl3fpynhZYW3O/SfVGZiDUHNqOBiuflEp2ZD/3+zGP1nTUtKE4EpwoCr6FeczjfqkvNiONcMel9sJ++yollgIJU7l/E7Y1r9RU8lrHtuzMI5uV2vCfME9ZjwfJu88AKtg/g==,iv:XO1damF3218lWXcB6vNI/h2F8gCWxSHLonOiZnAK2fc=,tag:ssT19lwxnV1Njcj02c759g==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBLa3NYR0hscU05KzUzbHk4\nRlh5RllXTU54NmFpUis0Q0xwY2U0cVEzZmtzCm94UE9hODh5bS9NcmdwbEtTTGxX\nckx2TnB6cjBmdHF0bTIwWEtKS2tIcG8KLS0tIDdzSkczdVNxOTVPVUFXSldvUWtk\nQzVTNkdaRXJvR1R3dzM0Skhndi9kdFkKjwiglxNnZzoYQWiDkBbTAvR64LBNO8cO\n+8a5ppCUwUcgoH3Zv04+ODfUJ9+FqHCk97BQSdwG+Y2BE8P9EFEHpQ==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-11-27T07:11:05Z",
+		"mac": "ENC[AES256_GCM,data:3/6AzWpNyRBeI8F3A4faQ+9SzvwkW+m4DwLo2PmENFT++mRtUnf6J668HDFX3MYmPuM3hFvSbyhFldd/EyVzqbckCHOpkGOpY40m64Qx38oNLrCUgZAJPWtT+uas7+ZlfwdMLPJtWfYFgVWWXsf1njJ7ht5MCS2aur0TqrP4/oc=,iv:49Pf7Dmh/e3OXgoF6JQSQfrHmK4yHWWB/eEgsj8KNkU=,tag:RqZj3Zo+7rzZLSyKqLcWGQ==,type:str]",
+		"version": "3.11.0"
+	}
+}
