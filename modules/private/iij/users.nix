2eef8cb9f6b769c3e09399bc5f8026f173ed5021
yonzilch <github@yonzilch.com>
2025-11-29 16:59:50 +0800

    feat: add host iij

---
+++ modules/private/iij/users.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:ZOlzxcPEYDP0umL1DQ8M3LhJx+s0vytr6I5hyJ4XRvnA3jTDu6nJwsj8FfX6OG/EkTwW6SENqQUCTO/7ehLthn9Z2EWNc3JhqJA0ZNFS8Ir7beAaOhy9d0VvrOeQjgNklATUX9z+lu5spvwvj2Fe3foVRPF79OlJ/Lqa5+tbaeu4QsGzPG+09FFqR5GoHayL/cFE8y9JwucJCksy0iPoTu68wD92jQLNF/hyTU7fwJFQzDUcAYAC3H+K0TCYP0KBkHVOL/Ddjo6bDkG8XHF1QGrbwLu09MsPjeOLuq5/j2rTfKQgVaI/k7Iw048K1mGiyvE+2AOSypfYVkIOha4qyOfTOBmS5NjI6mpWKusPneznWzcF4dQs0awZG+U0s4ids8wIJKCCGw==,iv:vBGw/P7kjNy4rhCbn6I7TFL0C71aAcL4VK8ObGhDB7Q=,tag:fEXWfIpS7DaJGp2mbOmpaA==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBhRE9WejRNQmMwdUFvQkF2\nYWhGZVQ3TDBwa2c5N0R5Z1R6dEwzZGEyM1EwClFyUmdGbURXNEtObHhqVlNXWGc5\nSGNQNUN6YTBmbGZVMlRvN1dvRHRwT3MKLS0tIGdJV3UwWUpqbXRTaXVxdGt1L1dr\ndk9kTVZGTkRNaHRwcEt0clorZERsUmcKwrLVBTh8QLcz3E+gO1tz9wKA2EErutSj\nE3Lo/o4BEL+AJNZX9CjNVtX+6FLW/xElu0brac0ET1/T+IFzM5tD6w==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-11-29T08:55:16Z",
+		"mac": "ENC[AES256_GCM,data:LvaKvDq3P7PDEoswCETFiMsnD8kYBLoeuilzEeOFVlLTa2XldzEAcjrQF0WhqBX/cSVIsnEsR1Iv3SvwFvQvy90jWZaO8Pxzt6vVEjPJW5/pXieWsbEzmXucJnTV9BnKjiZozTA9wx4kPQ+GMIyOBX1l1ZzbTSe5pZarP+vvwjg=,iv:Jf9d0/FgZUmgdKdBlT5zxHf5pUq2VHPtExcJhQjwwgI=,tag:MLSvqJOgntHJUJ81h3VCDw==,type:str]",
+		"version": "3.11.0"
+	}
+}
