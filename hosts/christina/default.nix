8e70ab4d22548156956493e65ac32cae7c1d2210
yonzilch <github@yonzilch.com>
2025-11-30 18:07:22 +0800

    feat: update flake and NixOS to 26.05

--- hosts/christina/default.nix
+++ hosts/christina/default.nix
@@ -2,24 +2,25 @@
   hostname,
   lib,
   ...
-}: let
+}:
+let
   ls = lib.filesystem.listFilesRecursive;
   primary-disk = "/dev/disk/by-path/virtio-pci-0000:00:07.0";
-in {
-  imports =
-    [
-      ./disko.nix
-      ./hardware.nix
-      ../../modules/optional/podman.nix
-      ../../modules/optional/zfs.nix
-      ../../modules/options/komari-agent.nix
-    ]
-    ++ ls ../../modules/private/${hostname}
-    ++ ls ../../modules/shared;
+in
+{
+  imports = [
+    ./disko.nix
+    ./hardware.nix
+    ../../modules/optional/podman.nix
+    ../../modules/optional/zfs.nix
+    ../../modules/options/komari-agent.nix
+  ]
+  ++ ls ../../modules/private/${hostname}
+  ++ ls ../../modules/shared;
 
   boot.loader.limine.biosDevice = primary-disk;
   disko.devices.disk.main.device = primary-disk;
   networking.hostId = "f9e0c106";
-  system.stateVersion = "25.11";
+  system.stateVersion = "26.05";
   zramSwap.enable = true;
 }
--- hosts/hiyajo/default.nix
+++ hosts/hiyajo/default.nix
@@ -2,22 +2,23 @@
   hostname,
   lib,
   ...
-}: let
+}:
+let
   ls = lib.filesystem.listFilesRecursive;
   primary-disk = "/dev/disk/by-path/virtio-pci-0000:00:07.0";
-in {
-  imports =
-    [
-      ./disko.nix
-      ./hardware.nix
-      ../../modules/options/komari-agent.nix
-      ../../modules/optional/terminal-implement.nix
-    ]
-    ++ ls ../../modules/private/${hostname}
-    ++ ls ../../modules/shared;
+in
+{
+  imports = [
+    ./disko.nix
+    ./hardware.nix
+    ../../modules/options/komari-agent.nix
+    ../../modules/optional/terminal-implement.nix
+  ]
+  ++ ls ../../modules/private/${hostname}
+  ++ ls ../../modules/shared;
 
   boot.loader.limine.biosDevice = primary-disk;
   disko.devices.disk.main.device = primary-disk;
-  system.stateVersion = "25.11";
+  system.stateVersion = "26.05";
   zramSwap.enable = true;
 }
