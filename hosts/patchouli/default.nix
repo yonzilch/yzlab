22ccbb093921018e0f964cc0bfff20cf9942ca23
yonzilch <github@yonzilch.com>
2025-12-11 17:22:27 +0800

    feat: add host patchouli

---
+++ hosts/patchouli/default.nix
@@ -0,0 +1,27 @@
+{
+  hostname,
+  lib,
+  ...
+}: let
+  ls = lib.filesystem.listFilesRecursive;
+  primary-disk = "/dev/disk/by-path/pci-0000:01:00.0-nvme-1";
+in {
+  imports =
+    [
+      ./disko.nix
+      ./hardware.nix
+      ../../modules/optional/podman.nix
+      ../../modules/optional/terminal-implement.nix
+      ../../modules/optional/zfs.nix
+      ../../modules/options/komari-agent.nix
+    ]
+    ++ ls ./modules
+    ++ ls ../../modules/private/${hostname}
+    ++ ls ../../modules/shared;
+
+  boot.loader.limine.biosDevice = primary-disk;
+  disko.devices.disk.main.device = primary-disk;
+  networking.hostId = "18ace1e1";
+  system.stateVersion = "26.05";
+  zramSwap.enable = true;
+}
---
+++ modules/private/patchouli/users.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:kP0I3XKE/TL8fOt49eoh8+PzxeLjgI3h//vY5aGUJAd6Zm4Ie9drBOLhuv8uD0tu7ti07j0Z1z2UMqwS+jfMOMGggQ3pNi7e+A7yunyBJ4E/4hux3dYAzFwIEFkA28qDxTClkxn/NdCnQRgF09RwQy5rOZNv7y0959TF0LQsEXiZ3yqxDv9nW44X/DZjkEe5WOmfvHQFZ7jU2w7UNATvyHJaApKB+hNfxVjws5zYWLQ5nQx3eAZOuyQcpGexqOcsfeDu4YEJJIjCjeE958TDveQ1VXc+4/nVyQDD6TB0sRql4Ag2nMeMQB5cn36qAa1Xt92n0FvRjC4IZz+mcot54jLpxJ+M3Umk3YIVQC6T5r/rglFTzdGt5alcp2e2Rshen/TXP3Ii0eFEjg==,iv:LfeB1bFDrIHU+Wvtm09txk5HEMavfBiUP9OuFnPQ+Ew=,tag:7R94b3ygKNUzDdcuAfxn8A==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBQOS92UlFVVnpZMm91dnFh\nSTZ6ajNTNnVtR0M2aVkwNzhZVnhyTHIreGk4CjMvakVjRzhUU3NwQStIWHdOamxT\nRVJEeG96MjFxY2gySytLbHFNZVRiRjgKLS0tIDBKc1h5WGlUK0UyRGs1WGExREU4\nR0NhUnBpcnZFcUcyODFlRzEzR3VzRmsKVGXosEVWqHMqFZBNWb3c1U68ZTLV7438\nZQBoOLqf8M1g8xJ/4nWfoea0P8McN7Ke+Vc6GkGGRjjsilkHhPxJKQ==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-12-11T09:19:15Z",
+		"mac": "ENC[AES256_GCM,data:Q08GArRSowBOeeSJKPtJt4s1TfHYs/pUauWQEMxgiuLpF2u8k2FRGL9+Ck4JnX0rUOVOTWMQSV21K7/6+L2UPR7f4zWuU2q/XXbaKVO86AeYO8Qo9mi+wfpT5Cwu8Q+Lp3hvhA0cErLOzy+let/iKrHqFWrlyws0A+UdWVyawDo=,iv:/QCtQfSXlMq3ncEwYJAbHossoQnbURZ5zOGMLhTJavY=,tag:E4iZQcxpFM3zo0GHKod7qA==,type:str]",
+		"version": "3.11.0"
+	}
+}
