df6a1cce0dfed463f4f80757463801d04a1516a4
yonzilch <github@yonzilch.com>
2026-01-01 19:16:11 +0800

    feat: add container-bentopdf in host rintarou

---
+++ modules/private/rintarou/container-bentopdf.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:GfgsC3Vag4Z8gm87XpJjnKrvUmdEwZn4PlJ1AWLTgURq9ClDJF+r29PQISCmcSpXFzsEgEpYwFQpZp1fISrYr/TnQFUwfb1D4rmoyFpkiILvkOBFG8f07IJQwjklZzzVfY6/Y6H6UO4KxhDesECsR6EEDZchuuMcGQkXPEmClNckBJ2aFhsEfI2BnhxxuLjghxldNNiSkp8tUmRihl1Z3+Bp9fBzlVCddaYsp+eo2zh/U51PKwOzQNYxSw==,iv:6R+Dj+nEW98vruxRpeZ5pDqyYIQ81hvHUosGdi/vnVk=,tag:aFeelLpLIZp0/ixuBznhLQ==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSA5Y0FnRkFpOXlrR1YvMUQv\nUmNRdDVOSm9vZWlSZkpSOUNXZk1QWmR3UUFFCnhFTCtBU29jeVV5Wmt2aE8rdEMv\nRGcybzhYWmJqYVNaRU15RFRYZGNWUEkKLS0tIGppanRpVk5XNmpkYmE1MGVoZ2pm\nSjhwK1VTNTg2MGJ0NlNFc3J2MjRTMVEKMCcsWSyVJWZAGNyNsm9k8a2Q6fmO7QB4\nlyXN9T1vFm+FD2NoqOb+M0gkRj5x1CitSeaC5sNZrN1xezkS4bBfIA==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2026-01-01T11:15:44Z",
+		"mac": "ENC[AES256_GCM,data:iWv5PWBGSY+8fsrRw796T4w9nKoTGowLebTElufyDLM613uuxi5Xmfhs5RoAUIVc0GCxbp95cxXtsGhgBbzkPvvICVDXbLmc00xS43ZpJQhrjNuOF3ioUg3MAQw+Nthgm+OVzqE9/Bt6pRU9tAJmnaQCu9mVUzl0rOnYiYHjhe8=,iv:81AV5xEu1cZ5oTmyfg6XbndoKMjHgx8Oxv0xCTswQLI=,tag:saWazlnKE25IhVJvozo4Uw==,type:str]",
+		"version": "3.11.0"
+	}
+}
