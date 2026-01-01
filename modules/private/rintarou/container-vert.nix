df7c9d38c830fa0b02511da4ac70303cbf18c297
yonzilch <github@yonzilch.com>
2026-01-01 19:40:33 +0800

    feat: add container-vert in host rintarou

---
+++ modules/private/rintarou/container-vert.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:1MsBB581sGc7PZ9lCaxg6TL4lqdrvSXghchrj4ez9Mdof/AiVDQnXB7or4XrIYU0ZosHZhlywRChgoG93HLL/ZYIcksM0L3VYUeLf18cggwNapGWOvKuKe6WrJii8nlIZuSQBjEnueMtqOSb3EQtnmW1V1KavIt4esvfxDfrDtg0PbiGJPUV5GFgcVcUCbyuxaNsPgvNtq3S5ee9vAc5Ah1T29M+wylyFM1awzm0Ub/M,iv:qr21XZfA4MJzY6niFR0wUVwXdjdLi1M2mnx5geMzOIc=,tag:y/0ZDSw6DLoaaTiBA+1/kQ==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBtUkxsdkoyOURkR1ZvbDcr\ndCtxV1dMMlNsdUdERFliUkNJZFAxeE1qVFdzCkVOZkRoQ09PZnBqaFo5UEk0TFky\nSlc1UisyVFpSaUxOT0NSQUQ1cWxyM1kKLS0tIEd3Z0M2RXVZMVFBSElRUHBCZ2hH\nTlFoL0VFblJkUzdMVXM2WFp3QzJYUnMKVheT6am32M7EpliD04T8sxYROm85TpfU\nZ9hUeFyszYvC7161YaUKqdYnlTqrVGPyNdTuuUXWgDFpD8huD78g8Q==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2026-01-01T11:40:12Z",
+		"mac": "ENC[AES256_GCM,data:fMTr5dRysgynzypveuXf1djK38EovN5heG78a81jSBUnn8FXRAMxM2SBpUJtLrEnCUMoWQFt0YhCdaN/AiElDNa4xPan4CspXkM8DWnViQL8FYnsEuYEeFF0BRgLnUajJYk4wjnZ7746E5Bd77H+csn8IFvxBFkkR+KSrRWAu60=,iv:tlZ9jhKEf9xgaJggxO7o2buG8BaSji5oJ0OUmGNrI7Q=,tag:9+j4qz6J36gsxf/j8Agdig==,type:str]",
+		"version": "3.11.0"
+	}
+}
