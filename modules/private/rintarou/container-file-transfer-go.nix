44ac3beae2931e0d32fd3299b9b916d618022f32
yonzilch <github@yonzilch.com>
2026-01-01 18:07:57 +0800

    feat: add container-file-transfer in host rintarou

---
+++ modules/private/rintarou/container-file-transfer-go.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:1KE0HRmsWaTlgyamwem/qz8mlwBi3ZCwJGf9i6ntK29fJbweRXDp2d3G5Kbl6qa6q99liSlX4MH4/PoXI7rroCQICuMFmdvU3XLaLD9B/BBYAmkjUj9x0NZa2+P728rATVN08cot2mI8Sltth4mIwNQAeyYAog9b1FEllg9JaXQSxRSRZt/9FP1Hgi0OGyon7KecUr2Fomnfldw9n2lV/4NUG6chUEjZzTBQCqU4ES7h/PSxTRhQyDrgJtsPeLMlz7aJuOGOsA==,iv:PN1z6Toc1SpgUTGZ27tf8zgkq25uyM4PLz9rYtpQUL0=,tag:ZSks1mEuC8Z/H3IPm6O7OQ==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSB1cWViL1A1STVzSUhtUlpO\ncVNUb3VJZHE5cmc2a2hZY0FJQ3ljd2Z0RUJNCklreWlXZExzK0RwTnh3T2xaSzZB\nR1dCVjNHLzYzWDRSTVdCTDdlVXE2ZEEKLS0tIGQzOHFIWElJMytrcWlkRDlITUFC\nY202L3gzbG9oOTJJeWJXaExHQlF0Tk0KYuccy4WdLTkqQIm0VD00g47NZmMKh60Q\nG0SyoXN7O1CmsqPdm7b1rhHnFIRJiormeb8UREG125O4cBs3fBsn2w==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2026-01-01T10:07:28Z",
+		"mac": "ENC[AES256_GCM,data:emzmpSOs/nH7Y8T6XuZGPZ96HzvxQeLgca7pC7qzYwmEkuPRrM1gmIxN+zkzFXWlv/zFEpDo8tnyMUDNClNuDP2xFnbwg2bwkx/qGGkWn1FWhOID/Y0IevhY5Asu6qaiJe8/tmDPIGrYcRg4lFip1c73kJnq8fFUdavM025ML38=,iv:qwvIWlZAvanhFtJChTIENme4DN0fEQjQ85jQepMhwm8=,tag:Fn9eUTc3859Lyy2570o7Cg==,type:str]",
+		"version": "3.11.0"
+	}
+}
