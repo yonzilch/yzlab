7f4009f58bc15e787f08b06bd5adb493307e9f16
yonzilch <github@yonzilch.com>
2026-01-02 19:25:06 +0800

    feat: add container-etherpad and rm container-rustpad in host rintarou

---
+++ modules/private/rintarou/container-postgres.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:GitVJDKhfl1aOeQXGPnO1GD+KUeoOTuH5hswr1Sg85z2eUqMmfUCVOP6F40elUq4dD0v19PaHh6dL3NJ/K/zkGw63bOKfqK9t9NDSyfSuxeq0gwZkFzStN9sa5Sw4GiMSgO/DPnUBwxCCEvzeSyZIaTmZMW4xnCLvOLm6tKdM0/QK0vBxZmCSQUar8VOf087TwKUZ7e6X+JWrxi01cPwAWFkGEgE3okZiPhhVgcwT9qmNnm4viplcBj0gD+8/42/Hu291i4N0dCSl38zNZW5750ws5kdGXZwWkFA9XRAI/3EqwqRv1RpGhKnmR/RcKtTl98kQpvcoPjMIbL4Izg+an7noYdURwyGYKH8tnkiaHT5yxk4mjmUTn/fNS+X5zX/TbHwkOowz8KtBpGp863X/oqgysV8hkCWtgaI51F1yb1WsPA/rcq/l7inuHNsNaud+zhYlN41UEJZx7ckD1CSlIs1T9jF3t5TcpzDlaJT3uYYYdleKbdER+3WSA8R8YwEfbV5FTIzHFZHYX9GzzApD+Twb+sKirBX8gWnElR3A9ILlYHKzQ==,iv:Et2w1CY4FsasHH2/kPyCSurdtNCCojxRD3bw6YpLyfM=,tag:3WOF1SL3ORW5eciBaB3QTg==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBackZZOGVpaVBkVC9QUFJM\ndXJKZWw1bkxWcFN2a05JelZXU1U5VFRKVDBjCklYTXB6eDNxczdaRDVPeWJEVGNy\nZFpJTmJhRVVMMjkwY0drcWtBTVBKT00KLS0tIEZGd0JaQUhCOGlwVXFzemcyOU92\nbG9tcnd2OEdVUkZUUC9LWW1nVXA3L3MKuuOs1hoOXA+FLx58OIFZ9/+ZXI/sNFwX\ndp/XRderDKaeDxtymvVwN9mLy4BWar0QnSqHdxG5B/kuvD4IgeuviA==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2026-01-02T11:22:53Z",
+		"mac": "ENC[AES256_GCM,data:6yWtkItliv8M1S29LGytqvIaobqey6RG4nIHrUAlnuIcMef+CFNPtfTJ5eDrq4gmvgmxYNHG34f5pAIt1uUOu/JX7obRSCPT8Re2OQkXtEUtYLhykmaFHQFOEvQbbdnoLq4zWe4sRHY4hRCgtI4tO1lvafetbaLgP/ijPif1Haw=,iv:bZdyqladWuGpP2/XRiPbxcUMRDE9LDtHoO0XmxQ8JgI=,tag:cHXE8JnpVwFQIkGaS5UX1Q==,type:str]",
+		"version": "3.11.0"
+	}
+}
