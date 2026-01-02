b4ee9cd584025a78b5511bb745d78e53a8955464
yonzilch <github@yonzilch.com>
2026-01-02 15:17:09 +0800

    feat: add container-komari in host rintarou

---
+++ modules/private/rintarou/container-komari.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:efZKjUC/y2j+6DxVM2KObTsREr7i6pAGMNz3DTqYTKjYXzJWXkDS8wFpRAtukmXEeSr2IWk3C2ybRirnDJ21uavwAgdnd3myBnVIBbvqdNuSfbN5cSbVrQWu8XjRLeXG8GFBdPBhLx56RmRzLthEsfENfUwMHvRodAhyT2YI8AQX/2/AEJpCMDzoTXU2AkWHglm6m+2fGaGrmmwNVIDrF7e4sUNrftuQXQF3EUKNfF99e2Dt3DJCk8dd78hwt5Qe5inyIHVKwcNGKgV9LXATbnW4cHslDw4zw9rM2c5bU3Agwvv4u/7x/B7h3KNleN8=,iv:tz3yFdLOUUaclldeBLFasxsny+azqdhV4s7ARMukqyI=,tag:OpV3bRwohuq7GJ4hAiaLEw==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBraEV0b05tNzB5YVdvaloz\nZGhpNFJXNllaZmI5YzVqNmp4M0tKZXZwT3c0ClJ2ekRySjdEQjBGanpSeDNBK3pE\nOXdhOC9OVjRIOStvOVgreTBLQTdSelEKLS0tIGRjTkdSN2p3ak5mQ0tnK1crSTRk\nRUJraDdQanhDOXdRUEpwN3JxQ2lNdTAKNdP93YzNt1dBYMRhPf19eh2AHtCQKepz\nfhVJf6xJfmGaLCzDOxtSfEDDb9dxT6qROtmmYFEVKqF95EWOgMXeCg==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2026-01-02T07:16:49Z",
+		"mac": "ENC[AES256_GCM,data:VT0zWKzPj28HqPjMKY5kWng74Vi89oqbCyxq8LhV7fFt4R5RiN5pUmNsMG2yIrBGO1GpbGaTUjVBHsKwfiftym03o52LQw0v38aBh0nfK00ziVr2tcIFy2u1FDt579IqyhK6ujOfZ+x6B3AAmk8aXgUsvKexvs6Q7+vf/y+zUs4=,iv:pDx+ZnF7j5C0R1cpZSbsUWynC2cdpmBhY0Zam+G5MqU=,tag:hZGVWbdOTXZDW7iAkcBZsw==,type:str]",
+		"version": "3.11.0"
+	}
+}
