494578099a59c6fdcd33a291b529e7ea478251be
yonzilch <github@yonzilch.com>
2026-01-09 14:46:49 +0800

    style: format code

--- hosts/example/disko-f2fs.nix
+++ hosts/example/disko-f2fs.nix
@@ -4,40 +4,41 @@ _: {
       main = {
         type = "disk";
         content = {
-          type = "gpt";
           partitions = {
             boot = {
+              attributes = [0];
               priority = 1;
               size = "1M";
               type = "EF02";
             };
             esp = {
-              priority = 2;
-              size = "256M";
-              type = "EF00";
               content = {
                 type = "filesystem";
                 format = "vfat";
-                mountpoint = "/boot";
                 mountOptions = ["umask=0077"];
+                mountpoint = "/boot";
               };
+              priority = 2;
+              size = "1G";
+              type = "EF00";
             };
             root = {
               size = "100%";
               content = {
-                type = "filesystem";
-                format = "f2fs";
-                mountpoint = "/";
                 extraArgs = [
                   "-O"
                   "extra_attr,inode_checksum,sb_checksum,compression"
                 ];
+                format = "f2fs";
                 mountOptions = [
                   "compress_algorithm=zstd:6,compress_chksum,atgc,gc_merge,lazytime,nodiscard"
                 ];
+                mountpoint = "/";
+                type = "filesystem";
               };
             };
           };
+          type = "gpt";
         };
       };
     };
--- hosts/example/disko-xfs.nix
+++ hosts/example/disko-xfs.nix
@@ -4,30 +4,26 @@ _: {
       main = {
         type = "disk";
         content = {
-          type = "gpt";
           partitions = {
             boot = {
+              attributes = [0];
               priority = 1;
               size = "1M";
               type = "EF02";
             };
             esp = {
-              priority = 2;
-              size = "256M";
-              type = "EF00";
               content = {
-                type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
                 mountOptions = ["umask=0077"];
+                type = "filesystem";
               };
+              priority = 2;
+              size = "256M";
+              type = "EF00";
             };
             root = {
-              size = "100%";
               content = {
-                type = "filesystem";
-                format = "xfs";
-                mountpoint = "/";
                 extraArgs = [
                   "-b"
                   "size=4096" # block size
@@ -40,12 +36,17 @@ _: {
                   "-s"
                   "size=4096" # sector size
                 ];
+                format = "xfs";
                 mountOptions = [
                   "allocsize=128k,logbsize=256k,inode64,largeio,nodiscard,noatime,swalloc"
                 ];
+                mountpoint = "/";
+                type = "filesystem";
               };
+              size = "100%";
             };
           };
+          type = "gpt";
         };
       };
     };
