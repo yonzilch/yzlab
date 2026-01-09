7d0150261ae23a1e3057b0d09feb1d57c77a456d
yonzilch <github@yonzilch.com>
2026-01-09 14:20:23 +0800

    feat: add disko-encrypt-f2fs example

---
+++ hosts/example/disko-encrypt-f2fs.nix
@@ -0,0 +1,50 @@
+_: {
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        content = {
+          partitions = {
+            boot = {
+              attributes = [0];
+              priority = 0;
+              size = "1M";
+              type = "EF02";
+            };
+            esp = {
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountOptions = ["umask=0077"];
+                mountpoint = "/boot";
+              };
+              priority = 1;
+              size = "1G";
+              type = "EF00";
+            };
+            luks-root = {
+              content = {
+                content = {
+                  type = "filesystem";
+                  format = "f2fs";
+                  mountpoint = "/";
+                  extraArgs = [
+                    "-O"
+                    "compression,extra_attr,inode_checksum,sb_checksum"
+                  ];
+                  mountOptions = [
+                    "atgc,compress_algorithm=zstd:6,compress_chksum,gc_merge,lazytime,nodiscard"
+                  ];
+                };
+                name = "luks-root";
+                type = "luks";
+              };
+              size = "100%";
+            };
+          };
+          type = "gpt";
+        };
+      };
+    };
+  };
+}
