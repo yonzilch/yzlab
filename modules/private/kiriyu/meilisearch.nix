90bf8271ac7471d71994b337f007d5dbfc40fbca
yonzilch <github@yonzilch.com>
2026-01-10 20:02:26 +0800

    chore: deploy some sites on host kiriyu

---
+++ modules/private/kiriyu/meilisearch.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:60urk1xrrCWUsjKKTKrAwfXmbjg6TetkiS6/Hai+LwsaeMegPy4kCj7rvu8bHQ7WqROigBNhksznl7Q1fBXK5/mLEjssPbUatP7lNtfCtb2HSf95/2mZXWoqgZc/nzDU5Z/kQ4om4XxhZS3Szxkt4KK4DvRDmK24rLLZHnO2j634WJGsggttNB/ObOW3/xst2pEIYiJioOqrV2gL8wSDwflhTS0Dq8CH59fBT+Ipg7t7k52/i6fjM0AiNvdjP9CA,iv:JXis2bNlw3c2vzZvX+DvyuK29tVULaqd4vJX4CgUgIY=,tag:I224L+jR9/Kd1ClbPjJ9nA==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBiYkZSaWRIdjVaT0pTdUo0\nRHBEYlJlMUlpOGRIL1pPMkpZMk1FZUVERENFCkx0UllZUjJrWW5lc0J6MWFMd1Rk\nbkIyQllOdVUwczJYa3JHQTBERTlkZWMKLS0tIEE5bytObVozMGNZZEg4SEhjKzFS\nOTdZbkdkVlFlRDREdDZNMDA3bG1uejQKO1lbsk3MD/wGtjycT2zwny5T3SVlqtyM\nL94x8fUu09nzZ/XynVfzBgnJZ7ffcanOZubrJEn/yMSbrOXoV0aMug==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2026-01-10T12:01:35Z",
+		"mac": "ENC[AES256_GCM,data:PoKZhXDW0KN25QWGO3zqM8/G6xLJJfEdEH1iczZkaJcN2ZYmdSWk7BmcJoVm+nx5lXLZquhYe0V6hCOzrqFWX/3sSv9t6onjynJXv9PH1cpsY9hM8gZJ+9yVgurPbVWkiZ1MujEYxkEkGYBMXCTS46xt0C2QU82D4FuRzF3ApYg=,iv:tWD+QFmF0RpuguewtsVsP/cs4ILe5QJMhEExEgQ4eb8=,tag:NcIxUQpmo85QZFB6GuxVgQ==,type:str]",
+		"version": "3.11.0"
+	}
+}
