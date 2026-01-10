4ac27f6c449ff698da79ac80f1961dffd34a5926
yonzilch <github@yonzilch.com>
2026-01-10 20:02:26 +0800

    chore: deploy some sites on host kiriyu

--- hosts/kiriyu/default.nix
+++ hosts/kiriyu/default.nix
@@ -10,9 +10,14 @@ in {
     [
       ./disko.nix
       ./hardware.nix
-      ../../modules/options/komari-agent.nix
+      ../../modules/optional/podman.nix
       ../../modules/optional/terminal-implement.nix
+      ../../modules/optional/www.nix
       ../../modules/optional/zfs.nix
+      ../../modules/options/dufs.nix
+      ../../modules/options/komari-agent.nix
+      ../../modules/options/openlist.nix
+      ../../modules/options/st.nix
     ]
     ++ ls ../../modules/private/${hostname}
     ++ ls ../../modules/shared;
@@ -20,6 +25,6 @@ in {
   boot.loader.limine.biosDevice = primary-disk;
   disko.devices.disk.main.device = primary-disk;
   networking.hostId = "3f70d062";
-  system.stateVersion = "25.11";
+  system.stateVersion = "26.05";
   zramSwap.enable = true;
 }
