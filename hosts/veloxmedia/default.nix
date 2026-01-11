419ea002847d1639a014679f49db7575ec4dc89e
yonzilch <github@yonzilch.com>
2026-01-11 14:09:39 +0800

    chore: reinstall host veloxmedia

--- hosts/veloxmedia/default.nix
+++ hosts/veloxmedia/default.nix
@@ -10,14 +10,8 @@ in {
     [
       ./disko.nix
       ./hardware.nix
-      ../../modules/optional/podman.nix
-      ../../modules/optional/terminal-implement.nix
-      ../../modules/optional/www.nix
       ../../modules/optional/zfs.nix
-      ../../modules/options/dufs.nix
       ../../modules/options/komari-agent.nix
-      ../../modules/options/qb.nix
-      ../../modules/options/st.nix
     ]
     ++ ls ../../modules/private/${hostname}
     ++ ls ../../modules/shared;
@@ -25,6 +19,6 @@ in {
   boot.loader.limine.biosDevice = primary-disk;
   disko.devices.disk.main.device = primary-disk;
   networking.hostId = "20b9c718";
-  system.stateVersion = "25.11";
+  system.stateVersion = "26.05";
   zramSwap.enable = true;
 }
