bdaa04a6e9d51974b261f9d9677e44500d7646ef
yonzilch <github@yonzilch.com>
2026-01-13 20:20:10 +0800

    chore: add rathole in host patchouli and rm rathole in host itaru

---
+++ modules/private/patchouli/rathole.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:O2/QXeTlMrSf98iuKrqczAT74WSCqcua6kqG+ClDy4yWfX+b1zkHcz3kr4dhrd24aK5HU+dnoAlarZ8jwhGOdrodHovUQ4hykmHoKQ05CecA7tF/WidhP3fiBkvMxinIsIiqchouB2BEGdYW93so95pZ3H4NWG8NpPOLImuuMuGOMrDRJokE4mDbtVwSH3lwDSnMEm2LgSMhwtihE9L0zv7pW/Wqx3mqM4cUXrbDmXAHVkLSO0zHDEMjF5HAuABUViTc48DZTo75mZrIG2t7P8bo8XAwrf2XXI3WSDSXNf2FJogS91BSTtg4UNyqtTnFkziH/HZmOTpC5owh8fCZG8/3re1Ft/RD4n3GDDd4cwK8WXdzNYM84cXDLuNa98v02GFGfw+r13hbyayAKOsEH/clt9kDlbkaWkPdfT+ChjGk,iv:RAtSwAe+SYPDoADp6xuWgMA/yBBkT3b/6d4xuByGJSU=,tag:9KjF1095Vp7IoO0TZkiS0w==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBwb2JlQ2dGNWo5b1VjdndQ\nU1NuaUZhbVVPNG90cGZmQmZGMUxlb0JTc3pvCitORjczbnZKT1BnOVNGbHBWdVVD\nd0ZFTWNxTmpVZHF5cEZXSmUrNlR6SFEKLS0tIHJoMmpGTUhtd3ZUWXVIVm1rVkpW\nWTUyYmN2OEQyc1pWckp0c0d4NjVPUDAKcSMeNEhSl6X2w7XWVwiCq9n4t/3c3ykY\n7k4OnI4ZkGEYaBnn9ssDTGN9D6WWhKuSeBDyMpNBSyIxanXZeTwrzw==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2026-01-13T12:19:34Z",
+		"mac": "ENC[AES256_GCM,data:fQ6hBwZRDb1jX8UZIHeUu8hstpbP3ZJBBkLHfp7eWESvKAn0GSZgyRPxKUQfzNTYV6go57T4ZMHtll/5REDeYLeX+KagAWGslh3bOzDgtcarfq4wjrCx6WRVrMNknhjyWM0cCqXKJ2/3lJ9jMoouAYrCyIu0c4gvB4G7e4+ghAk=,iv:YV6Q42NcWRzl0z2f8n7L+TU1zFLg89pogsXhrU/6EoM=,tag:YK+OIJTmJYWEZPwQj/ElHQ==,type:str]",
+		"version": "3.11.0"
+	}
+}
