2f7693aac9384f51f7fdd0f2f1d4720f5f9b1d3d
yonzilch <github@yonzilch.com>
2026-02-02 16:19:44 +0800

    fix: services.resolved.settings

--- modules/shared/dns.nix
+++ modules/shared/dns.nix
@@ -1,26 +1,28 @@
 _: {
   services.resolved = {
     enable = true;
-    dnsovertls = "opportunistic";
-    domains = ["~."];
-    fallbackDns = [
-      # CloudFlare servers
-      "1.1.1.1#cloudflare-dns.com"
-      "1.0.0.1#cloudflare-dns.com"
-      "2606:4700:4700::1111#cloudflare-dns.com"
-      "2606:4700:4700::1001#cloudflare-dns.com"
-
-      # dns.sb servers
-      "185.222.222.222#dot.sb"
-      "45.11.45.11#dot.sb"
-      "2a09::#dot.sb"
-      "2a11::#dot.sb"
-
-      # Quad9 servers
-      "9.9.9.10#dns.quad9.net"
-      "149.112.112.10#dns.quad9.net"
-      "2620:fe::10#dns.quad9.net"
-      "2620:fe::fe:10#dns.quad9.net"
-    ];
+    settings = {
+      Resolve = {
+        DNSOverTLS = "opportunistic";
+        Domains = ["~."];
+        FallbackDNS = [
+          # CloudFlare servers
+          "1.1.1.1#cloudflare-dns.com"
+          "1.0.0.1#cloudflare-dns.com"
+          "2606:4700:4700::1111#cloudflare-dns.com"
+          "2606:4700:4700::1001#cloudflare-dns.com"
+          # dns.sb servers
+          "185.222.222.222#dot.sb"
+          "45.11.45.11#dot.sb"
+          "2a09::#dot.sb"
+          "2a11::#dot.sb"
+          # Quad9 servers
+          "9.9.9.10#dns.quad9.net"
+          "149.112.112.10#dns.quad9.net"
+          "2620:fe::10#dns.quad9.net"
+          "2620:fe::fe:10#dns.quad9.net"
+        ];
+      };
+    };
   };
 }
