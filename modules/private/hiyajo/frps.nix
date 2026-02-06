264dabc216c91949bd7490f9dbee2a0ad9c4b2a7
yonzilch <github@yonzilch.com>
2026-02-06 13:09:14 +0800

    feat: add frpc in host patchouli and frps in host hiyajo

---
+++ modules/private/hiyajo/frps.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:WOgVFdtJiLq/LkB66tCvjOeksWf5zTD3ZPqiWpTPipf0ZZjC2xtG2v8IxxGFr5loafLDj1y8T1WfskzjNizNwJMddUFl7FjdJTIbVObpcQXpvQuzVhSjwJCqpzgRac0X9V202veNREQyDpaXN9KipIK/TSBeYyTfMuttFrHo2WMVTcIjDiGR/6mHg+FICOWtfHRA/9+o/yLTc8B8pBHDWvAGYmsrWIF2Qo26NKeYrB240Lx9QxLgaTBlMRXfcHo0A31il6I1HqiaFQBy8N1pop8vQyUJSKq+NN0m73/DdwWVX7LO4yWNXHtBF82rMppXZK1AWD0ZPJ43w3BrW5KUUw==,iv:K3y1QzCDoV+ql8GIxmleDgRrUsvujeSULrXr+vOWg7E=,tag:hRL71yKzCdYfbvKOWhZfSg==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBSSDhRNlpqNzZMSVBRYzZS\nTFNhejdhQm1RN0MrVkQyQmFBRUhad0I2LzNBCnFHcTVpTm9KeHNZQlBRdHE5NFN3\nZEliYy85b2Y3amZ6QmxRa09HZnMvK1EKLS0tIDlKN08yYW9WaHkxMm5xNEtKQVh2\ncm9iczc5TzFhb0FSWllTRFVFNTNJMnMKpNwSEbUFFA1uAQmSYMTvdgNW6TfBpD0g\n7lE52myJoOhOhJ2GtfcCg0nWK3fhUkEN8TMyagwLkPgfQVfwBZ7GSQ==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2026-02-06T05:08:07Z",
+		"mac": "ENC[AES256_GCM,data:IN7T0tGvJS4UktFZI6u2W5sym9+UigvFP62/3pN0d+MiydkLZ/A61uRRwQCHpBnHsXhnjnOWGsf+C/JdH6eqG7aAX83ky/OpFCqbQUbxukCQHhXzGPboeenA6gNhm2cVk5+NmROV8BphH9wgy8kEqUorepma++QdUPogtPLVtUA=,iv:xPo52xt0sBzGOutC7OvYJ16HH8p0AT0xLRp8l28zj2I=,tag:d0wpz5FCEzKXdijj0PcALw==,type:str]",
+		"version": "3.11.0"
+	}
+}
---
+++ modules/private/patchouli/frpc.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:TtrQKKbVi3nes3Xym/krtj1f7H4U+OZsDtzlXCVP21QrO9CbRn+1CUfslKt70O+azmQTKD4ZW2QrOzKvDbii4SzzaWBLDyM5S+q9PStt9VahL5op9KRH/otS/29gTQonriMl8l8KR64R9gutVfyHqItiZhdycpZC8eX8L+xoOCO2ZTjKcL2mh4rgWMvlw1+627SvtLbluv+QjAq7WX6ORMm3x3AilFUwv2P/GCVJa40jeayGLVS5C6qSgygmvz+/mA9H9Rc8iLwY1r9kij8aEpLC3hHnF5YL7gWQYErQ0o7QhS0C0veUNgnxzT3DCl/GuhinTcpmax5AT0tBy0XpIGeW+irUp5Nj5iZUdclpDAxTknPYMpVnMW7tvzghPvKWM4o+qQ2u22eAU5K31OBNBZBsbxK2BA238nBtX6Ch2anLau+rWZ/LZ2EgY0zfu0cEM01pSNvPtxMqZ5LEbzYZUbxEoMzuhW1EyPNBlB9f1gu5YqeZRdGN/Bz/QOyjIrXknA==,iv:+62QsZksZ0Wm9dACu1Ktfnnhn/OMyPoVDRUnGTXI4Bw=,tag:QOulgTeFDjWWMHP0YSZnVA==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBBc0QzVnFtWWliYWYzTUp0\nNzhpTlgrN1RxVFlGanZoelBkeUREVHNLckZrCkJRTmdhY2JKM0xOOW1jeFZXNVFy\nbkJ0c0pZeE9wS3B1bEViNVd0dFpWOG8KLS0tIEszNVY2R0RuSnBPaXlqcFlDN1Q5\nblJpVG1VbnVkTzdETmlNYUtFbEtTNWsKMN2slwOFN2xnDKkWj7ZkBe5/ln/A2YaP\npqKNmoWlISNHrYBGXZIKhcRDdsqSUCy7pF1ZwT45ljUsWqoqZTc6/w==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2026-02-06T05:08:11Z",
+		"mac": "ENC[AES256_GCM,data:C+37peII6vwgtuPCn5y3Ml/3UZOWLUaP5pxeEKd+vHmrueu4Svd0E4jkZlOTj1iTFbBU5vnU2ICmxxNrbWtAK4LdZBfPsj/JKs7Sc2mS/5g7JHdFdKVnc8MAZ9YXLNhCh8qbdyL05pdc1CVzt07O8qIIzPOTigsp7TX/MLoDlEI=,iv:KVzejNBqWb5xIG4RlO4p9pCR7vqRBG+NI5cBsbFJgmo=,tag:V8xozmj0Ic36rPI0WKirRQ==,type:str]",
+		"version": "3.11.0"
+	}
+}
