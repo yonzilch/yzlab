6590a24f1c112453feb3cd0e175377155eb7347b
yonzilch <github@yonzilch.com>
2026-02-17 18:16:14 +0800

    feat: add container-cyberchef in host rintarou

---
+++ modules/private/rintarou/container-cyberchef.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:uiQnAuIoyQHgvZrGMy5maxi73JNh6q3YAgZLvnGOobsQpG5O328doWDYFeLDeo2rUdx04pD7QQ143zFvWV9NDWTC4k7vDFXdde1X5AT3DaR7kG2iE+0+BCpnFjXkIU5TMqo6NjRThKc2qvPW+zYKIByUypOAbEMadoTp32d0jOWLquzU5q30zzBVcpaS/WsJJGfByZt4fLHEglsYqH/RdU/WMaiKFOKHFfdbhunImWbnyRTf4lEi6w==,iv:5iiiFMeH0ZLoJk5iVk75cw4FPEYubH0+DSrFghAbPa4=,tag:LeGJ3sj7QcM2aWotZ1Hw3A==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSB1dHJkNGl0NTRnSWw1RWYv\ndXpoSzlhVUdqSk5wc2JCdmpRcDN0a2hTdEVBCkpzYWhFbjIveDBUbS9JZVA0YVFh\nUXJMci9heUhacGhXZW5aZk1VRVpSK00KLS0tIHpjemsrUzFlV1AvNW1HZjN4c2RN\nbFZNeUozamFSdTFmcTMvNXFiVDBXd2sKg+IgAlLWyx01b0OxqBgoJed6R94dd1il\nPcA6cA5MbKBxb6n/soZgRJ8wXVqYvuffOhZsUK1b6taa04xbZzA+sQ==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2026-02-17T10:15:56Z",
+		"mac": "ENC[AES256_GCM,data:aDh6MNZYI7I1NZTlutGJzRiHd7B77IANM/F4oz552uV4qrlcawd3K43yoMfFe/svMy9WKTvUCCVbpC/Yg51+5GCGpsu6sEI+VX3LTbpK+2ZkdlQMJqaei9v5d32JReok8i37eAPfPaxfeL5/KvjjSMAJoepsu75rlwrpzCB6yy8=,iv:ykulCH/bry9sbEOKg1rfvWo32shzm88pbvXk5Xixglw=,tag:QIISaXNOvOFRMFOzK2QKyA==,type:str]",
+		"version": "3.11.0"
+	}
+}
