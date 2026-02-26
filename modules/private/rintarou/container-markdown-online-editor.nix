b44ca08e2ee4131a0bdbbd44e2cbb5fa2658f6fc
yonzilch <git@yonzilch.com>
2026-02-26 10:21:48 +0800

    feat: add container-markdown-online-editor in host rintarou

---
+++ modules/private/rintarou/container-markdown-online-editor.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:JRZzdDYMWYl9hoJUS2XVdZAUwBa6twDAHeEIIciVu8/kI6SrBA++p+ozfZr7Znq1hWx4YTgXpz35oDRcn1uRJb2xu641V8yPMkBESKhNVmNQbt4phIojdvQySPblwJL9m1oKKSL3aE3ZHua8m6Vm18luBHiaGjUZmlFffFR6pQCowninaMtq3A+oq0RiixsHV5HrZ/9phFbzbW3NcAp9ceN+yhYbe4CCoj3m691dPZpWs3fMgGzrwoQ=,iv:mz3bSKaJN+o99tFUMiAXr3g3/2emSy0f2qPGyTXVMaw=,tag:VPKCI5Pk45B/ew4EksqAdw==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBZZUczUGhHSzkxSXJrVkM3\nTXpXcWhSVXpod2tDcmVUOGQ4NFdyUXB2Z2tBClFycWJIT3ZUUnVQUXdZbE9EdDd1\nci9tc2daZTFzdDQ2Z0E4Z2ZlNjlVRlkKLS0tIGduVlRsb1V6NkpaUStvMFk3UGU3\nWGh0UDJ3L2JMQVJHNmtyNlBUQld1NVkKEdCgkUV2m4txG4unlbgqZ6qJb2lVe745\nID/mRaUfaPvtHESHBc2dlvj1X5AI1xXkKyCCSSxohtR+/6gooB4QuA==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2026-02-26T02:21:18Z",
+		"mac": "ENC[AES256_GCM,data:hwH/rd00QTH622DSntBd/IozMCsN+TgoZ5ggwyLxb1kwxP75hVxAUW67Bd9wbcmEEWUMAjEZtyNHz1tB+yl0u0eWhYEWASQ3hS55gf4W4oVmS+aM5CNAySwAy3ykfYKFKFJNv8Tu1f0mZ/i3PPvAvHZ1R01mLjFarM8o7CReBlE=,iv:04CwbtxH89x63hEhpFe8/cpR2q1/B06/R+CzloHxNH4=,tag:10e6WX1wXoKD89GHQKuN8A==,type:str]",
+		"version": "3.11.0"
+	}
+}
