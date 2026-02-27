064ed7d17b138d9d8ffa43d61334b9aa492a7819
yonzilch <git@yonzilch.com>
2026-02-27 21:25:09 +0800

    feat: add container-elk container-reactflux in host rintarou

---
+++ modules/private/rintarou/container-elk.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:PK5WvcST4454O57BkFgAQydoV5F+PNhTJEmXOOBPKsBQDul4c3Akj5m1o+H+AA06vHMkWVrDGlpGJlL30umVVxKTtwoX/A6bwMKX1453swGnR8wwRODRR9brzZlpPi7xuOgnxAU4Xkp9iMoZaLXkwZqlahOJ3GVC0PahiNnYPoMPufDA3neVDPtCo0401h1TKYIUiTY+dxdmXZvAehYtcTEMZq9SBXZbMKer0fZmzdOMM6NaMWcLmtAAaPZnMXfeHf9p2c2EyA0MGrfrfriCApxehUtH1bVALVOY8e6ZXaE=,iv:Tx3oaiH86rJ6vjGlHbnZyXlgfURxp8JbFZSWQPZIyAY=,tag:t4TMKozGwqXxuZBcGV0QAw==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSAwYVppNXpyU3lzUTA1Lzdy\nT3hnRXNhQ0hzL0JST2NZTTZLK2E5alViN2s4CjZ3RmpNZkxOcnVNUGZkY1E2RUdl\nc201TmlSamh2b3hpbE4zWEhxU1FmNlkKLS0tIHlQSkVxN3pxbnVwSDA0R09NN2xz\nOFpta2RpQjJiSnY4cStIc25IbVJhdXMKx/OH0D5rYKi70QyiRtdd8D2HUbt4Q7sw\nLkxp/ReCgLUx2feafCFFkM95EQgnu5sLsaF5H8NN/j4gIvye8q275g==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2026-02-27T13:24:39Z",
+		"mac": "ENC[AES256_GCM,data:d1pw2V4iXQ5hTH50uimRCXcgadmYSg6O4cRjbAcChjhvX9fHmXPweEPmVtfqx60vk2vUHNUsmsBQx4YIyFjVTLBP8Px1MbVctLCurJrXa+L/2mtK0mJA2UweQkDAIz3FjRCFpcF5ypcFTmOzizEfJ6rkmzTKSW96rsbH9+Qww+c=,iv:ZPH3ugDc9vfXEnt8Ln9bE6r8z7x7AQd5b1fMK4EeOj4=,tag:E/4vt9vNPnN0R5/xqZL/0g==,type:str]",
+		"version": "3.11.0"
+	}
+}
---
+++ modules/private/rintarou/container-reactflux.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:cvmhc7PJe2vJFynepzDQCAj0k95OOrbuwiJoW1ZHd4OeFPG2ZN7/hQW6Nx++gu2rGbLuf+0s4JmsRSTD9WUThzQndae2strP8zqH5z4ggWgIBnrFC9TeSG6SVacdltc9NkEjr29U9WDpvUGwlqoHsLMu4PrDIsuPCsPtXfMqQYerPXE54ivv4/ulReBOZVqQMZCnSbMPML6JDOXQT3r4gMjmYLcBM9gTDFT241XrlLMjlJ/n,iv:dsrZDLZqHP7Gw/SFrRgrbcoorf/LjzcbuKaDYfrGRH0=,tag:OKitVZBKWPvMBhm8Toh9fA==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSB2c0JYVTFtQmQxb1ByUU5k\nMjFkbzQyaURTbi9RdHBnNExDdjdxU1B1ZFY4CkR1ajQrWGR2elhTaXdrWURpUkhN\nYTBxdHAxaVVxWlZYZXorTkIydVRqMHcKLS0tIGgvaDl0UTJPcThNYlh4RDlTNC9G\nR2tZZVV5WGxyYkNrYW1rVWh5VTk3eUUKX6QaAEwY3nOAejE+Ahd6uLrqNwm4Qer2\n7JJolNElZA4/f1NwLkgr2RPMks4zM1/GbOxteV4W5EZi6u2ou4ETDQ==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2026-02-27T13:24:39Z",
+		"mac": "ENC[AES256_GCM,data:I4VOsP0ktqPNThhMeGWIx6cpn8zZQOYOsjdJRSRI+pxjUbtpZFNrbhWeM0qj0/aADsukwWUa3pyMwwiw7MXut1/pWsDMVvllYjkvcErp7txwdo+SpNeeQHa910MbiLxbxg+ooNytbHc17xakl/AJurlvnlK1+JUGBDvOl8eiWPI=,iv:/7orxY/UVa9sFwkvGuCVk3AOh0/JaaZi23Sj8x2Z09c=,tag:Q+GyTCOvBtatWU7u/Qry6A==,type:str]",
+		"version": "3.11.0"
+	}
+}
