2f88e06608e6ee67268ebd7bd59f346563d35852
yonzilch <git@yonzilch.com>
2026-02-28 15:51:36 +0800

    chore: mv container-komari from host rintarou to amane

---
+++ modules/private/amane/container-komari.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:hDdzzSw3aZOANmO8pOqv+MKlMgUeAIs+iU96kE30dTini82LBwQJ3nO5nkcy8gBEtXloUnIOfAtekp52AanbZ0Ew1SE9BFfScPEA+2ua4bIJiVlhZyIKbgbBk8nA+hzbPvTE19W6WMuibRqfXSJgoG/H1N99lxhapt8twF1x4DYwwJkyrEDVZR0joIWBefcG+8s8Mw7Vm081OuEbyol7qtZ5tmTr5OjeXoMHNpAnF+rsZgeTPCiITxfEKTxIVFyzFE1AhX5o/8StsS+g0km5zOUaLDoin/YmhL+zLViCdFsSp47JRweQnRVm8mORrhk=,iv:+A8pS1cDanKubjA8YMWvViZYnsk2xYr4a1WdmqoFB+Y=,tag:ymRyKYSPK8hz0hmTaeAM/g==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBoamJabFROWjU2NWJSWDRB\nUGYrUlM0dklONVBFaHFOQi95VG50VDN6elMwCndRTmJ1RkFyQ1dxbmE5Y2Y4ekh5\nOHJ6T1JRUk5DZmJuT1hMWkFMQk95MFEKLS0tIHBYRFMwSWp0SEM4a3JVcXZPNDBx\nWUZqcVBWNlZ5cXQwalNHYU9zT3VIQjAKOpmLSvAX31zNHwDos3HcQB9WjeJDcSVV\nS7mECoGN3aiWjtjpMKheITwTmzHAgsjJ8YZ890djLiDPSgnGpAshZQ==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2026-02-28T07:51:02Z",
+		"mac": "ENC[AES256_GCM,data:eIX5ymZQ0EfGInPU0KSCdXMEExraW5f7rTOCRtLgdY1StgkErcH2mueCLXLBEg5ytSMNOOUSRCMckjx7IjL7+OPVqBaylnaD44KIqWu+an+8B32F9Q3VkjN8AZeWPMunipPuAsbiZjr6OK2rOMVuFcjAa3z3twRL8IVcrnvC/RU=,iv:gkMXIKJmdDBIIVtWtjqEZ9n7+oszQAQSPVAIJOBroMM=,tag:atF4UdCHq5GSY1IqLYuR6A==,type:str]",
+		"version": "3.11.0"
+	}
+}
