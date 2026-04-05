f6b73ee131f8d614a008b2d00c26621ffc9758fc
yonzilch <git@yonzilch.com>
2026-04-05 23:48:06 +0800

    feat: add container-tuwunel container-vexgo in host mayuri

---
+++ modules/private/mayuri/container-vexgo.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:bFqgnfKCLcRYeSAsdDYY1rq25hUAlfDjDr70hBSdlPdmFekh53s5jPPtrplppZ1/SLPyhQ3iwYHtv5iJKeP9Yb1xCqGGvtcIuuEgWpqoGO3pm7v948ltnAyvHc5fgP/521gbPaEi6frSWW+Lcph53KX2PA6qGDvyntw2B5JQTT0vsbCFA8DrcfcddM8KXatmx4sKyHScKHyo5DYQ0bXY6KWsI4MIE0itwYkPQdmsOczPXtWrnxiijrjvrEO6gN4/SMT7WBcrwJEh93b8HbNIJBY2Oqdw5RdMH5ZGDyjPNnpxznUElU+4DVGkTJUVMoFv3Y69PupUqqDHQxGilABiHjlLlEXXyPiq/EpRWZ/axXEOLDidpuTt75ShoYaruZgd+cdJAh0/NEPjoUTbjRyG1jVDI+X7lB++BDzMMhvTFg4ddkpbk3Kk5lKVsknOEOLIsorOxupNQV1j07ISHn+51wM1NFQm0F3wZNqJjnT4h+rljsIabLCXlgG3hJ/TMBGxMoYph64P44iAjud1fnHfhoeMOccwQ1F0sVDruR5CI6uHYceiJsJB2ccfV6w=,iv:lUPyiGo8lnZvEmGr/S72ocTWHJKtC18/yfYeMs7cEqI=,tag:iYfaiIyZ7F2lDTrzhUdmEw==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBQdVVNM2JaRzduQ1dROWFp\nUW1oaG9WSFBWV0RGTjJWd202RDFjMGJhYjE4CjBsZWFzbWJ1aFZDM1dCMFVRUjNS\naVlQeEtVSHdvdzIwOEFqeXpEVVFiS3cKLS0tIG1vczhZSHNZalJBVUpwMFEzNWE1\nSjltRUt3eEF4bE1RYnA1SmNueThkMGsKdJrjLEeZisyqQbxh20z4jZBrfOZdOWZT\nvbRxpualisqBINx7U8J7P7qbun9RBY4NZ0IQwEIBKI9s7O8FzMs+1w==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2026-04-05T15:47:30Z",
+		"mac": "ENC[AES256_GCM,data:/4VR9eAsr50iR4EcjYU1OrDi6HGXcq1zInpmtUuYsxPYcLFNqjFxrJNBLeqCNtFhSK0Uwjw5WgfTLKUDqxSXCnteYVvEXA9L39sH+cGCxrTi2EbwtkwdLU4hHnTNl+19rv35tH+k/Ycai+wEeK6FScrQbUKaH/qW2XBklNPnpoI=,iv:Bx5PKGzqEDkAotUGPHpdhJAvc2UFjQ1cuWkdfJ0WGB8=,tag:1W9kwB3NFw+GZEEverYglw==,type:str]",
+		"version": "3.12.2"
+	}
+}
