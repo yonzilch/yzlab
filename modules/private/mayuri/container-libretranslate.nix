e7d509add770ff8d865f5e0dd41fc31fb421330d
yonzilch <git@yonzilch.com>
2026-04-08 00:15:08 +0800

    chore: mv libretranslate and mozhi from host kiriyu to mayuri

---
+++ modules/private/mayuri/container-libretranslate.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:IWS5pYGqDWiectCV6zPjy4FxWsslnpyQscJ6nTmsQWypSliqSR8N+yF8XNvOgmziIHSfDamWy7TUB4Qe3RNUW3T+QqOscNxUfUwMpx6/G3msehto/d7KAuGPPPSIjCDFfm91aXV1XWi0S+MWTz+AfKjhId+YMAsFAs3ERIsD1m6te4GuI9+0MDhT2VXsBsj8KyzP+SKjvttxNy0Op9iiARRBIUgI0du7LBM5viN3HJaBrE7hhVChLmFUBxyv8u7kxYUTVLIc,iv:EUtFlbdwA964Rbw9ZX9k+4PHQ72zCgA/K8EhXtwIy/Y=,tag:mcKM6aKpNhJV80cdZoDa9Q==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBoU1JkdEJGMW5XL2ZEMis5\ndll1OXdac2x2bi9lS2FNNE1OeVRkckpvQkNnCnU3c2k5MEJhVzVzQmpsL3VtNCtH\nM3Qza2FrWFd2RXl0SVhMaGNqUjdkVncKLS0tIHo3VU9jRnZsbkhBSk9sbzVqcTYr\nZ0JsSmJKTE8xa0dEWXdvQitMeHQ4ZFUKDlyL8CSBC+B5sgzjDtX8trnU56v0Vw3z\nigN0dhU6Nj0pn91pOTq/LKMzo704hXBr9s2OfwtuvBP7145BFkjVyQ==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2026-04-07T16:14:29Z",
+		"mac": "ENC[AES256_GCM,data:oJw1y7B47gv6zq2fPDsjO5nSNZKwD79s8Y3r1nNqykZ8GYSFP3JIpYsXCP5LP2vo+S8xwCWE+ChVN4HW8v/fjU1B8ye9dXGnicfXN9xc2RGc9gJ8/oN7QgqRsU2xnFhl/48P+mYZkCcJQY44QWPxuTbFv80pbUcFijhj9Z82wC4=,iv:NxKDC91Ok/UgGgIe6n5aMUHGGtK2Kk55Q+HjyVLjhHI=,tag:h3MCAqOfRWRYBKJOlfPzaA==,type:str]",
+		"version": "3.12.2"
+	}
+}
---
+++ modules/private/mayuri/container-mozhi.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:Zh4DJgi7j9Fz+ZmuST3pWNoW1xLKhTPU9lO1YGuTY5emiiYE3dZ/6GkOVwxdEhVo1eK9KIUbmq4AvPyilKS13UqH+gbbICp6fmXbaysh1DlTEUToJlBaESmvc5erDNjoWgv1vJej56LSahH/IuDAYNair9Ax+HgqXF1lAU4kgCvH0eHGrn1RBueuCANlAshB/9eE1klbw06meanSKnHi/PcuBg29d53EcGa+gpMtiKizT1/A+8B/ai6CHUpcZrbjHTsugPZEKO3g1w2C3R5Xp6qH9lflueP9ui+R3Xvnd1p1jIiLTrVxiSvE0WfpDVN+BOUQsWl0xYtVb47tS0G27gsoX3OH8n8pJ+D4X7Ho/TFXgMekNatmGAJD5bxnyuqVGP5h9QbVdPTwASBotr1QA0K4lK8rz0NiIns44TyB/qU+aNbDBoqcTR1CD1l7lHE1kFbPeegDuPM4vZaEy4QNpQqx,iv:I4iXw5+gX7hz5q6GG+4Sp8NSlTC4XVysuPuGknzqGHA=,tag:klvvfEg5hy247Pjx/B1vUA==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBSbGkyUnBkYnhpZEhjeFky\nQWR4Slo0eUM1aFpISEtCdWFaNTI1REg5TkM4ClkzN1FNcWZNL05sSExhK3BDeHhi\nSlFpRVY1WlNrdjFRcjFpSXp2TW1pNmsKLS0tIGsyaU1XTW9ZSWREMzdpR1RnMkZH\nS0QwSk5sMWZ3SWlFTDJUbitXL241YmMKdTuEXyeOpTqQRs5JRdLt9vvcOymesd0V\nCcQ1NCLN4znpvcpeN0MZUqn+FMNFc119+e2AytymJOPrewkh8ehLRQ==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2026-04-07T16:14:29Z",
+		"mac": "ENC[AES256_GCM,data:Q3JKvMjVED4ujzCSoJfek+qlEzy1XqUEKOUQII0vVRwiEKXZMrqLE/AP4ZDmfpfHfkVSyELFatzfC9WW6pUn4UqjsYUe++oXLb3UMHmg3VMsT5FwrtYtJHZAJJjvkAGUqYaFN3lLwmC/OvJ+uSlG0FyvwWU9lntShgiby3dFdoA=,iv:XfJpgjq4G12vRBKwfSFUMvJodsY8zQbz0AZ3oBAndqg=,tag:ZWC6B51ZqEqdY89ua1Nh1A==,type:str]",
+		"version": "3.12.2"
+	}
+}
