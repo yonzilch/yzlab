edcb7e7f292bbb66768c0fd0c986fd2a227c23e4
yonzilch <git@yonzilch.com>
2026-04-12 10:14:53 +0800

    feat: deploy container-alpine-chrome and update karakeep config on host mayuri

---
+++ modules/private/mayuri/container-alpine-chrome.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:VwmYE9YdjUg9TdSolh1lvsQv1cNMDenZ55kAtcTHeE4pIlkZXBbZYjuTIOZModXtrlUuYNa8cV4V4h2N66mwagEMTemBo4RPBjQ/cSXwI2MUtNYJqVVRshVWIn9xop02I5+AawpnvyGgybqh0zQkoIGI/vcDfKR4cqeCTe3HZWtsYO5q+aI+3B9iD1gjDv7yJS+YzavmWVGJ6HKlrC2DQUqpd8a1Lmu8doGdWTY+v0dLKtp2tPd723nHVxfM/rN580m7JCq7YTlw/Vh/QyIqULkiSNZoq0h6OZsprafX9kxAj1nS3LrEkUs+JtzNiPjRXd71e1jOtzhzJ3Cnswl4PEUmBDUgz4y9VWjKSy8d6YZ0HsxBydSAsDitcdePNDnF+Bbmnk6Wkz7y2CXhLZsxP/fVWlcQkR5Mw9iowZgiQEpnekvCz5jlAdrctcfxRfopeqIBFWOGWdX9HTQtlb2cmg53DurPGUnGIEdaC0HUOt0/Fzn4thtBchpvrRH0Ly/AY+8P/NgrLsZ16/ctPrY=,iv:J3oEEoEQGpZaUyRKiTz9OD47uuXKm6Fj+Qv0PJxidkM=,tag:WDzO/5jWgbhmotapyMYCkQ==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSA1bDllWGwyejZlcy84TlJq\nb1lOTTNITDBGSC9NY0NZNWtLcmllcS9PakFZCkJtQ0JzWEVWaGdVUytOY2xZV1hN\nbXV0bkkrNVhlc3dHbkJGeHlONlhpb3cKLS0tIGFGeHdiVmkzSWw5d2w5YkNXMHM2\nZVo5b2VNZEZIdE13VkV5QUM0ZG5FSzQKnpyWVH+5dJ05z637+S2roWztpboc/vzG\nUTBwk6ieRXJxTe13a2BQ+mVn1xAhyxnHwgYg+v4vAQY6FfsFHYKy+w==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2026-04-12T02:14:05Z",
+		"mac": "ENC[AES256_GCM,data:Ays0omjq7iLjwjjaOy2GrL2vmCA5ismY/mtZDR2drk0qkX9tYM6Sw6lJ4xwRwuK4wQLMjj7vPdTuftIY45LUefgijmpUBVCWSWL6e+I6IgUPmCL8XwBlEprc18ESMENp/Gm5F7FYbrwVTOnEiPEfP+l+i2EfVm+00EbMQGi+06A=,iv:X2GpEoVYZ4s9Qvv+C4VUZnaUbtfXxNtwMZ8xIliAgZY=,tag:jz9A5ssm56l+WREYdc3w1Q==,type:str]",
+		"version": "3.12.2"
+	}
+}
