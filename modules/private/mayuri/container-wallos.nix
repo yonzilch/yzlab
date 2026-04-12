c13dda5ac13302fd52ba4a50e996d70d5a0a1a26
yonzilch <git@yonzilch.com>
2026-04-12 10:43:17 +0800

    feat: deploy wallos on host mayuri

---
+++ modules/private/mayuri/container-wallos.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:YIMzLpua7/rKCSx2dEY2Z4Un0XDPgWYYPtISK80UODhBsAzoFmkicFA+Yy29yAh/qL/kwW1Eq1l9z4HwI03hxv/eypl7wFWdnOX35hG4pTfwIL+CJRp8SBg09LIH4Litg2G1BvbEHB8AvYltvTvKwWqie+wwMYKLRsnNIDTK8ETNvuMaL5uNkb9N2kklhvq5iwwWGCN5/2OHdVpTrZNHWsedpVkYf1jX9COokhWxCurAwoNhotiU1i8ZTaKjbRehuslZN5Hun8ETmy/tzEeuHQQWAUtZGJpGD9yGHR2eGTYIbJCyuBMzsGpNwk6TqLX2psFpD/hd+8dTTSgDaAMntzPfLN3uCiT4IHgimm+fcDJFXEis9APMWYnv,iv:aLZKzuKTYLx+WswPJUJQ30IbtGhfh90mycxUwbI125o=,tag:OkKFdvARXUyS3f4eYw/pfQ==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSB0UnJrSzd0S0RJa1I4cW1l\nZ3hTTVp1c0ZZTHRZUHJyeUdKTnprT05ueERjCk5hWTViS3ZRZjh3SFpEVU5kWUNh\nenJHeWNZTUhRVGIrYW1Nak9RLzRxWHcKLS0tIEhzNTNhS0ZDS2ZRSlVVelAyZ3M3\ncnBZNG1FbGE5MWVNUzU5MlYzeTRQaGcKaS/cVUdqnZc9Ob85U/25mNvvdIShRGRJ\nOkTq13v+jpv2y2cyslhyyhh28QWVMhL2RjKD0K/X4fWjpHHKg3ptgQ==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2026-04-12T02:42:53Z",
+		"mac": "ENC[AES256_GCM,data:j2O9cc5nzbdQnvFsVufrWJ81ltkKV2srCenRllhcs9BZqxOxrYx/BqNNLfWRd8n2SWb7nBnik9dcf8l22bGdJmTYMTBjDdY3alLNpd/quGPLz7iBCv8t/10KvOQBMegvoFL7Zws3CM537NZc7WuL4AwHQEDx1HVn1Fd2kCnot3E=,iv:Ngmk+OqmsCsAYQmV+SVTPy4YdqOwHWvoHUZWPVJS+1E=,tag:ASblT1qWImyKR0rNBuJqzQ==,type:str]",
+		"version": "3.12.2"
+	}
+}
