f078d02d7361b7c31c4f0c615b4a59b06dd1460c
yonzilch <git@yonzilch.com>
2026-04-12 23:40:02 +0800

    fix: prohibit anonymous volume created by container-voidauth and add necessary `STORAGE_KEY` env

--- hosts/example/modules/container-voidauth.nix
+++ hosts/example/modules/container-voidauth.nix
@@ -4,9 +4,11 @@ _: {
     environment = {
       # see https://voidauth.app/#/Getting-Started?id=environment-variables
       "APP_URL" = "https://auth.example.com";
+      "STORAGE_KEY" = "";
     };
     volumes = [
       "voidauth_config:/app/config"
+      "voidauth_db:/app/db" # Do not need if using postgres as database
     ];
     ports = [
       "127.0.0.1:47126:3000"
