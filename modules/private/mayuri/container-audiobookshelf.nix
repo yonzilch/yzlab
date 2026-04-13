2df2c91bea6f30be7acbacc77ae36716e30f00a8
yonzilch <git@yonzilch.com>
2026-04-14 00:18:09 +0800

    feat: deploy audiobookshelf on host mayuri

---
+++ modules/private/mayuri/container-audiobookshelf.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:2QjQBqfKZ55ofa0WHhNmYv+lSpfBZwrfrz9evIcSxHfbeU3y/npcWyRYvityDnLaNW1qdCRYzgVayOjjRNgBAfd9d81zTG7bLAxDkfNeRL8auqDeDfR8p7iC2tBaVUvYvyWkzg0KHj9RuMheUZmnn9xOWOKY2P2zJ3tVu2RxX5itS/c3/7K3FX9DzG3lR9Eq/WpFLbjRfK/U9R3Tt9uZiUXrDxPU7Swl1eIA4aLNy8aS27bVv52xmJhgAQ3zJRvS5vEd/eUxibMIEzrzh2ZhsiKhoCQUiC6inemb5JvqknZ3f8WcNuyquIB+JQIUt+MkikdKWFBpjlYjoSe4nawvWvJph7u8XA5FCv7JAcXNNGxyhp/LRY5sE6qasr7J6TD5ILgJzuxh9KCQbq9PDZHpE7xWjpkkpjDs27Y+bWkbwslnrl8SvLaUHihpGSnmLl54+k5OsKtgyzoDeVTcQB7M1CfeWI/R2yyNKy4zK1rdNinSfrVjWpSCyPzk2nE2z2v6SBsClAXHDDm2vj80tW0QdLHffO8IW77UchVLKzEN1Kh09BeYeC/PYsUug13uOhHu5hthuxyxWKCoTZ8IaURjWdNXDX9ZD3hibt0O7BMnnx+dFIJgXcNUwg4AMdXJV7+Nc9qgWHdw59cy3nLJSgZiLpShHkfiU32UQKIaWrvPd/ccwpWBvtSVYQ==,iv:MdGo5nl84FI6fcXHP/QgzK6Gr/6oxD8sNWFmrKg2v0g=,tag:EqbB9gimY2T4FoOeKXsD+A==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBHQkdPZnNCSEZSVzN5UU5j\nRE91ZHo4eUtOa3l4Q2hKbk9ITUU5bzBmSDFjClFhbXZodDZqT3V4M25WK25iTSsv\neUhHTzZrZ2VzTHhWaG1jdmhXZVUyYkkKLS0tIEdlbk1ONmoxY0gxclZEUytmUTdZ\nZnZQclJYblR4cEFoSWJNMmZJajdiUkEK/l6wlOqJDcA0Ty7BesS+tIgNMcEiVYlM\nPlw1Op/wlWOg0HwqR9vTMAlMBPNplDCdKG4S2uyhuDKihEFvlZyl6g==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2026-04-13T16:17:42Z",
+		"mac": "ENC[AES256_GCM,data:FC080SmGWGAnjaQsvUdVYHtlVJFyDk7xIwhMO+mr4PdO0rD2b+SZu4RtbRuyWYci3Y1d20Ckl1myjTIy2dbn7EMbOdR1ZbH2DSVfgLZO4duhvw9jN9SMEIVge2QHdtx0tUUUXtxwAL6xiNTKtBJAFWiYsKlvQWdvEBMz4KQzC4E=,iv:UigyQbznPow/sDsJ4kYVkGrze/gWz9+U0ebuz6iAtHQ=,tag:UynGYDc54PlcKe/xPRGX6w==,type:str]",
+		"version": "3.12.2"
+	}
+}
