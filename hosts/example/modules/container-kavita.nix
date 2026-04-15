2ff9f72bdba44e450d053059cff25ad600057aa1
yonzilch <git@yonzilch.com>
2026-04-15 15:35:58 +0800

    feat: add some classes for container-kavita example

--- hosts/example/modules/container-kavita.nix
+++ hosts/example/modules/container-kavita.nix
@@ -3,7 +3,10 @@ _: {
     image = "lscr.io/linuxserver/kavita:latest";
     volumes = [
       "kavita_config:/config:rw"
-      "kavita_data:/data:rw"
+      "kavita_book:/kavita/book:rw"
+      "kavita_comic:/kavita/comic:rw"
+      "kavita_image:/kavita/image:rw"
+      "kavita_lightnovel:/kavita/lightnovel:rw"
     ];
     ports = [
       "127.0.0.1:5000:5000"
