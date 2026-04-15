23518f9534b32236c98dba26894c851b8c987afa
yonzilch <git@yonzilch.com>
2026-04-15 18:56:00 +0800

    feat: deploy calibre-web-automated and kavita on host mayuri

---
+++ modules/private/mayuri/container-kavita.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:GBhYek+tWndXl5zXPENUsNP039wKsYPKcao7q81B4ngvLr2tbl7v1YaEnkXk5UNdh2x/5jtebTbnTAQvqXyLb+wWMa7GSqfLKTEpxM1qvfd165y0N5HcYHek/J64AR9Izu4NwXiIBg6tTanViuih4pdozFUNemIs06PqXW4J9lKwjEZRnVuCB73xHnFr7lVbuReI4drUp32/Sgt3MUnQwp+yr8M8KPGLkAQcDnJnBS6vIJrB8vDdFCkNJaazYHUI7xsw5pPVSewwhx/zGx1AVGX6raWAWznLof710iWVYjPYBVMhbyOaxhPn6QrOVcM7ZKdSsQLv3rbXJglTy5wVHN7wlTYG3JpJrvyRhwARTalyYWVU9zlPQ2eGKjgEuR9APWD6D8F2MI9GvdZqk+hh4qEi9GhCQN8tPqRI0HrhM4IcTr/fWlgGHvtFsH+7GTtctjzw0En/Q02in/KHAMbc6oBzPrHSXS341l2Aqs2twRv6PCE4oD048x2e5jNzjA==,iv:PaydZp69rdQJ5d0Ib4+Cv7yS8LRE4aR8kFraN0xgn4g=,tag:ExdNIuY693bifRJXmQ7KvQ==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBSaGR3eHhvTVdLNFlzTTBh\nR3hqcUFmYzRzU21DMzlacWIwWWFpbHRrVEN3CjBTODF5cUp0MkJZelZlcUNuU2N6\nRjNBWmpZWm0vOUFQVnkrSlA5dUFMK2MKLS0tIEJyb1FoQ2tTN0FWVy9idytZUHpm\ndllQbm1rd0JGTENPd3JwQ0xyZjRiU2sKc1oql9WUktCHv8oEq7sT9ZRJR/zSyRuU\nVqMSaK10M74R0vanE5poK2MOnZFQOZI11pJSM5nyztZHRyZtoKfifA==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2026-04-15T10:55:27Z",
+		"mac": "ENC[AES256_GCM,data:xv7k/NW3kuI2ZUN7sIfL+tvE4qKUDOZwbcQ7IeYoxw+Yl+MqBFrmuiStsKfvYf7ob9NfrSgMNd2iJdQ0CNoz4mW7yGueYxCkHAMygDzcaADZGziKNp09hHdsjDj6HsV9l7If2o3S6X2xv0f6g7bHgrxRGcS8izLUm5FvgyL9Zec=,iv:30kvRG/KRpaeR5R9sxAzk2sIMINQoFMdQl4tXEHhy/o=,tag:uwqPyR2CpisaOz5W/ZS3xQ==,type:str]",
+		"version": "3.12.2"
+	}
+}
