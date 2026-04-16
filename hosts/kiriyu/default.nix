d4e184eee0345c09ad4b3bfce23c61711a782e33
yonzilch <git@yonzilch.com>
2026-04-16 16:06:25 +0800

    chore: reinstall host kiriyu to encrypt the disk

--- hosts/kiriyu/default.nix
+++ hosts/kiriyu/default.nix
@@ -12,12 +12,8 @@ in {
       ./hardware.nix
       ../../modules/optional/podman.nix
       ../../modules/optional/terminal-implement.nix
-      ../../modules/optional/www.nix
       ../../modules/optional/zfs.nix
-      ../../modules/options/dufs.nix
       ../../modules/options/komari-agent.nix
-      ../../modules/options/openlist.nix
-      ../../modules/options/st.nix
     ]
     ++ ls ../../modules/private/${hostname}
     ++ ls ../../modules/shared;
