115533a59b773a6cb50c167a1f46c6c0f239634a
yonzilch <git@yonzilch.com>
2026-04-19 15:03:00 +0800

    fix: dns not work in containers created by forgejo-actions

--- hosts/example/modules/forgejo-runner.nix
+++ hosts/example/modules/forgejo-runner.nix
@@ -33,4 +33,30 @@
       ];
     };
   };
+
+  networking.firewall.interfaces."forgejo-actions" = {
+    allowedUDPPorts = [53];
+  };
+
+  systemd.services.podman-create-forgejo-network = {
+    wantedBy = ["multi-user.target"];
+    path = with pkgs; [zfs];
+    script = ''
+      ${pkgs.podman}/bin/podman network exists forgejo-actions || \
+      ${pkgs.podman}/bin/podman network create \
+        --ipv6 \
+        --subnet 10.89.0.0/24 \
+        --gateway 10.89.0.1 \
+        --dns 1.1.1.1 \
+        --dns 45.11.45.11 \
+        --dns 9.9.9.9 \
+        --dns 2606:4700:4700::1111 \
+        --dns 2a09:: \
+        --dns 2a0d:2a00:1::2 \
+        --interface-name forgejo-actions \
+        forgejo-actions
+    '';
+    serviceConfig.Type = "oneshot";
+    serviceConfig.RemainAfterExit = true;
+  };
 }
