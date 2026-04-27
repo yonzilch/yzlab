5183b76f3037f6726dd296f6fb5d41c92a719651
yonzilch <git@yonzilch.com>
2026-04-27 14:14:15 +0800

    feat: add photoview example

---
+++ hosts/example/modules/photoview.nix
@@ -0,0 +1,26 @@
+{lib, ...}: {
+  services.photoview = {
+    enable = true;
+    user = "www";
+    database.type = "sqlite";
+    host = "127.0.0.1";
+    port = 39013;
+    dataDir = "/home/www/photoview/dataDir";
+    mediaPath = "/home/www/syncthing/photos";
+    settings = {
+      disableFaceRecognition = true;
+      disableRawProcessing = true;
+      disableVideoEncoding = true;
+    };
+  };
+
+  systemd.services.photoview.serviceConfig = {
+    ProtectHome = lib.mkForce false;
+  };
+
+  systemd.tmpfiles.rules = [
+    "d /home/www/photoview          0755 www www - -"
+    "d /home/www/photoview/dataDir  0755 www www - -"
+    "d /home/www/syncthing/photos   0755 www www - -"
+  ];
+}
