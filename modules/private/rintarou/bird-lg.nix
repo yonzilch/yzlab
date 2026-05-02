43bb1e0d87ba9effaab1afb388e16b2b9e1d2b44
yonzilch <git@yonzilch.com>
2026-05-02 21:09:13 +0800

    chore(dn42): rm host amane and update dn42 config in host hiyajo and rintarou, add bird-lg in host hiyajo

---
+++ modules/private/rintarou/bird-lg.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:lPHbci4FeyY7Mq8FKmhXx6lR3EwU5d5BqO6pW1rY9IboCXs2MbM1LRRjkkaRVGTtD3MP4TxNr2MGW5avpYOtje5UwVz3s4uedfiPH9JWe0HJA/8FoYNI7z3dqntHFA3NtlL1C4z8YDxIinzujU4s+fqJ+LT1s/5JMdlniA8yzdJG6F6sdbPQ4OYWrl242eZpsFgIXztExWL8Yfuomk8pHPG3ilxQwcSO8mJufCqIrxuZ2eHrdz2kGLEY0+LwFAVHAM3kUPPpfKp4cbUfcFgyL8BLvDbLfjnDNyuEgo7401T+BE2SzgFUFXh/f9DTRXH3QAxmVWG2ZatFuwoduh9va6JXIKNnx3cVzrpoJ0gdsf7PyzmSsEFB5mwmmT+/flon/LMWxce9YWhIeAAKI5uwZk6ozcvtmpDJK8qEIY6j3wskvx7QgfGSMhxxE1cogXYwy3AM3fX7sCvTItgOC5b5YwMFaRhaypYlGIG8t02LZXQv2uU+lJzkyvN0qEV5+iLE0ekZHGod8L7yCWEj2F/kVAamNPPkM8s5QSp+TQ==,iv:hD90wtH3/7CQiHmrnCHumZMBXfOHQDBaOKQrBSyYrwk=,tag:80fZK5tzyddAX1fyfZtYkg==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSA2em1MNW5HTm01dEEyamhN\ndjNhVWU2SG0yejhMUzJ2RDgyZ3NZMEx0U1h3CngwZThHVFZkd0c4YmNyNEZ4a3hC\nZUVZbi83TDBnT2JJeHJucmp4eFY5L1EKLS0tIERFQmgrNCsxcjU5QnBoaGRTSkd0\nTkJDc1lzZCtERkdZQjBiV084YmZnYWsK1NZSBAv9BI4RH5iuPKU1TO3IeCyjD0aS\nvF12ML3Qz7XsRH+jo1gOmwHVV4/i4fwvL+bYhRTidiZdtJqi+Yr6Eg==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2026-05-02T13:07:31Z",
+		"mac": "ENC[AES256_GCM,data:hevQ5wDjtk6Nd5qqha4N0/qBX+rbILbQEAWIO7Hvq1nGamkwxc4Ek06gi00rb6kWuyv96EP2v94Y4RSIFCIvoaUrncj0xCXKWbByr9dOwljwMjJ37Hjm50+FsjwNXO6+PUuzWm55c/FvkI1IUCPuRLvtcOi2f/2l4bwnORnIoQI=,iv:AKms5SdgEUWbcf7jtihO1XOXNIB7JEN1jrRWPHhBoxs=,tag:YGc8HmchYF9uDwWo5CTvzQ==,type:str]",
+		"version": "3.12.2"
+	}
+}
