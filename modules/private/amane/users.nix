36d1060845dab89b5941d3e734af4b8b1fe41f2b
yonzilch <git@yonzilch.com>
2026-05-03 16:59:25 +0800

    chore: reinstall host amane

--- modules/private/amane/users.nix
+++ modules/private/amane/users.nix
@@ -1,15 +1,14 @@
 {
-	"data": "ENC[AES256_GCM,data:45rWgBNQf4ld95MYB95msDKqopj2PE9ZXOHmiyIFT0rTS+scisj8HLayUYJO7TpB1Lm66sxKEVvN3ZT8GlbFVrE90Zcp5NZ4RRESba50rnC7E719dyBCHw/FqT09NUD3/msrqLn/5SE7E/3hUQ+xW0UzT6uI1qW0J/3msC/oQ4uAR5k/hI86O1aIqbmRzryLgGWXuelxUFbCm4F3QqFCjSPHmlU9JaWx9ezkyxNKsuvUw4KPrmTOe+FtPTY5Te/za2/y4teTuZCN7/WkD3BS55XeMQo+3XJsI3Gf8dvWiebKBPXtLXnvcBfQHpy9xI+zvmpcSmlHF0W9PAc2M0rluObdIxb9fxCjjlRVROi8Dr48JHeDl5eajQQ6q+6p5RAqoT6cU34c,iv:V9DIHOy3P0bm+bjtWXCC3U30iD0estF9bNzaj3B96/0=,tag:kzJEOSzr/4k61mPTTNoF2w==,type:str]",
+	"data": "ENC[AES256_GCM,data:2o9trdsZXxAVF3kOcLm3JpjykyEGq01YNeF/SX1FTysJhKXGR0kxt/es1w0FaxkSCehcmW/HacfEiW47ZF2JRpSAORAopjkxNS44UkpmjOmzVjrlsmGCy6HrD5/pr9hHhK7oY+2n3dzkUzvfzqxBI3HinAPyyWtHBpvYG6ZUeS1LZah0BAAQHWQ3NNtJPECGJs7cks6XNOy74Gz/6BLAQFlUrBHG6l05H+py8jWCIEXX8arBiQZAGxFYA30vBq73YQZvl2O+3wOP97CBWlPyZyOztueUpdCxwXrru6JhSXiOvCK2+u2zD8MTwl/AaFgEyPuzgzKELO9udWIDS/wU+k01q6VVn497YGh/ZFUyEU+D7X4qN7t8N2SLWYJ4QeFdZttWeZc+,iv:Ld1ApFL6OmMIq1JKKYQwn2Ml7OvD8n1R/0wqMH3sbmo=,tag:2uNUXcogxlzPl/OQumzrUA==,type:str]",
 	"sops": {
 		"age": [
 			{
 				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
-				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSB0UWdVaVdIZjZtQ2kvU3FU\nTUNNMUtHMEtoQUdEVW50Yk5zQk5tZFB1c21vCmF6eHBBZjdTOGM5c05tVmgwRzR0\nRmFxYUI5TlFtQnV3dlV4VCtvNGdSM0kKLS0tIFpkcHR5dCszWllKdkdONjJ6OXM5\nUmlwVlkrR21FdTY2ZEJBOHRrUmRJNUUKMYbGzyZzIJumoIebSGlaqisMRG/RDrM1\nZqNNDDzN5tlucNXyPv6wGa/Mpd/BHEBjxrYuepSz2f7PMbaENUUaMw==\n-----END AGE ENCRYPTED FILE-----\n"
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBZRVFpTHl0N21zT256aVd2\ndkk1c1JQTndnU2tKdVpaZTBNNTVOaHpVeGdRClJZSWZ5aWVWajRML0RCZFZCUFFS\ndVhHYWI5N0dHRVdEVnMxUk9IaisyVWsKLS0tIGlpMUN0anY1bWcvUnRpeFVHVmtl\nTWx0Yk05M2JCSVpGRzhmQXZOdFdMSmMK617uayR3za9EVf80Lp5Fvbk7sKvon+hW\neAPbVQFYctXArZk3rL85fdVwPC5wTmh/zhzZKD3A/gJuchWmQtnonw==\n-----END AGE ENCRYPTED FILE-----\n"
 			}
 		],
-		"lastmodified": "2025-09-25T02:21:20Z",
-		"mac": "ENC[AES256_GCM,data:MNpWI1/orlvr2aKe/LsGwF28/SXT94vGhoCOAn7O0T4hIHruS665KykBrAyHu/8417iOrc9giVFgJczWA4g6aMhNBszzj1ys3KdTT60LtF0Jb0HMk2wQcM+QU12ef2eX4yJ0lJKw6UsTR2D4J7FV9MJj6bWUZw0+cnDCsQY44yY=,iv:sShWB6fRpDfrtNtruXf4iDALhHplsJYZFlf7Poi9oaQ=,tag:JokqSvA4YeEyFr+0UqC8xg==,type:str]",
-		"unencrypted_suffix": "_unencrypted",
-		"version": "3.10.2"
+		"lastmodified": "2026-05-03T08:58:52Z",
+		"mac": "ENC[AES256_GCM,data:XSkibTC4xr8R8ILLPADfAzxw8ikbtvwJ9PlZGgoYaT2/tJRtoDoT41SDM+AZg3zJss1Adw/J/HqvutpySoiu81t9A8jol0oyMIhfDjCId0en5wUaIcZ+/iwyZEIafgj+q6DaNiDoZe6F6aSHxkdOoemB8piIYEIn5e/gmE00LP8=,iv:rQtNau5I9M7SUlAHZzcVihZ6ZzdKcfviRUIre/ftNuM=,tag:b4XRM6NnUg3Pjs28UOj3ow==,type:str]",
+		"version": "3.12.2"
 	}
 }
