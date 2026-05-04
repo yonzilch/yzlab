6b518b02ed84ce7af2b783109ceb9d495ba02c5b
yonzilch <git@yonzilch.com>
2026-05-04 22:08:50 +0800

    feat: add atftpd example

---
+++ hosts/example/modules/atftpd.nix
@@ -0,0 +1,19 @@
+_: {
+  services.atftpd = {
+    enable = true;
+    root = "/tmp/tftp";
+    extraOptions = [
+      "--bind-address 0.0.0.0"
+      "--verbose=7"
+    ];
+  };
+  networking.firewall = {
+    allowedUDPPorts = [69];
+    extraInputRules = ''
+      udp dport 69 ct helper set "tftp"
+    '';
+    extraCommands = ''
+      nft add ct helper inet filter tftp { type "tftp" protocol udp\; }
+    '';
+  };
+}
