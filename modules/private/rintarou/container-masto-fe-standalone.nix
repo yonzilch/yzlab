cdf06f676c1c1564050c7e165cf67f6a33c90565
yonzilch <git@yonzilch.com>
2026-05-05 16:17:27 +0800

    feat: deploy masto-fe-standalone on host rintarou

---
+++ modules/private/rintarou/container-masto-fe-standalone.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:Mfs4MTrklSMcSQiqOfeAGJIH7I3KNMN9S8pNiVSHqehbjz6HZxO96koBxtl5h3vsHTKBkTTbEa9/UMYjPlLaVS1ll5zMfVfpl3Vjip9pnXTCCG+laTOE7TC+1H4z3WQytOmF6XsFlxktp1vAQ4C21VlhHgjFkCziws9KdFc3MBhFCmFKYdaSGuvaJnOQsl/8R3sQhSbzv3rGYMuIyjcLy1tuKToVnHDu5J0vtCiN+cQuDpE7VzvVrgLyyp2IQ7MJBdC1tUi2dus=,iv:cUfKng9vQujzuM6KPlDtzyUtOqDwMGnS4OaEGjk8Xy4=,tag:PHFfsVSrmyBNG/8FZX248g==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBiQUZOMU0xREdlTEJ6bWhC\nUE4wL1R2N29mMDd6dCtPYlZjWUZQTi95eVNJCnBtWTdqUWZEY2x1TVZjcDhHQlZN\nbittM2grY0NCK2t0VmVPb0ZXdlYxRmsKLS0tIGcvWHRabUN4ZGlNeEljTVl0UTZ3\nSWlFYmNSTmg1dlVISGJ1aEhoazI0UFEKeac1eebfRCJK7lRZbXuNMid3TsUGzbqP\nyD42DoPK0sIuM8eeT9R4G2VcPariHCmU7dqQGbcJHQdhoCVr49t6cQ==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2026-05-05T08:16:59Z",
+		"mac": "ENC[AES256_GCM,data:wRyIJrfgaRMLzBFdxYBamCkD/XzrVVohTFyAxZzb83mADmkUUOe4YTm3Vb5BSU2NLvQXfJnzHsDcW4KVyuXvORkoEPUwxZZUBPZOU23AazIzpnvv+lpXvOoVCxDtDdhYQYKSdhGNOJfqFxeT1bs5/j2tSuBoRV8cMkfOAm/5TIY=,iv:VgLLlSGdx9YcKXorOwwyUaYg1hSQ/dAORTUXeKpam5E=,tag:osFv7p4PNIm1NjW6RWp3+Q==,type:str]",
+		"version": "3.12.2"
+	}
+}
