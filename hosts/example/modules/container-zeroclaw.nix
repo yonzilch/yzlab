2e096b9789f0ca40a011737a8981ee8a0282cfff
yonzilch <git@yonzilch.com>
2026-05-05 15:06:27 +0800

    feat: add container-zeroclaw example

---
+++ hosts/example/modules/container-zeroclaw.nix
@@ -0,0 +1,24 @@
+_: {
+  virtualisation.oci-containers.containers."zeroclaw" = {
+    image = "ghcr.io/zeroclaw-labs/zeroclaw:debian";
+    pull = "newer";
+    cmd = ["gateway"];
+    environment = {
+    };
+    volumes = [
+      "zeroclaw_data:/zeroclaw-data:rw"
+      # "/etc/zeroclaw-data/.zeroclaw/config.toml:/zeroclaw-data/.zeroclaw/config.toml"
+    ];
+    ports = [
+      "127.0.0.1:42617:42617"
+    ];
+  };
+
+  # You can use web dashboard to configure zeroclaw
+  # environment.etc."zeroclaw-data/.zeroclaw/config.toml" = {
+  #   mode = "0755";
+  #   text = ''
+  #
+  #   '';
+  # };
+}
