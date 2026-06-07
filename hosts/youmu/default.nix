782a526c4b3d41f98038527250ef7f79254125a3
yonzilch <git@yonzilch.com>
2026-06-07 16:48:50 +0800

    feat: deploy xl on host youmu

--- hosts/youmu/default.nix
+++ hosts/youmu/default.nix
@@ -9,6 +9,7 @@ in {
     [
       ./hardware.nix
       ../../modules/options/komari-agent.nix
+      ../../modules/options/xl.nix
     ]
     ++ ls ../../modules/private/${hostname}
     ++ ls ../../modules/shared;
