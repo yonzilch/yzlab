474dbd10663ab087116d3785374a98904cde7e42
yonzilch <git@yonzilch.com>
2026-06-08 22:57:57 +0800

    feat: add toolchain in host christina

---
+++ modules/private/christina/toolchain.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:UxrAUZClvXrCMHKnH1scq9ek0ARjbtd+g66iRBBv2jiHJRwBcVi7bQWTn2um0zKeKXDP71oBmKH6WOOE/rzMKKNxZdyMHjPyPMNEh6DCLzwX/hUboor3MLB++JMtmD0YA6/QBuHycTndGBCDR9xr1eUEzz4WF4+xhP77v3Oh5P2NH7CPRDZ1pcWTgLxi3cDpXbcRnktSgIoWYSfydsmJk/KpCuo2,iv:p5CaInjP2hyVJZzdGTTEHhnL0TwK/tL5x1KEZy05rt8=,tag:legE3XeNsopxcwv0F6Et1A==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBJcmFvdnFRM25FNUduZFMy\nR2NTVUl2dVlXK1pLTVoweC9NZzdzUms5TmlzCjNWRXk2WXk2eEZXbm1jQXNMS2I1\nU3gzQVNsVlB1dnRwVWNrQ21wemdLVmsKLS0tIHZhTk9PbFBibCtDcFk2aGQza1B6\nYkgzVXR1bGVuUFpOcDF6VWxMcExkcEUKT39b3HkgzjbPgX1SOHAs9g3yr7kTED/h\nUCbpPIeQz9+DT6EGPlz+MQHlaOPZRso3tUKtfBGXb7Xt/ndDy3f2UQ==\n-----END AGE ENCRYPTED FILE-----\n",
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel"
+			}
+		],
+		"lastmodified": "2026-06-08T14:57:37Z",
+		"mac": "ENC[AES256_GCM,data:O4nTtKqV1x80P8FZGWiHmZKmXYKZawpB+IK0a8WTaLmH+cNsB9r3WImLWRhFwEZmzi1DNbnbvx6iol43z20YTjDCDACD1kXV9UFsxq5dR5n6jTjHh70loOQZhkIBXGGWbzx+z/FvzOeAqsTXoZ8ztpM/Q7bwMgAP6MJIa10PN9A=,iv:qXIIeXntnNj66o0IJ1DKXIEyQaBXaQk8+8RzqCqvaNU=,tag:/RD/Mro0esp1OMPZ2fRPEA==,type:str]",
+		"version": "3.13.1"
+	}
+}
