7cd1aa19a159de31ba4f8320fb657f4d6eaaf05f
yonzilch <git@yonzilch.com>
2026-06-12 01:30:51 +0800

    feat: configure git config in host christina

---
+++ modules/private/christina/git.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:7MayimikawSTzT7bBMNU64zp/O/uOMPSzwFNYonncJlSYohif6VpS9xtvR6HnnqIudDHOca2pGR2Pb1dVASenSAH+zWyy9ivInkLLiz8NxA+itAk1W5PFrmKIqodbPULso8MxtUr/Td8yXJa3K9AwASX2P1xMoT/jr7R0gC/59SbVM+MF/DPDn2laC10rvcaF4TyE6locbTHzXvumd7GR/dFGt+MXkRfvWqLQwy0x7A9oP3tEci0/QdNlcBMoiyd/kToWdkq5HY9co4vzQkfj5jIzPK1H0opg0jNhP5cCLm+vHhYtH9nwFbOGK77/3CIfx+F/ON6PQmIHc1YH7ehSxYr4kr28w07PXNI6DOqBq3G2snctc/uXWkSPTDztEUulIgGiQNpRXNSmGrjuq+juBTBg2sPuuuxxZCOaRpfzCefygzwUDNA/stNLo8JVLf5rd4=,iv:aACunfdUE7z91x8PoV8w3u/nc6s5EL/m1cPlE4MJ1Rs=,tag:CXpEkODMf3oUn9u7VAOfiw==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSA1STJ2SFRzOURObnpKaDBh\nWG5jb0tlMWEzOHlXQTJUcnZvZkRmWG5CMkc0CnBueitXVFZnTHpOcElOVjlERDRt\nTmdKZDVNUXdrd3d2WlQ3S09aN1k3K1EKLS0tIG8vTVFuMGN0NjRwSERoMUJxeDdS\nYlVmd2U3dWpyZklxNVFXb3JrYlIwMmMKnVFci3gnqB7GpuoY2Sv/sBXT7Xsng4QZ\n1hNJH9ONOG5N5OrY8FWzuN46RgQ54mE5qCqfbe0aQ6/qbwi0cH4RJg==\n-----END AGE ENCRYPTED FILE-----\n",
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel"
+			}
+		],
+		"lastmodified": "2026-06-11T17:30:32Z",
+		"mac": "ENC[AES256_GCM,data:UxJd0drIGO2yd7wX9f+IGY2NUa6RXmrHeKI7hpZJ0PfGPexdT8M4vEGaMbC65YEOd5wwVjjRzfWsWxeONOhx++liJOKhT0yokzmjVdtVKLyqAyEzT+SwjVjLrnk25uQafmMPQao++ckK6DIwbnWKHh87kBqiubP8kcRZtvAarOY=,iv:qK7CejdSHCaZ/cT9dQMZqktJj/y2HBxHGqbVFjIAsfY=,tag:N7row3aJxmubSUfzz8MzIg==,type:str]",
+		"version": "3.13.1"
+	}
+}
