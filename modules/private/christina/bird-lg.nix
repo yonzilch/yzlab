0449c77aae5cc86a487a0b279fb8c9c9c694d79f
yonzilch <git@yonzilch.com>
2026-07-12 16:13:17 +0800

    feat: deploy bird-lg node on host christina

---
+++ modules/private/christina/bird-lg.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:UwWweCzJAN37qIgbJCq3AMtgiLqfluh1BNNxgw/96GCtyjLf5DnuH4fBDwmL6bceJDhoTZ1m2c6xtXMyAiidd7EHK1+t3zu/BDd3zUSAsUxRza4PawmYj+p/qO5UaItIZL0vPoWclGXSo96/iahbiTl/plsh4spEuJ55sFzE7jtqFA1EQrotUyaHMUYLNCefEMmGvEjjIwRwNUzEXpP6w4glcsLcnKxf3WUclLYgpWId1UeazbmHDGTByyrPIdmF0qaJT78jAx63Q7GCYNVrhdC02fDLg60eM3sC1jxnYnaJcEnoEE2ja9zwVUfpeZy7XLZplUVfNZMFivJ770wrOD99homA7AH4uPf3KhvII/uA0YYbAofjRpcjhX6W7xRma7Ut6NywHMK882rzGwFf3zXpC1cGsWCboowAt44n+MFi6A2fgbxNBRvPf18hROsMflm8plfyLj2mm9fh3iSJCrBDGGJ5fo0deKDhEdtGJX8JoSftCuYBp4ket3wIljkt3IF7VRgWrlGzQVf9Z2Lr74oiq1IXbJmtkkPEXg==,iv:DeSKyF9yWJJPJciBMaEyhMcQu5bippp9aTpbFhLC7LQ=,tag:312KFwEsmRwXrRB+ZfpdYw==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBCdzJua0p0QTZ2Y2QrU2xy\nNG9VNi93SnRtLzBNQ1l3WHR3dHV1T1VUTHgwCkIzQ1Y1a2lKVjl4d3QxenVNMjZl\nRmRCOUxJMGYvd28xRUtCQmc1UFIxL28KLS0tIG42Z0ozaDFQRE1sYkdPVHdZdDJJ\nMjhLcU9XZjdTdjlaQi9kZlJuK1VSTmMK0uywyJot+nT7RNdT/w4SIMHRZdrCFwkl\niJPa/+vJBmH30NgOxqlzaCQMb0Ej2m2O/Hj/eXY8BtE638jEFEx0+A==\n-----END AGE ENCRYPTED FILE-----\n",
+				"recipient": "age1yzce0p359lc6lfg087l3mvj0yrqd4x9526yyvnj2qet6t3ufnuns8f6rel"
+			}
+		],
+		"lastmodified": "2026-07-12T08:11:53Z",
+		"mac": "ENC[AES256_GCM,data:43iqdtVlbiQ18vH6V5m/XoT1i8h5jvo9/pve4bA81ReCk6zAxVl1KBi6ExQVlxovBABlGLUY6Jo4pFLtRsvgjzcG6QgXSoBdGeDQfRToo4yUAaAtNhIA65/hovrmH1PJ5CEGVFcTsreu75WTabxzFBjgwTcMyzJ2Ort/zn+Gi9s=,iv:dvTJBQQhHbG8MkfwCw39rEd05pxxDy15BiQAJttQ6Ag=,tag:os/7oM4rPIUuiUMYlSqJmA==,type:str]",
+		"version": "3.13.1"
+	}
+}
